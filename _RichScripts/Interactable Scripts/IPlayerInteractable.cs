a730a8e41c9dc8a47a5b2c58ec3317276bf7ebb7
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2021-02-03 23:11:49 -0800

    Update IPlayerInteractable.cs

--- _RichScripts/Interactable Scripts/IPlayerInteractable.cs
+++ _RichScripts/Interactable Scripts/IPlayerInteractable.cs
@@ -1,13 +1,10 @@
-﻿namespace Explore
+﻿
+/// <summary>
+/// An interactable that uses Player as an Actor.
+/// TAG MUST BE SET TO INTERACTABLE FOR THIS TO WORK AS EXPECTED.
+/// </summary>
+/// <remarks>Strongly-typed for convenience and brevity.</remarks>
+public interface IPlayerInteractable : IInteractable<Player>
 {
-    /// <summary>
-    /// An interactable that uses Player as an Actor.
-    /// TAG MUST BE SET TO INTERACTABLE FOR THIS TO WORK AS EXPECTED.
-    /// </summary>
-    /// <remarks>Strongly-typed for convenience and brevity.</remarks>
-    public interface IPlayerInteractable : IInteractable<Player>
-    {
-        //interfaces just kind of exist
-    }
-
+    //interfaces just kind of exist
 }
