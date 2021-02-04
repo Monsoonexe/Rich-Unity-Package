17a9fdd6f3d9285ddfcb4989483d14c6b22bc991
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2021-02-03 22:59:18 -0800

    Create WeightedItemStack.cs

---
+++ _RichScripts/Weighted Random/WeightedItemStack.cs
@@ -0,0 +1,20 @@
+﻿
+/// <summary>
+/// Weighted ItemStack.
+/// </summary>
+/// <seealso cref="RandomItemStackGenerator"/>
+[System.Serializable]
+public class WeightedItemStack : AWeightedProbability<ItemStack>
+{
+	//exists
+
+	#region Constructors
+	
+	public WeightedItemStack(int weight, ItemStack stack)
+        : base(weight, stack)
+    {
+
+    }
+	
+	#endregion
+}
