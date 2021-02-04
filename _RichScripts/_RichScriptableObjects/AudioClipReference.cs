b7ca3e97a8fbe51cb828b1dadbd23cc855cb1fd1
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2021-02-03 23:08:00 -0800

    Update AudioClipReference.cs

--- _RichScripts/_RichScriptableObjects/AudioClipReference.cs
+++ _RichScripts/_RichScriptableObjects/AudioClipReference.cs
@@ -1,21 +1,17 @@
 ﻿using UnityEngine;
 
-namespace Explore.Audio
+[CreateAssetMenu(fileName = "_AudioClipReference",
+    menuName = "ScriptableObjects/References/Audio Clip")]
+public class AudioClipReference : ScriptableReference<AudioClip>
 {
-    [CreateAssetMenu(fileName = "_AudioClipReference",
-        menuName = "ScriptableObjects/References/Audio Clip")]
-    public class AudioClipReference : ScriptableReference<AudioClip>
-    {
-        [Header("---Clip Options---")]
-        [SerializeField]
-        private AudioOptions defaultOptions = AudioOptions.Default;
-        public AudioOptions Options { get => defaultOptions; }
+    [Header("---Clip Options---")]
+    [SerializeField]
+    private AudioOptions defaultOptions = AudioOptions.Default;
+    public AudioOptions Options { get => defaultOptions; }
 
-        /// <summary>
-        /// Play the AudioClip with the attached AudioOptions.
-        /// </summary>
-        public AudioID PlaySFX()
-            => AudioManager.PlaySFX(value, defaultOptions);
-    }
-
+    /// <summary>
+    /// Play the AudioClip with the attached AudioOptions.
+    /// </summary>
+    public AudioID PlaySFX()
+        => AudioManager.PlaySFX(value, defaultOptions);
 }
