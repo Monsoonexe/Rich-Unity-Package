c8e7c6cb91ed2f9a01742deab983a1f7d2a00770
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2021-02-03 23:06:21 -0800

    Update IDamageable.cs

--- _RichScripts/RPG/IDamageable.cs
+++ _RichScripts/RPG/IDamageable.cs
@@ -3,6 +3,6 @@
     public interface IDamageable
     {
         void TakeDamage(int damageAmount);
-        void TakeDamage(DamagePacket damagePacket, Entity damageSource);
+        //void TakeDamage(DamagePacket damagePacket, Entity damageSource);
     }
 }
