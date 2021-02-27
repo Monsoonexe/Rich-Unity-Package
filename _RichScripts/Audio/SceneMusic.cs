bb59f7c6dd98ab258ef376d79f982581d1029d88
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2021-02-27 08:50:31 -0800

    new audio stuff

---
+++ _RichScripts/Audio/SceneMusic.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+namespace ProjectEmpiresEdge.Audio
+{
+    public class SceneMusic : RichMonoBehaviour
+    {
+        [Header("---Scene Audio---")]
+        [SerializeField] private AudioClip music;
+
+        [SerializeField] private bool playOnAwake = false;
+
+        [SerializeField] private AudioOptions options = new AudioOptions();
+
+        private AudioID audioID;
+
+        private void Start()
+        {
+            if (playOnAwake)
+            {
+                PlaySound();
+            }
+        }
+
+        private void OnDisable()
+        {
+            StopSound();
+        }
+
+        public void PlaySound()
+        {
+            audioID = AudioManager.PlayBackgroundTrack(music, options);
+        }
+
+        public void StopSound()
+        {
+            AudioManager.StopSFX(audioID);
+        }
+    }
+}
