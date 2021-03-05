3e792763d61dccb82614c9e0ea00b16c12f1590f
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2021-03-05 03:17:17 -0800

    new audio utility for playing a clip from anywhere

---
+++ Editor/RichEditorUtility.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class RichEditorUtility
+{
+    /// <summary>
+    /// Play an AudioClip one-shot inside the Editor.
+    /// </summary>
+    /// <param name="clip"></param>
+    /// <param name="loop"></param>
+    /// <param name="startSample"></param>
+    public static void EditorPlayClip(AudioClip clip, bool loop = false, int startSample = 0)
+    {
+        System.Reflection.Assembly unityEditorAssembly = typeof(AudioImporter).Assembly;
+        System.Type audioUtilClass = unityEditorAssembly.GetType("UnityEditor.AudioUtil");
+        System.Reflection.MethodInfo method = audioUtilClass.GetMethod(
+            "PlayClip",
+            System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.Public,
+            null,
+            new System.Type[] { typeof(AudioClip), typeof(int), typeof(bool) },
+            null
+        );
+        method.Invoke(
+            null,
+            new object[] { clip, startSample, loop }
+        );
+    }
+}
