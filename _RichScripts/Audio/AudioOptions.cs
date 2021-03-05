806efea9159334d3984d88e43f0b8864172c725a
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2021-03-05 03:33:11 -0800

    added attributes, comments, and metas

--- _RichScripts/Audio/AudioOptions.cs
+++ _RichScripts/Audio/AudioOptions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using UnityEngine;
 
 /// <summary>
 /// Options for dynamically playing sounds
@@ -10,31 +11,40 @@ public struct AudioOptions
     /// <summary>
     /// Repeat when complete.
     /// </summary>
+    [Tooltip("Repeat when complete.")]
     public bool loop;
 
     /// <summary>
-    /// -3, 3. Randomize pitch slightly for variation.
+    /// Randomize pitch slightly for variation.
     /// </summary>
+    [Tooltip("Randomize pitch slightly for variation.")]
     public bool pitchShift;
 
     /// <summary>
     /// How much time should this clip should be played for.
+    /// Value LT 0 means "full length of clip".
     /// </summary>
+    [Tooltip("How much time should this clip should be played for. \nValue LT 0 means 'full length of clip'.")]
     public float duration;
 
     /// <summary>
     /// Slowly fade out old track to replace with this one. Useful for background music.
     /// </summary>
+    [Tooltip("Slowly fade out old track to replace with this one. Useful for background music.")]
     public float crossfade;
 
     /// <summary>
     /// 0.0 - 1.0
     /// </summary>
+    [Tooltip("[0.0 - 1.0")]
+    [Range(0, 1.0f)]
     public float volume;
 
     /// <summary>
-    /// 1 (high) - 256 (low)
+    /// 1 (high) - 256 (low).
     /// </summary>
+    [Tooltip("1 (hi) - 256 (lo).")]
+    [Range(1, 256)]
     public int priority;
 
     #region Constructors
