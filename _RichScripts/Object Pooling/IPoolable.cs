62f364e83a958147661833e73337ef0e9de153de
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2021-03-05 03:18:30 -0800

    added bool InUse property

--- _RichScripts/Object Pooling/IPoolable.cs
+++ _RichScripts/Object Pooling/IPoolable.cs
@@ -4,12 +4,17 @@
 public interface IPoolable
 {
     GameObjectPool PoolOwner { get; set; }
-    void OnCreate();
-    void OnEnpool();
-    void OnDepool();
+    bool InUse { get; } // has been Depooled and is doing its thing.
+    void OnCreate(); //when created
+    void OnEnpool(); //into pool
+    void OnDepool(); //out of pool
 }
 
 public static class IPoolable_Extensions
 {
+    /// <summary>
+    /// Return to the Pool from which you came.
+    /// </summary>
+    /// <param name="p"></param>
     public static void ReturnToPool(this IPoolable p) => p.PoolOwner.Enpool(p);
 }
