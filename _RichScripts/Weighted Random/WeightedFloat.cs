9fc3c0320c51861dac1d6b512fe52a008e07684b
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2021-03-28 02:53:17 -0700

    new weighted float

---
+++ _RichScripts/Weighted Random/WeightedFloat.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+/// <summary>
+/// 
+/// </summary>
+/// <seealso cref="RandomFloatGenerator"/>
+[Serializable]
+public class WeightedFloat : AWeightedProbability<float>
+{
+    //exists
+
+    #region Constructors
+
+    public WeightedFloat(int weight, float value)
+        : base(weight, value)
+    {
+        //constructs
+    }
+
+    #endregion
+}
