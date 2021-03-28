b2edbeec16d410a9144ab242b4ad44333464f391
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2021-03-28 02:56:09 -0700

    new poolable Mono

---
+++ _RichScripts/Object Pooling/Poolable.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// A basic Poolable MonoBehaviour.
+/// </summary>
+public class Poolable : RichMonoBehaviour, IPoolable
+{
+    public float lifetime = 10f;
+
+    public GameObjectPool PoolOwner { get; set; }
+
+    public bool InUse { get; }
+
+    private Coroutine lifetimeCoroutine;
+
+    public virtual void OnCreate()
+    {
+        //nada
+    }
+
+    public virtual void OnDepool()
+    {
+        if (lifetime > 0)
+        {
+            lifetimeCoroutine = StartCoroutine(
+                Utility.InvokeAfterDelay(ReturnToPool, lifetime));
+        }
+    }
+
+    public virtual void OnEnpool()
+    {
+        if (lifetimeCoroutine != null)
+        {
+            StopCoroutine(lifetimeCoroutine);
+        }
+    }
+
+    public void ReturnToPool()
+        => PoolOwner.Enpool(this);
+}
