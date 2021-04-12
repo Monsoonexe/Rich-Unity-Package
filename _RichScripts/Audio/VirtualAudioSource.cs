a0bf9b979a486a7eb5a1167fb68e368d6e2ff4ed
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2021-04-12 15:50:06 -0700

    new virtual audio source that ties into AudioManager static calls

---
+++ _RichScripts/Audio/VirtualAudioSource.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// A Mono that will play audio from static AudioManager.
+/// Helpful for rigging events and prefabs to audio that aren't
+/// yet aware of an AudioManager in a scene.
+/// </summary>
+/// <seealso cref="AudioManager"/>
+public class VirtualAudioSource : RichMonoBehaviour
+{
+    public AudioClip clip;
+    public bool playOnAwake = false;
+    public bool stopOnDestroy = false;
+    public AudioOptions options;
+
+    private AudioID audioID;
+
+    private void Start()
+    {
+        if (playOnAwake && clip)
+            PlaySFX(clip, options);
+    }
+
+    private void OnDestroy()
+    {
+        if (stopOnDestroy)
+            StopSFX();
+    }
+
+    public AudioID PlayBackgroundTrack(AudioClip clip)
+        => audioID = AudioManager.PlayBackgroundTrack(clip, default);
+
+    public AudioID PlayBackgroundTrack(AudioClip clip,
+        AudioOptions options)
+        => audioID = AudioManager.PlayBackgroundTrack(clip, options);
+
+    public void PlaySFX(AudioClip clip)
+        => audioID = AudioManager.PlaySFX(clip);
+
+    public void PlaySFX(AudioClipReference clipRef)
+        => audioID = AudioManager.PlaySFX(
+            clipRef.Value, clipRef.Options);  
+
+    /// <summary>
+    /// Play the given clip. If 'duration' LT 0, then it will be length of clip.
+    /// </summary>
+    /// <param name="clip"></param>
+    /// <param name="options"></param>
+    public AudioID PlaySFX(AudioClip clip, AudioOptions options)
+        => audioID = AudioManager.PlaySFX(clip, options);
+
+    public void StopSFX()
+        => AudioManager.StopSFX(audioID, 0);
+
+}
