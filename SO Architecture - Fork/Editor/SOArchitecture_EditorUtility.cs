04a03e091e5244fdf433f7ad71ca7961beac0362
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2021-05-03 14:54:01 -0700

    hard fork of SO Architecture
    but better!

---
+++ SO Architecture - Fork/Editor/SOArchitecture_EditorUtility.cs
@@ -0,0 +1,158 @@
+﻿using System.Reflection;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.Callbacks;
+using Type = System.Type;
+
+namespace ScriptableObjectArchitecture.Editor
+{
+    public static class SOArchitecture_EditorUtility
+    {
+        static SOArchitecture_EditorUtility()
+        {
+            //We use this as a default since it'll be Assembly-CSharp-Editor
+            _defaultTargetType = typeof(SOArchitecture_EditorUtility).Assembly;
+
+            CreateDebugStyle();
+        }
+        
+        /// <summary>
+        /// A debug <see cref="GUIStyle"/> that allows for identification of EditorGUI Rect issues.
+        /// </summary>
+        public static GUIStyle DebugStyle { get; private set; }
+        private const float DebugStyleBackgroundAlpha = 0.33f;
+        
+        private static PropertyDrawerGraph _propertyDrawerGraph;
+        private static Assembly _defaultTargetType;
+        private static BindingFlags _fieldBindingsFlag = BindingFlags.Instance | BindingFlags.NonPublic;
+
+        private static void CreateDebugStyle()
+        {
+            DebugStyle = new GUIStyle();
+
+            Color debugColor = Color.magenta;
+            debugColor.a = DebugStyleBackgroundAlpha;
+
+            DebugStyle.normal.background = CreateTexture(2, 2, debugColor);
+        }
+
+        /// <summary>
+        /// Converts the entire rect of a <see cref="UnityEditorInternal.ReorderableList"/> element into a rect used for displaying a field
+        /// </summary>
+        public static Rect GetReorderableListElementFieldRect(Rect elementRect)
+        {
+            elementRect.height = EditorGUIUtility.singleLineHeight;
+            elementRect.y++;
+
+            return elementRect;
+        }
+        public static bool SupportsMultiLine(Type type)
+        {
+            return type.GetCustomAttributes(typeof(MultiLine), true).Length > 0;
+        }
+        public static bool HasPropertyDrawer(Type type)
+        {
+            return HasPropertyDrawer(type, _defaultTargetType);
+        }
+        public static bool HasPropertyDrawer(Type type, Assembly assembly)
+        {
+            if (HasBuiltinPropertyDrawer(type))
+                return true;
+
+            if (_propertyDrawerGraph == null)
+                _propertyDrawerGraph = new PropertyDrawerGraph();
+
+            _propertyDrawerGraph.CreateGraph(assembly);
+
+            return _propertyDrawerGraph.HasPropertyDrawer(type);
+        }
+        private static bool HasBuiltinPropertyDrawer(Type type)
+        {
+            if (type.IsPrimitive || type == typeof(string) || IsFromUnityAssembly(type))
+                return true;
+
+            return false;
+        }
+        private static bool IsFromUnityAssembly(Type type)
+        {
+            return type.Assembly == typeof(GameObject).Assembly;
+        }
+        [DidReloadScripts]
+        private static void OnProjectReloaded()
+        {
+            _propertyDrawerGraph = null;
+        }
+        private static Texture2D CreateTexture(int width, int height, Color col)
+        {
+            Color[] pix = new Color[width * height];
+            for (int i = 0; i < pix.Length; ++i)
+            {
+                pix[i] = col;
+            }
+            Texture2D result = new Texture2D(width, height);
+            result.SetPixels(pix);
+            result.Apply();
+            return result;
+        }
+
+        /// <summary>
+        /// Goes through the entirety of the project and collects data about custom property drawers
+        /// </summary>
+        private class PropertyDrawerGraph
+        {
+            private List<Type> _supportedTypes = new List<Type>();
+            private List<Type> _supportedInheritedTypes = new List<Type>();
+            private List<Assembly> _checkedAssemblies = new List<Assembly>();
+
+            public bool HasPropertyDrawer(Type type)
+            {
+                foreach (Type supportedType in _supportedTypes)
+                {
+                    if (supportedType == type)
+                        return true;
+                }
+
+                foreach (Type inheritedSupportedType in _supportedInheritedTypes)
+                {
+                    if (type.IsSubclassOf(inheritedSupportedType))
+                        return true;
+                }
+
+                return false;
+            }
+            public void CreateGraph(Assembly assembly)
+            {
+                if (_checkedAssemblies.Contains(assembly))
+                    return;
+
+                _checkedAssemblies.Add(assembly);
+
+                foreach (Type type in assembly.GetTypes())
+                {
+                    object[] attributes = type.GetCustomAttributes(typeof(CustomPropertyDrawer), false);
+
+                    foreach (object attribute in attributes)
+                    {
+                        if (attribute is CustomPropertyDrawer)
+                        {
+                            CustomPropertyDrawer drawerData = attribute as CustomPropertyDrawer;
+
+                            bool useForChildren = (bool)typeof(CustomPropertyDrawer).GetField("m_UseForChildren", _fieldBindingsFlag).GetValue(drawerData);
+                            Type targetType = (Type)typeof(CustomPropertyDrawer).GetField("m_Type", _fieldBindingsFlag).GetValue(drawerData);
+
+                            if (useForChildren)
+                            {
+                                _supportedInheritedTypes.Add(targetType);
+                            }
+                            else
+                            {
+                                _supportedTypes.Add(targetType);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ SO Architecture - Fork/Variables/BaseVariable.cs
@@ -0,0 +1,135 @@
+﻿using UnityEngine;
+
+namespace ScriptableObjectArchitecture
+{
+    public interface IBaseVariable 
+    {
+        bool IsClamped { get; }
+        bool Clampable { get; }
+        bool ReadOnly { get; }
+        System.Type Type { get; }
+        object BaseValue { get; set; }
+        void Raise();
+    }
+    public abstract class BaseVariable<T> : GameEventBase<T>, IBaseVariable
+    {
+        public virtual T Value
+        {
+            get
+            {
+                return _value;
+            }
+            set
+            {
+                _value = SetValue(value);
+                Raise();
+            }
+        }
+        public virtual T MinClampValue
+        {
+            get
+            {
+                if(Clampable)
+                {
+                    return _minClampedValue;
+                }
+                else
+                {
+                    return default(T);
+                }
+            }
+        }
+        public virtual T MaxClampValue
+        {
+            get
+            {
+                if(Clampable)
+                {
+                    return _maxClampedValue;
+                }
+                else
+                {
+                    return default(T);
+                }
+            }
+        }
+
+        public virtual bool Clampable { get { return false; } }
+        public virtual bool ReadOnly { get { return _readOnly; } }
+        public virtual bool IsClamped { get { return _isClamped; } }
+        public virtual System.Type Type { get { return typeof(T); } }
+        public virtual object BaseValue
+        {
+            get
+            {
+                return _value;
+            }
+            set
+            {
+                _value = SetValue((T)value);
+                Raise();
+            }
+        }
+
+        [SerializeField]
+        protected T _value = default(T);
+        [SerializeField]
+        private bool _readOnly = false;
+        [SerializeField]
+        private bool _raiseWarning = true;
+        [SerializeField]
+        protected bool _isClamped = false;
+        [SerializeField]
+        protected T _minClampedValue = default(T);
+        [SerializeField]
+        protected T _maxClampedValue = default(T);
+        
+        public virtual T SetValue(T value)
+        {
+            if (_readOnly)
+            {
+                RaiseReadonlyWarning();
+                return _value;
+            }
+            else if(Clampable && IsClamped)
+            {
+                return ClampValue(value);
+            }
+
+            return value;
+        }
+        public virtual T SetValue(BaseVariable<T> value)
+        {
+            if (_readOnly)
+            {
+                RaiseReadonlyWarning();
+                return _value;
+            }
+            else if (Clampable && IsClamped)
+            {
+                return ClampValue(value.Value);
+            }
+
+            return value.Value;
+        }
+        protected virtual T ClampValue(T value)
+        {
+            return value;
+        }
+        private void RaiseReadonlyWarning()
+        {
+            if (!_readOnly || !_raiseWarning)
+                return;
+
+            Debug.LogWarning("Tried to set value on " + name + ", but value is readonly!", this);
+        }
+        public override string ToString()
+        {
+            return _value == null ? "null" : _value.ToString();
+        }
+        public static implicit operator T(BaseVariable<T> variable)
+        {
+            return variable.Value;
+        }
+    } 
+}
