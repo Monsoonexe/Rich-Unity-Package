7a402c4003b955e53ffd9a5433d48c143ce7eb67
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2021-05-03 14:44:43 -0700

    UI elements and metas

---
+++ _RichScripts/UI/ToggleBoxUI.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using ScriptableObjectArchitecture;
+
+/// <summary>
+/// Say something!
+/// </summary>
+public class ToggleBoxUI : RichUIElement
+{
+    [Header("---Toggle State---")]
+    public bool ToggleValue;
+
+    [Header("---Toggle Vernacular---")]
+    public string onString = "On";
+    public string offString = "Off";
+
+    [Header("---Toggle Colors---")]
+    public Color onColor;
+    public Color offColor;
+
+    [Header("---Toggle Sprites---")]
+    public Sprite onSprite;
+    public Sprite offSprite;
+
+    [Header("---Prefab Refs---")]
+    [SerializeField]
+    private Image toggleImage;
+
+    [SerializeField]
+    private Button toggleButton;
+
+    [SerializeField]
+    private TextMeshProUGUI toggleTextTMP;
+
+    [Header("---Game Events---")]
+    [SerializeField]
+    private BoolUnityEvent toggleEvent;
+
+    private void Awake()
+    {   //subscribe to events
+        toggleButton.onClick.AddListener(Toggle);
+    }
+
+    private void Start()
+    {
+        SetState(ToggleValue); // init starting state
+    }
+
+    private void OnDestroy()
+    {   //unsubscribe
+        toggleButton.onClick.RemoveListener(Toggle);
+    }
+
+    public void Toggle()
+    {
+        SetState(!ToggleValue);
+    }
+
+    public void SetState(bool togg)
+    {
+        ToggleValue = togg;
+        if(togg)
+        {
+            toggleImage.sprite = onSprite;
+            toggleImage.color = onColor;
+            toggleTextTMP.text = onString;
+        }
+        else
+        {
+            toggleImage.sprite = offSprite;
+            toggleImage.color = offColor;
+            toggleTextTMP.text = offString;
+        }
+        toggleEvent.Invoke(togg);
+    }
+    
+}
---
+++ _RichScripts/UI/VariableText.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using TMPro;
+using ScriptableObjectArchitecture;
+
+public class VariableText : RichUIElement
+{
+    [Header("---Scene Refs---")]
+    [SerializeField]
+    private TextMeshProUGUI tmp;
+
+    [Header("---Data Refs---")]
+    [SerializeField]
+    private BaseVariable data;
+
+    private void OnEnable()
+    {
+        data.AddListener(UpdateVisuals);
+    }
+
+    private void OnDisable()
+    {
+        data.RemoveListener(UpdateVisuals);
+    }
+
+    private void Start()
+    {
+        UpdateVisuals();
+    }
+
+    /// <summary>
+    /// Update UI elements with current values.
+    /// </summary>
+    public void UpdateVisuals()
+    {
+        tmp.text = data.ToString();
+    }
+}
