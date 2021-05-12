4a1532ad85bf58a16aedacb19e078c51a5a9c356
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2021-05-12 14:17:55 -0700

    new scene menu

---
+++ Editor/SceneMenu.cs
@@ -0,0 +1,21 @@
+﻿using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
+
+public static class SceneMenu
+{
+    [MenuItem("Scenes/Reload Current Scene")]
+    public static void ReloadCurrentScene()
+        => LoadScene(SceneManager.GetActiveScene());
+
+    public static void LoadScene(Scene scene)
+        => LoadScene(scene.path);
+
+    public static void LoadScene(string scenePath)
+        => EditorSceneManager.OpenScene(scenePath);
+
+    //e.g. 
+    //[MenuItem("Scenes/Start Menu Scene")]
+    //private static void LoadStartMenuScene()
+    //   => LoadScene("Assets/Scenes/StartMenu.unity");
+}
