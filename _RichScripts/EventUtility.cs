0174a59f5cf2176cd0efdf03221afb10a7b2666e
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2021-05-17 15:16:57 -0700

    new utility

---
+++ _RichScripts/EventUtility.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace ColdironTools.Events
+{
+    public class EventUtility : MonoBehaviour
+    {
+        #region Fields
+        [SerializeField] private UnityEvent enableEvent = new UnityEvent();
+        [SerializeField] private UnityEvent disableEvent = new UnityEvent();
+        [SerializeField] private UnityEvent startEvent = new UnityEvent();
+        [SerializeField] private UnityEvent destroyEvent = new UnityEvent();
+        #endregion
+
+        #region Methods
+        void OnEnable()
+        {
+            enableEvent.Invoke();
+        }
+
+        private void OnDisable()
+        {
+            disableEvent.Invoke();
+        }
+
+        void Start()
+        {
+            startEvent.Invoke();
+        }
+
+        void OnDestroy()
+        {
+            destroyEvent.Invoke();
+        }
+        #endregion
+    }
+}
