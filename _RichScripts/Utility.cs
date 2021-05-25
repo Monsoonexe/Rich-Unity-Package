486c4d6cffdef317c37f1c70936a68faf6919f87
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2021-05-25 02:16:00 -0700

    parse bool value helper

--- _RichScripts/Utility.cs
+++ _RichScripts/Utility.cs
@@ -488,6 +488,27 @@ public static class Utility
         SetLayerRecursively(gameObj.transform, newLayer);
     }
 
+    /// <summary>
+    /// Case insensitive check.
+    /// </summary>
+    /// <param name="source">Source string.</param>
+    /// <param name="result">Result is invalid if 'false' is returned.</param>
+    /// <returns>True if parse was successful. Result is invalid if 'false' is returned.</returns>
+    public static bool TryStringToBool(this string source, out bool result)
+    {
+        var success = true;
+        result = false;
+        var lowerSource = source.ToLower();
+        var isTrue = lowerSource == ConstStrings.TRUE_STRING_LOWER;
+
+        success = lowerSource == ConstStrings.FALSE_STRING_LOWER
+            || isTrue;
+
+        result = isTrue;
+
+        return success;
+    }
+
     #region Math
 
     public static int AbsoluteValue(int i)
