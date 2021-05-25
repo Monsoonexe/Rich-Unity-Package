cbdba361c2fb18e23f16c2ba8bd3185686be9984
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2021-05-25 02:18:16 -0700

    new helper for loading scriptables from InputField

---
+++ _RichScripts/UI/InputFieldToScriptable.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using TMPro;
+using ScriptableObjectArchitecture;
+
+[RequireComponent(typeof(TMP_InputField))]
+public class InputFieldToScriptable : RichUIElement<BaseVariable>
+{
+    //member Components
+    private TMP_InputField inputField;
+
+    private void Awake()
+    {
+        inputField = GetComponent<TMP_InputField>();
+    }
+
+    private void Start()
+    {
+        inputField.text = targetData.ToString();
+    }
+
+    public override void ToggleVisuals(bool active)
+        => inputField.enabled = active;
+
+    public override void ToggleVisuals()
+        => inputField.enabled = !inputField.enabled;
+
+    protected override void SubscribeToEvents()
+    {
+        inputField.onEndEdit.AddListener(UpdateData);
+    }
+
+    protected override void UnsubscribeFromEvents()
+    {
+        inputField.onEndEdit.RemoveListener(UpdateData);
+    }
+
+    private void UpdateData(string stringVal)
+    {
+        if (targetData is StringVariable stringData) //string
+        {
+            stringData.Value = stringVal;
+        }
+        else if (targetData is BoolVariable boolData) //bool
+        {
+            if (stringVal.TryStringToBool(out bool boolVal))//if value parsed correctly
+                boolData.Value = boolVal;
+        }
+        else if (targetData is IntVariable intData) //int
+        {
+            if (int.TryParse(stringVal, out int intVal)) //attempt to parse
+                intData.Value = intVal;
+        }
+        else if (targetData is FloatVariable floatData) //float
+        {
+            if (float.TryParse(stringVal, out float floatVal)) //attempt to parse
+                floatData.Value = floatVal;//should be valid because of inputfield constraints
+        }
+        else
+        {
+            Debug.LogWarningFormat("[InputFieldToScriptable] Scriptable {0} " +
+                "does not have a supported type {1}. Try bool, int, float, string.",
+                targetData.name, targetData.Type);
+        }
+    }
+}
