49c2130d11a75611b828a76d4fcf20c9a6003df0
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2021-05-26 00:57:58 -0700

    new WeightedScriptable and Generator

---
+++ _RichScripts/Random Generators/RandomScriptableGenerator.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Generates a random ScriptableObject from a list of weights.
+/// </summary>
+[CreateAssetMenu(fileName = "RandomScriptableGen",
+    menuName = "ScriptableObjects/Random Generators/ScriptableObject")]
+public class RandomScriptableGenerator 
+    : ARandomGeneratorBase<WeightedScriptable, ScriptableObject>
+{
+	//exists
+}
---
+++ _RichScripts/Weighted Random/WeightedScriptable.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+/// <summary>
+/// Weight and ScriptableObject
+/// </summary>
+[Serializable]
+public class WeightedScriptable : AWeightedProbability<ScriptableObject>
+{
+    //exists
+
+    public WeightedScriptable(int weight, ScriptableObject value)
+        : base(weight, value)
+    {
+        //constructs
+    }
+}
