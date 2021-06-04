4153bffeebd5455e6e942e7060f0e0d255a2cbc1
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2021-06-04 14:43:13 -0700

    new weighted class

---
+++ _RichScripts/Weighted Random/WeightedSprite.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+/// <summary>
+/// 
+/// </summary>
+[Serializable]
+public class WeightedSprite : AWeightedProbability<Sprite>
+{
+    //exists
+
+    #region Constructors
+
+    public WeightedSprite(int weight, Sprite value)
+        : base(weight, value)
+    {
+
+    }
+
+    #endregion
+}
