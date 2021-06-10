bb18700f53fe2f789d27aa81c5e8d6c5da6237b6
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2021-06-10 03:32:27 -0700

    better extensions

--- _RichScripts/Extensions/Vector3_Extensions.cs
+++ _RichScripts/Extensions/Vector3_Extensions.cs
@@ -23,6 +23,16 @@ public static class Vector3_Extensions
             y.HasValue ? y.Value : a.y,
             z.HasValue ? z.Value : a.z);
     }
+
+    public static Vector3 WithX(this Vector3 a, float x)
+        => new Vector3(x, a.y, a.z );
+
+    public static Vector3 WithY(this Vector3 a, float y)
+        => new Vector3(a.x, y, a.z);
+
+    public static Vector3 WithZ(this Vector3 a, float z)
+        => new Vector3(a.x, a.y, z);
+
     //can be called as Vector3_Extensions.With(a, ....), but that's ugly, so the 'this'
     //keyword allows you to call the method from an instance, like a.With(...);
 
