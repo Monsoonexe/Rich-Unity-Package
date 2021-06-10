e7477a07a77c40fdad6dc3b3a79e6c66c6818561
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2021-06-10 03:32:18 -0700

    new color extension methods

---
+++ _RichScripts/Extensions/Color_Extensions.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+
+public static class Color_Extensions
+{
+    /// <summary>
+    /// Handy function to make assigning/copying vectors easier
+    /// myPostion = somePosition.With(z = 1);//same, but with different z-value
+    /// </summary>
+    /// <param name="a">Vector3 source to copy</param>
+    /// <param name="x">x value, if you want</param>
+    /// <param name="y">y value, if you want</param>
+    /// <param name="z">z value, if you want</param>
+    /// <remarks>But seriously, 4 conditionals????</remarks>
+    /// <returns></returns>
+    public static Color With(this Color c,
+        float? r = null, float? g = null, float? b = null,
+        float? a = null)
+    {
+        return new Color
+        (
+            r.HasValue ? r.Value : c.r,
+            g.HasValue ? g.Value : c.g,
+            b.HasValue ? b.Value : c.b,
+            a.HasValue ? a.Value : c.a
+        );
+    }
+
+    public static Color WithR(this Color c, float r)
+        => new Color(r, c.g, c.b, c.a);
+
+    public static Color WithG(this Color c, float g)
+        => new Color(c.r, g, c.b, c.a);
+
+    public static Color WithB(this Color c, float b)
+        => new Color(c.r, c.g, b, c.a);
+
+    public static Color WithA(this Color c, float a)
+        => new Color(c.r, c.g, c.b, a);
+
+    public static void MakeOpaque(ref this Color c)
+        => c.a = 1;
+
+    public static void MakeTransparent(ref this Color c)
+        => c.a = 0;
+
+    //can be called as Vector3_Extensions.With(a, ....), but that's ugly, so the 'this'
+    //keyword allows you to call the method from an instance, like a.With(...);
+
+    //following two lines are equivalent.
+    //Vector3 myVec = new Vector3(myTransform.position.x, 150, myTransform.position.z);
+
+    //Vector3 otherVec = myTransform.position.With(y: 150);
+}
