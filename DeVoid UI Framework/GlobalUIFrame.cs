aeec3e95f7a3006dc61194f14e1219ca97ebdad9
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2021-06-15 13:17:52 -0700

    new hlper

---
+++ DeVoid UI Framework/GlobalUIFrame.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// 
+/// </summary>
+public class GlobalUIFrame : RichMonoBehaviour
+{
+    [SerializeField]
+    protected UISettings uiSettings;
+
+    public static UIFrame UIFrame { get; private set; }
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        InitializeUI();
+    }
+
+    protected void InitializeUI()
+    {
+        UIFrame = uiSettings.CreateUIInstance();
+    }
+}
