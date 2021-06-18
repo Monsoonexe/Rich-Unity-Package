5a63535fde8ed08316ee2a1dbe3740ab09ec6613
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2021-06-18 12:36:23 -0700

    added 'with' extensions

--- _RichScripts/Extensions/Vector2_Extensions.cs
+++ _RichScripts/Extensions/Vector2_Extensions.cs
@@ -10,4 +10,10 @@ public static class Vector2_Extensions
 	/// </summary>
     public static float RandomRange(this Vector2 range)
         => Random.Range(range.x, range.y);
+
+    public static Vector2 WithX(this Vector2 a, float x)
+        => new Vector2(x, a.y);
+
+    public static Vector2 WithY(this Vector2 a, float y)
+        => new Vector2(a.x, y);
 }
