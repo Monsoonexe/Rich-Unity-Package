fe61f4a7935c6ac151898e7129e246d9351f85e1
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2021-06-20 13:28:01 -0700

    added ability to subscribe to get Inverse Value

--- ThirdParty/SO Architecture - Fork/Variables/BoolVariable.cs
+++ ThirdParty/SO Architecture - Fork/Variables/BoolVariable.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
 
 namespace ScriptableObjectArchitecture
 {
@@ -8,8 +10,34 @@ namespace ScriptableObjectArchitecture
         order = SOArchitecture_Utility.ASSET_MENU_ORDER_COLLECTIONS + 5)]
     public sealed class BoolVariable : BaseVariable<bool>
     {
-        public bool InvertedValue { get => !Value; }
+        private readonly List<Action<bool>> _invertedActions 
+            = new List<Action<bool>>();
 
+        public override object BaseValue
+        {
+            set
+            {
+                base.BaseValue = value;
+                RaiseInverted(!_value);
+            }
+        }
+        public void AddInvertedListener(Action<bool> action)
+        {
+            if (!_invertedActions.Contains(action))
+                _invertedActions.Add(action);
+        }
+        public void RemoveInvertedListener(Action<bool> action)
+        {
+            if (!_invertedActions.Contains(action))
+                _invertedActions.Remove(action);
+        }
+        public void RaiseInverted(bool invertedValue)
+        {
+            for (var i = _invertedActions.Count - 1; i >= 0; --i)
+                _invertedActions[i].Invoke(invertedValue);
+        }
+
+        public bool InvertedValue { get => !Value; }
         public void InvertValue() => Value = !Value;
     } 
 }
