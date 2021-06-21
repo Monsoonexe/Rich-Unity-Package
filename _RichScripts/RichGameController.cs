1351d0ff9321e89038a0fef470962b5238dd3ae9
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2021-06-21 03:11:38 -0700

    generic game controller

---
+++ _RichScripts/RichGameController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using Signals;
+
+/// <summary>
+/// 
+/// </summary>
+public class RichGameController : RichMonoBehaviour
+{
+    protected override void Awake()
+    {
+        GlobalSignals.Get<RequestQuitGameSignal>().AddListener(QuitGame);
+    }
+
+    private void OnDestroy()
+    {
+        GlobalSignals.Get<RequestQuitGameSignal>().RemoveListener(QuitGame);
+    }
+
+    private void OnEnable()
+    {
+        
+    }
+
+    public void QuitGame()
+    {
+        GlobalSignals.Get<GameIsQuittingSignal>().Dispatch();
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+        Application.Quit();
+    }
+
+    public static void ReloadCurrentLevel()
+        => SceneManager.LoadScene(
+            SceneManager.GetActiveScene().name);
+
+}
+
+/// <summary>
+/// Request that the app be closed.
+/// </summary>
+public class RequestQuitGameSignal : ASignal { }
+
+/// <summary>
+/// Dispatched when the Player has requested to close the app.
+/// </summary>
+public class GameIsQuittingSignal : ASignal { }
