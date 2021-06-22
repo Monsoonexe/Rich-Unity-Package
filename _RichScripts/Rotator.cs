9ebfae3f842a4c615205ad902beb0dde0888bfa8
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2021-06-21 17:22:25 -0700

    new rotator behaviour

---
+++ _RichScripts/Rotator.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace PolygonPilgrimage.BattleRoyaleKit
+{
+    public class Rotator : RichMonoBehaviour
+    {
+        public Vector3 rotateVector;
+        public Space relativeSpace = Space.World;
+
+        // Update is called once per frame
+        void Update()
+        {
+            transform.Rotate(rotateVector * Time.deltaTime,
+                relativeSpace);
+        }
+    }
+
+}
