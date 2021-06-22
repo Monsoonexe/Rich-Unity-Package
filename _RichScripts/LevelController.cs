5312fbecb5d1c7d531e2fd5dbe27871cc832d29d
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2021-06-21 23:16:04 -0700

    new script to rig change scene events to

---
+++ _RichScripts/LevelController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine.SceneManagement;
+using ScriptableObjectArchitecture;
+
+/// <summary>
+/// 
+/// </summary>
+public class LevelController : RichMonoBehaviour
+{
+    public static int currentSceneIndex = 0;
+
+    //member functions for events to hook onto.
+
+    public void LoadLevel(SceneVariable sceneVariable)
+        => SceneManager.LoadScene(sceneVariable.Value.SceneIndex);
+
+    public void LoadLevel(string name)
+        => SceneManager.LoadScene(name);
+
+    public void LoadLevel(int index)
+        => SceneManager.LoadScene(index);
+
+    public void LoadNextLevel_()
+        => LoadNextLevel();//forward call to static member
+
+    public void LoadPreviousLevel_()
+        => LoadPreviousLevel();//forward call to static member
+
+    public void ReloadCurrentLevel_()
+        => ReloadCurrentLevel();//forward call to static member
+
+    public static void LoadNextLevel()
+    {
+        ++currentSceneIndex;
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+
+    public static void LoadPreviousLevel()
+    {
+        --currentSceneIndex;
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+
+    public static void ReloadCurrentLevel()
+        => SceneManager.LoadScene(
+            SceneManager.GetActiveScene().name);
+}
