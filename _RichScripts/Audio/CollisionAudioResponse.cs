2251a53569156159c5e98ba9cd642c65d29dc0a9
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2021-06-24 00:33:13 -0700

    new way to respond to collision with a sound adjusted by force ofimpact
    kind of works.

---
+++ _RichScripts/Audio/CollisionAudioResponse.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using ScriptableObjectArchitecture;
+
+/// <summary>
+/// Plays a sound when collided against.
+/// </summary>
+public class CollisionAudioResponse : RichMonoBehaviour
+{
+    [Header("---Settings---")]
+    [SerializeField]
+    private float maxForceFactor = 0.25f;
+
+    [Header("---Audio---")]
+    [SerializeField]
+    private AudioClipReference collisionClip = new AudioClipReference();
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        var options = collisionClip.Options; //override option's volume
+        options.volume *= (collision.relativeVelocity.magnitude 
+            * maxForceFactor); //volume relative to impact force
+        collisionClip.PlaySFX(options);
+    }
+}
