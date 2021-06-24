cdd9081db6a5ff72eb4b465b10be3a60a837c72a
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2021-06-24 14:37:21 -0700

    new script that exposes Time.timeScale to a FloatVariable

---
+++ _RichScripts/TimeScaleModifier.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using ScriptableObjectArchitecture;
+
+/// <summary>
+/// Sets the timeScale.
+/// </summary>
+public class TimeScaleModifier : RichMonoBehaviour
+{
+    [SerializeField]
+    private FloatVariable timeScale;
+
+    /// <summary>
+    /// Setting this value updates Time.timeScale immediately.
+    /// </summary>
+    public float TimeScale
+    {
+        get => timeScale;
+        set => timeScale.Value = value;
+    }
+
+    private void Start()
+    {
+        UpdateTimeScale(timeScale);
+    }
+
+    private void OnEnable()
+    {
+        timeScale.AddListener(UpdateTimeScale);
+    }
+
+    private void OnDisable()
+    {
+        timeScale.RemoveListener(UpdateTimeScale);
+    }
+
+    public void UpdateTimeScale(float newScale)
+    {
+        Time.timeScale = newScale;
+    }
+}
