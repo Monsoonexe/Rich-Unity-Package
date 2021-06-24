d79bd5e5c1c1557cd51dbff293048f8525c76d99
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2021-06-24 00:30:15 -0700

    new class to be  "button" but in world space

---
+++ _RichScripts/Events/WorldButton.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+/// <summary>
+/// Forwards mouse events to UnityEvents.
+/// </summary>
+[RequireComponent(typeof(Collider))]
+public class WorldButton : RichMonoBehaviour
+{
+    public UnityEvent onMouseDownEvent = new UnityEvent();
+
+    public UnityEvent onMouseUpAsButtonEvent = new UnityEvent();
+
+    public UnityEvent onMouseEnterEvent = new UnityEvent();
+
+    public UnityEvent onMouseExitEvent = new UnityEvent();
+
+    public void OnMouseDown()
+        => onMouseDownEvent.Invoke();
+
+    public void OnMouseUpAsButton()
+        => onMouseUpAsButtonEvent.Invoke();
+
+    public void OnMouseEnter()
+        => onMouseEnterEvent.Invoke();
+
+    public void OnMouseExit()
+        => onMouseExitEvent.Invoke();
+}
