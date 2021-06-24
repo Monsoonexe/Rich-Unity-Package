bdde27d5681554a3c7e074922d68efeaa3c7b7f7
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2021-06-24 00:29:39 -0700

    Update AudioClipReference_Extensions.cs

--- _RichScripts/Audio/AudioClipReference_Extensions.cs
+++ _RichScripts/Audio/AudioClipReference_Extensions.cs
@@ -7,4 +7,11 @@ public static class AudioClipReference_Extensions
     /// </summary>
     public static AudioID PlaySFX(this AudioClipReference r)
         => AudioManager.PlaySFX(r.Value, r.Options);
+
+    /// <summary>
+    /// Play the AudioClip with the given AudioOptions.
+    /// </summary>
+    public static AudioID PlaySFX(this AudioClipReference r,
+        AudioOptions options)
+        => AudioManager.PlaySFX(r.Value, options);
 }
