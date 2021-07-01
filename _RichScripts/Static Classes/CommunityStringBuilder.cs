5c9dfaa1d168742df79522ee66b39bcbb96a7b0c
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2021-07-01 02:00:17 -0700

    new community string builder class
    to declutter Utility.cs

---
+++ _RichScripts/Static Classes/CommunityStringBuilder.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+
+/// <summary>
+/// 
+/// </summary>
+public static class CommunityStringBuilder
+{
+    private static readonly StringBuilder communityStringBuilder = new StringBuilder(10);
+
+    /// <summary>
+    /// Community String Builder (so you don't have to 'new' one).
+    /// Just don't bet it will hold its data. Always safe to use right away.
+    /// </summary>
+    public static StringBuilder Instance
+    {
+        get
+        {
+            communityStringBuilder.Clear(); // clear so it's safe to sue
+            return communityStringBuilder;
+        }
+    }
+
+    #region Constructors
+
+
+
+    #endregion
+}
