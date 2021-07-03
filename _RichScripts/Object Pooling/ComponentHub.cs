b438c6498fb93df526185ab159c0cc8351ef02d4
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2021-07-03 02:33:38 -0700

    new components for hubbing components and game objects

---
+++ _RichScripts/Object Pooling/ComponentHub.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// A locator for objects that add themselves to hub.
+/// </summary>
+public class ComponentHub<T> : RichMonoBehaviour
+    where T : Component
+{
+    private readonly Dictionary<string, T>
+        componentTable = new Dictionary<string, T>(); //lol. it's not what you think
+
+    public T Get(string key)
+    {
+        T component = null;
+
+        if (!componentTable.TryGetValue(key, out component))
+        {
+            Debug.LogError("[ComponentHub] Component <" + key + "> not found." +
+                " Add this to the hub prior to access.");
+        }
+
+        return component;
+    }
+
+    public U Get<U>(string key) 
+        where U : Component
+        => Get(key) as U;
+
+    /// <param name="newComponent"></param>
+    public void Add(string key, T newComponent)
+        => componentTable.Add(key, newComponent);
+
+    /// <summary>
+    /// Keyed by name.
+    /// </summary>
+    public void Add(T newComponent)
+        => componentTable.Add(newComponent.name, newComponent);
+
+    /// <summary>
+    /// Keyed by name.
+    /// </summary>
+    public void Remove(T newComponent)
+        => componentTable.Remove(newComponent.name);
+
+    public void Remove(string key)
+        => componentTable.Remove(key);
+}
+
+/// <summary>
+/// A locator for objects that add themselves to hub.
+/// </summary>
+public class ComponentHub : ComponentHub<Component>
+{
+
+}
---
+++ _RichScripts/Object Pooling/GameObjectHub.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 
+/// </summary>
+public class GameObjectHub : RichMonoBehaviour
+{
+    private readonly Dictionary<string, GameObject>
+        poolTable = new Dictionary<string, GameObject>(); //lol. it's not what you think
+
+    public GameObject Get(string key)
+    {
+        GameObject obj = null;
+
+        if (!poolTable.TryGetValue(key, out obj))
+        {
+            Debug.LogError("[PoolHub] Pool <" + key + "> not found." +
+                " Add this to the pool prior to access.");
+        }
+
+        return obj;
+    }
+
+    public U Get<U>(string key) 
+        where U : Component
+    {
+        Component comp = null;
+
+        var obj = Get(key);
+        if (obj != null)
+            comp = obj.GetComponent<U>();
+
+        return comp as U;
+    }
+
+    public void Add(string key, GameObject newObj)
+        => poolTable.Add(key, newObj);
+
+    /// <summary>
+    /// Keyed by name.
+    /// </summary>
+    public void Add(GameObject newObj)
+        => poolTable.Add(newObj.name, newObj);
+
+    /// <summary>
+    /// Keyed by name.
+    /// </summary>
+    public void Remove(GameObject newObj)
+        => poolTable.Remove(newObj.name);
+
+    public void Remove(string key)
+        => poolTable.Remove(key);
+}
