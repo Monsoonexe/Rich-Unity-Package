913fbfd2ec7e7aaf660c32f8303563b980bfb37f
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2021-07-06 20:17:51 -0700

    new events

---
+++ _RichScripts/Events/AManagedEvent.cs
@@ -0,0 +1,21 @@
+﻿
+namespace RichPackage.Managed
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public abstract class AManagedEvent : AUnityLifetimeEvent
+    {
+        ////override this because specific is better than vague for this system.
+        //protected virtual void OnEnable()
+        //{
+        //    ManagedBehaviourEngine.AddManagedListener(this);
+        //}
+
+        ////override this because specific is better than vague for this system.
+        //protected virtual void OnDisable()
+        //{
+        //    ManagedBehaviourEngine.RemoveManagedListener(this);
+        //}
+    }
+}
---
+++ _RichScripts/Events/ManagedUpdateEvent.cs
@@ -0,0 +1,29 @@
+﻿
+namespace RichPackage.Managed
+{
+    /// <summary>
+    /// Raises UnityEvent on ManagedUpdate(). Rig it in Inspector.
+    /// </summary>
+    /// <seealso cref="UpdateUnityEvent"/>
+    public sealed class ManagedUpdateEvent : AManagedEvent,
+        IManagedUpdate
+    {
+        //override because specific is better than vague for this system.
+        private void OnEnable()
+        {   //cast because specific is faster
+            ManagedBehaviourEngine.AddManagedListener((IManagedUpdate)this);
+        }
+
+        //override because specific is better than vague for this system.
+        private void OnDisable()
+        {   //cast because specific is faster
+            ManagedBehaviourEngine.RemoveManagedListener((IManagedUpdate)this);
+        }
+
+        public void ManagedUpdate()
+        {
+            lifetimeEvent.Invoke();
+        }
+    }
+
+}
