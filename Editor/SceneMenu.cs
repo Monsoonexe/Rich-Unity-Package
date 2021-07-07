d03437b1a0f57ea19a117d9d3acdfd72648337b0
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2021-07-06 23:25:04 -0700

    added save prompt and namespace to protect against depending on.

--- Editor/SceneMenu.cs
+++ Editor/SceneMenu.cs
@@ -1,21 +1,35 @@
 ﻿using UnityEditor;
 using UnityEditor.SceneManagement;
 using UnityEngine.SceneManagement;
+using ScriptableObjectArchitecture;
 
-public static class SceneMenu
+namespace RichPackage.Editor
 {
-    [MenuItem("Scenes/Reload Current Scene")]
-    public static void ReloadCurrentScene()
-        => LoadScene(SceneManager.GetActiveScene());
+    public static class SceneMenu
+    {
+        [MenuItem("Scenes/Reload Current Scene")]
+        public static void ReloadCurrentScene()
+            => LoadScene(SceneManager.GetActiveScene());
 
-    public static void LoadScene(Scene scene)
-        => LoadScene(scene.path);
+        public static void LoadScene(Scene scene)
+            => LoadScene(scene.path);
 
-    public static void LoadScene(string scenePath)
-        => EditorSceneManager.OpenScene(scenePath);
+        public static void LoadScene(string scenePath)
+        {
+            //prompt to save
+            EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
 
-    //e.g. 
-    //[MenuItem("Scenes/Start Menu Scene")]
-    //private static void LoadStartMenuScene()
-    //   => LoadScene("Assets/Scenes/StartMenu.unity");
+            //actually change scenes
+            EditorSceneManager.OpenScene(scenePath);
+        }
+
+        public static void LoadScene(SceneVariable sceneVar)
+            => LoadScene(sceneVar.Value.SceneName);
+
+        //----------EXAMPLE ENTRY-----------
+        //[MenuItem("Scenes/Start Menu Scene")]
+        //private static void LoadStartMenuScene()
+        //   => LoadScene("Assets/Scenes/StartMenu.unity");
+    }
+
 }
