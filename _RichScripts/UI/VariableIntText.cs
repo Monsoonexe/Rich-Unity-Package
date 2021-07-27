0adfd010e58c8b15d58a90feed1d67fa213c0777
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2021-07-27 01:42:01 -0700

    new text element to specificaly display ints

---
+++ _RichScripts/UI/VariableIntText.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using TMPro;
+using ScriptableObjectArchitecture;
+
+/// <summary>
+/// Displays an IntVariable's value with a TMP.
+/// </summary>
+/// <seealso cref="VariableTextUIController"/>
+public class VariableIntText : RichUIElement<IntVariable>
+{
+    /*TODO 
+     * Configuration: just show number
+     * Config: cur / max
+     * Config: min / cur / max
+     */
+
+    [Header("---Scene Refs---")]
+    [SerializeField]
+    [Tooltip("Update this text element with target data.")]
+    protected TextMeshProUGUI textElement;
+    
+    private void Reset()
+    {
+        textElement = GetComponent<TextMeshProUGUI>();
+    }
+
+    protected override void SubscribeToEvents()
+    {
+        targetData.AddListener(UpdateUI);
+    }
+
+    protected override void UnsubscribeFromEvents()
+    {
+        targetData.RemoveListener(UpdateUI);
+    }
+
+    public override void UpdateUI()
+    {
+        textElement.text = ConstStrings.GetCachedString(targetData);
+    }
+
+}
