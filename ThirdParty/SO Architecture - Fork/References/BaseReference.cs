6ff7b154ac2143f2eaddbc9efe210144cbcaf914
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2021-07-29 12:45:23 -0700

    exposed UseConstant property

--- ThirdParty/SO Architecture - Fork/References/BaseReference.cs
+++ ThirdParty/SO Architecture - Fork/References/BaseReference.cs
@@ -14,6 +14,7 @@ namespace ScriptableObjectArchitecture
 
         [SerializeField]
         protected bool _useConstant = false;
+        public bool UseConstant { get => _useConstant; }
         [SerializeField]
         protected TBase _constantValue = default(TBase);
         [SerializeField]
