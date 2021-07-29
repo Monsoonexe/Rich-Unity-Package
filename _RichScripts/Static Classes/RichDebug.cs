5843fbbae1071923a9f41a5ba4a6afacb093b883
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2021-07-29 03:40:40 -0700

    moved out of editor folder

--- _RichScripts/Static Classes/RichDebug.cs
+++ _RichScripts/Static Classes/RichDebug.cs
@@ -11,10 +11,12 @@ namespace RichPackage.Debug
     /// </summary>
     public static class RichDebug
     {
+        public const string UNITY_EDITOR = "UNITY_EDITOR";
+
         /// <summary>
         /// Only Logs in Editor. Removed from Builds.
         /// </summary>
-        [Conditional("UNITY_EDITOR")]
+        [Conditional(UNITY_EDITOR)]
         public static void EditorLog(string message)
         {
             Debug.Log(message);
@@ -23,7 +25,7 @@ namespace RichPackage.Debug
         /// <summary>
         /// Only Logs in Editor. Removed from Builds.
         /// </summary>
-        [Conditional("UNITY_EDITOR")]
+        [Conditional(UNITY_EDITOR)]
         public static void EditorLog(string message, UnityEngine.Object context)
         {
             Debug.Log(message, context);
@@ -31,12 +33,27 @@ namespace RichPackage.Debug
 
         /// <summary>
         /// This call gets stripped when built.
-        /// Do not put any mutable code in here or you might get inconsistent
+        /// Do not put any mutation code in here or you might get inconsistent
         /// results in your build.
         /// </summary>
         /// <param name="editorAction"></param>
-        [Conditional("UNITY_EDITOR")]
+        [Conditional(UNITY_EDITOR)]
         public static void OnlyDoInEditor(Action editorAction)
             => editorAction();
+
+        [Conditional(UNITY_EDITOR)]
+        public static void AssertNotNull<T>(T someRef)
+            where T : class
+            => Debug.AssertFormat(someRef != null,
+                "Reference not set for: {0} ",
+                typeof(T).Name);
+
+        [Conditional(UNITY_EDITOR)]
+        public static void AssertMyRefNotNull<T, U>(T caller, U someRef)
+            where T : class
+            => Debug.AssertFormat(someRef != null,
+                "[{0}] Reference not set for: {1} ",
+                caller.GetType().Name, //caller's name
+                typeof(T).Name); //ref's name
     }
 }
