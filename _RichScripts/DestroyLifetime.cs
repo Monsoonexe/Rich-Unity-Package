3b3a3978be938389f9205e10dac36c4aff30e9bd
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2021-08-01 15:49:25 -0700

    added dev description and min attribute

--- _RichScripts/DestroyLifetime.cs
+++ _RichScripts/DestroyLifetime.cs
@@ -1,10 +1,18 @@
 ﻿using UnityEngine;
-using NaughtyAttributes;
 
+/// <summary>
+/// Destroys parent GameObject after given time.
+/// </summary>
 public class DestroyLifetime : RichMonoBehaviour
 {
-    [MinValue(0)]
+    [Header("---Settings---")]
+    [Min(0)]
     public float lifeTime = 10;
+
+    private void Reset()
+    {
+        SetDevDescription("Destroys parent GameObject after given time.");
+    }
 
     private void Start()
     {
