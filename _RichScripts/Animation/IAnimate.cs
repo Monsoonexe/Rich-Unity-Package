1d6994c607cdc4bb9bcc9bd4d3068faac6016eb4
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2021-08-05 19:10:50 -0700

    new folder for animation-like objects

---
+++ _RichScripts/Animation/IAnimate.cs
@@ -0,0 +1,18 @@
+﻿public interface IAnimateBase
+{
+    bool IsAnimating { get; }
+    void Stop();
+}
+
+/// <summary>
+/// 
+/// </summary>
+public interface IAnimate : IAnimateBase
+{
+    void Play();
+}
+
+public interface IAnimate<T> : IAnimateBase
+{
+    void Play(T puppet);
+}
