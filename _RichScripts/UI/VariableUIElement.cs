f5bb2f4988b1e26ec3a38bbd1e9fb923971ba796
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2021-08-18 17:08:41 -0700

    new class

---
+++ _RichScripts/UI/VariableUIElement.cs
@@ -0,0 +1,16 @@
+﻿using ScriptableObjectArchitecture;
+
+/// <summary>
+/// Class that shows data based on a BaseVariable.
+/// </summary>
+/// <typeparam name="T"></typeparam>
+[SelectionBase]
+public abstract class VariableUIElement<T> : RichUIElement<T>
+    where T : BaseVariable
+{
+    protected override void SubscribeToEvents()
+        => targetData.AddListener(UpdateUI);
+
+    protected override void UnsubscribeFromEvents()
+        => targetData.RemoveListener(UpdateUI);
+}
