f20a7fef13afb627e321e0b07761f405fb4dca3a
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2021-08-18 17:08:09 -0700

    new UI class

---
+++ _RichScripts/UI/BoolDisplayer.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using TMPro;
+using ScriptableObjectArchitecture;
+using NaughtyAttributes;
+
+[SelectionBase]
+public class BoolDisplayer : RichUIElement<BoolVariable>
+{
+    [Header("---Settings---")]
+    public string trueMessage = "On";
+    public string falseMessage = "Off";
+
+    [Header("---Prefab Refs---")]
+    [SerializeField]
+    [Required]
+    private TextMeshProUGUI textGUI;
+
+    private void Reset()
+    {
+        SetDevDescription("I show a string based on a boolean value.");
+    }
+    protected override void SubscribeToEvents()
+    {
+        targetData.AddListener(UpdateUI);
+    }
+    protected override void UnsubscribeFromEvents()
+    {
+        targetData.RemoveListener(UpdateUI);
+    }
+    public override void UpdateUI()
+    {
+        //determine which message to print.
+        var outString = string.Empty;
+        if (targetData.Value == true)
+            outString = trueMessage;
+        else
+            outString = falseMessage;
+        textGUI.text = outString;
+    }
+}
