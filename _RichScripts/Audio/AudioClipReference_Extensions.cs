1a3c90f7c245109ff52ed4d2f9a728c1464a4956
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2021-08-25 16:01:11 -0700

    added bgm extensions

--- _RichScripts/Audio/AudioClipReference_Extensions.cs
+++ _RichScripts/Audio/AudioClipReference_Extensions.cs
@@ -5,6 +5,19 @@ public static class AudioClipReference_Extensions
     /// <summary>
     /// Play the AudioClip with the attached AudioOptions.
     /// </summary>
+    public static AudioID PlayBGM(this AudioClipReference r)
+        => AudioManager.PlayBackgroundTrack(r.Value, r.Options);
+
+    /// <summary>
+    /// Play the AudioClip with the given AudioOptions.
+    /// </summary>
+    public static AudioID PlayBGM(this AudioClipReference r,
+        AudioOptions options)
+        => AudioManager.PlayBackgroundTrack(r.Value, options);
+        
+    /// <summary>
+    /// Play the AudioClip with the attached AudioOptions.
+    /// </summary>
     public static AudioID PlaySFX(this AudioClipReference r)
         => AudioManager.PlaySFX(r.Value, r.Options);
 
