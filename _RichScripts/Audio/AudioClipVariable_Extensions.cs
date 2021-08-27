3d6814a89ade5010f99e253652ea5dff6a0bc359
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2021-08-27 16:02:21 -0700

    new extensions

--- _RichScripts/Audio/AudioClipVariable_Extensions.cs
+++ _RichScripts/Audio/AudioClipVariable_Extensions.cs
@@ -8,6 +8,26 @@ public static class AudioClipVariable_Extensions
     /// <summary>
     /// Play the AudioClip with the attached AudioOptions.
     /// </summary>
-    public static AudioID PlaySFX(this AudioClipVariable r)
-        => AudioManager.PlaySFX(r.Value, r.Options);
+    public static AudioID PlayBGM(this AudioClipVariable r)
+        => AudioManager.PlayBackgroundTrack(r.Value, r.Options);
+
+    /// <summary>
+    /// Play the AudioClip with the given AudioOptions.
+    /// </summary>
+    public static AudioID PlayBGM(this AudioClipVariable r,
+        AudioOptions options)
+        => AudioManager.PlayBackgroundTrack(r.Value, options);
+        
+    /// <summary>
+    /// Play the AudioClip with the attached AudioOptions.
+    /// </summary>
+    public static AudioID PlaySFX(this AudioClipVariable clip)
+        => AudioManager.PlaySFX(clip.Value, clip.Options);
+
+    /// <summary>
+    /// Play the AudioClip with the given AudioOptions.
+    /// </summary>
+    public static AudioID PlaySFX(this AudioClipVariable clip,
+        AudioOptions options)
+        => AudioManager.PlaySFX(clip.Value, options);
 }
