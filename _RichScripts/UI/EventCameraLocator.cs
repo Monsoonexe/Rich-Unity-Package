25265146deb4c29c0c05ea87c86b3ff6f7ebda17
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2021-09-29 13:26:00 -0700

    updated logic and added Reset()

--- _RichScripts/UI/EventCameraLocator.cs
+++ _RichScripts/UI/EventCameraLocator.cs
@@ -1,8 +1,8 @@
 ﻿using UnityEngine;
 
 /// <summary>
-/// Quick and dirty script to automatically link the Main camera to a World Canvas'
-/// world camera (event camera) property. Replace if there's a better Controller on this
+/// Quick and dirty script to automatically link the Main camera to a World Canvas's
+/// world camera (event camera) property. Replace if there's a better controller on this
 /// object that can do this.
 /// </summary>
 public class EventCameraLocator : RichMonoBehaviour
@@ -13,13 +13,34 @@ public class EventCameraLocator : RichMonoBehaviour
 	[Tooltip("[Optional] Canvas which needs an event camera.")]
 	public Canvas canvas;
 
-    protected override void Awake()
+	private void Reset()
+	{
+		SetDevDescription("Quick and dirty script to automatically " +
+			"link the Main camera to a World Canvas's " + 
+			"world camera(event camera) property. " +
+			"Replace if there's a better controller on this " + 
+			"object that can do this.");
+
+		//set defaults
+		if (canvas == null)
+			canvas = GetComponent<Canvas>();
+
+		if(worldEventCamera == null)
+			worldEventCamera = GameObject.FindGameObjectWithTag(
+				"MainCamera").GetComponent<Camera>();
+	}
+
+	protected void Start()
     {
     	if(canvas == null)
     		canvas = GetComponent<Canvas>();
 
-        canvas.worldCamera = worldEventCamera != null ? 
-        	worldEventCamera : GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+		if (worldEventCamera == null)
+			worldEventCamera = GameObject.FindGameObjectWithTag(
+				"MainCamera").GetComponent<Camera>();
+
+		canvas.worldCamera = worldEventCamera;
+
         Destroy(this);//my job is done!
     }
 }
