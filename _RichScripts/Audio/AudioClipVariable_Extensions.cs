3690b243546096165a0720382e03d9a72670a4b7
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2021-10-23 04:10:27 -0700

    Add DoPlay() variants for use with Action.

--- _RichScripts/Audio/AudioClipVariable_Extensions.cs
+++ _RichScripts/Audio/AudioClipVariable_Extensions.cs
@@ -5,23 +5,39 @@
 /// </summary>
 public static class AudioClipVariable_Extensions
 {
-    /// <summary>
-    /// Play the AudioClip with the attached AudioOptions.
-    /// </summary>
-    public static AudioID PlayBGM(this AudioClipVariable r)
-        => AudioManager.PlayBackgroundTrack(r.Value, r.Options);
+	/// <summary>
+	/// Play the AudioClip with the attached AudioOptions. Returns 'void', 
+	/// so good to use as Action.
+	/// </summary>
+	/// <param name="v"></param>
+	public static void DoPlayBGM(this AudioClipVariable v)
+		=> AudioManager.PlayBackgroundTrack(v.Value, v.Options);
+
+	/// <summary>
+	/// Play the AudioClip with the attached AudioOptions.
+	/// </summary>
+	public static AudioID PlayBGM(this AudioClipVariable v)
+        => AudioManager.PlayBackgroundTrack(v.Value, v.Options);
 
     /// <summary>
     /// Play the AudioClip with the given AudioOptions.
     /// </summary>
-    public static AudioID PlayBGM(this AudioClipVariable r,
+    public static AudioID PlayBGM(this AudioClipVariable v,
         AudioOptions options)
-        => AudioManager.PlayBackgroundTrack(r.Value, options);
-        
-    /// <summary>
-    /// Play the AudioClip with the attached AudioOptions.
-    /// </summary>
-    public static AudioID PlaySFX(this AudioClipVariable clip)
+        => AudioManager.PlayBackgroundTrack(v.Value, options);
+
+	/// <summary>
+	/// Play the AudioClip with the attached AudioOptions. Returns 'void', 
+	/// so good to use as Action.
+	/// </summary>
+	/// <param name="v"></param>
+	public static void DoPlaySFX(this AudioClipVariable v)
+		=> AudioManager.PlaySFX(v.Value, v.Options);
+
+	/// <summary>
+	/// Play the AudioClip with the attached AudioOptions.
+	/// </summary>
+	public static AudioID PlaySFX(this AudioClipVariable clip)
         => AudioManager.PlaySFX(clip.Value, clip.Options);
 
     /// <summary>
