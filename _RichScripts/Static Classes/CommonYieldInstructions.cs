8021867e56ba4df92a796ff61f1f12fe8c1f5b38
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2021-12-22 22:48:26 -0800

    Create CommonYieldInstructions.cs

---
+++ _RichScripts/Static Classes/CommonYieldInstructions.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace RichPackage
+{
+	public static class CommonYieldInstructions
+	{
+		public static readonly YieldInstruction WaitForEndOfFrame = new WaitForEndOfFrame();
+		public static readonly YieldInstruction WaitForFixedUpdate = new WaitForFixedUpdate();
+
+		public static readonly YieldInstruction WaitForTenthSecond = new WaitForSeconds(0.1f);
+		public static readonly YieldInstruction WaitForHalfSecond = new WaitForSeconds(0.5f);
+
+		public static readonly YieldInstruction WaitForOneSecond = new WaitForSeconds(1);
+		public static readonly YieldInstruction WaitForTwoSeconds = new WaitForSeconds(2);
+		public static readonly YieldInstruction WaitForThreeSecond = new WaitForSeconds(3);
+	}
+}
