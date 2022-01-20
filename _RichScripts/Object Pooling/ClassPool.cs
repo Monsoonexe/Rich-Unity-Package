303e96cd10808f45ba7bdca0f5d0a0c1688e0806
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2022-01-20 00:55:28 -0800

    Create ClassPool.cs

---
+++ _RichScripts/Object Pooling/ClassPool.cs
@@ -0,0 +1,47 @@
+using System.Collections.Generic;
+
+public class ClassPool<T> 
+    where T : class
+{
+    private Stack<T> pool;
+
+    /// <summary>
+    /// Can override this to provide a custom initialization for the pooled object.
+    /// </summary>
+    public Func<T> factory = () => new T(); //call default constructor
+
+    public int Count => pool.Count;
+
+    public int MaxCount { get; set; } = -1;
+
+    public ClassPool(int maxCount = -1)
+    {
+        MaxCount = maxCount;
+        int amount = MaxCount >= 0 ? MaxCount : 16;
+        pool = new Stack<T>(amount);
+    }
+
+    /// <summary>
+    /// Will always return an intance of T.
+    /// </summary>
+    public T Depool()
+    {
+        T depooledItem = null;
+
+        if (pool.Count > 0)
+            depooledItem = pool.Pop();
+        else
+            depooledItem = factory(); // create a new one
+
+        return depooledItem;
+    }
+
+    /// <summary>
+    /// Will only enpool if the pool is not full.
+    /// </summary>
+    public void Enpool(T item)
+    {
+        if(MaxCount < 0 || pool.Count < MaxCount)
+            pool.Push(item);
+    }
+}
