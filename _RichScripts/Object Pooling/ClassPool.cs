394f99d610dc00d2ec7a964e6e7877828b8ead4d
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2022-01-22 12:20:14 -0800

    Add Factory, Enpool, and Depool delegates to flow.

--- _RichScripts/Object Pooling/ClassPool.cs
+++ _RichScripts/Object Pooling/ClassPool.cs
@@ -14,15 +14,33 @@ namespace RichPackage
 		/// </summary>
 		public static readonly ClassPool<T> Shared = new ClassPool<T>();
 
-		private Stack<T> pool;
+		private readonly Stack<T> pool;
 
 		/// <summary>
-		/// Can override this to provide a custom initialization for the pooled object.
+		/// Can override this to provide a custom initialization for the pooled object. <br/>
+		/// Creates 'null' for reference types. 
 		/// </summary>
-		public Func<T> factory = () => default;// new T(); //call default constructor
+		public Func<T> FactoryMethod = () => default;// new T(); //call default constructor
+
+		/// <summary>
+		/// If not null, this function will be called when an Item is removed <br/>
+		/// from the pool.
+		/// </summary>
+		private Action<T> OnEnpoolMethod;
+
+		/// <summary>
+		/// If not null, this function will be called when an Item is added <br/>
+		/// back to the pool.
+		/// </summary>
+		private Action<T> OnDepoolMethod;
 
 		public int Count => pool.Count;
 
+		/// <summary>
+		/// Max count of items this pool will hold. If more than MaxCount items are
+		/// added to the pool, they will be dropped rather than enpooled. <br/>
+		/// MaxCount less than 1 implies there is no internal limit to the size of the pool.
+		/// </summary>
 		public int MaxCount { get; set; } = -1;
 
 		public ClassPool(int maxCount = -1)
@@ -33,27 +51,63 @@ namespace RichPackage
 		}
 
 		/// <summary>
-		/// Will always return an intance of T.
+		/// 
 		/// </summary>
+		/// <param name="maxCount">Max count of items this pool will hold. If more than MaxCount items are
+		/// added to the pool, they will be dropped rather than enpooled.<br/>
+		/// MaxCount less than 1 implies there is no internal limit to the size of the pool.</param>
+		/// <param name="preInit">Number of items to create up front.</param>
+		public ClassPool(int maxCount, int preInit)
+			: this(maxCount)
+		{
+			//prebake
+			for (int i = 0; i < preInit; ++i)
+				pool.Push(FactoryMethod());
+		}
+
+		public ClassPool(int maxCount, int preInit,
+			Func<T> factoryMethod, Action<T> enpoolMethod = null,
+			Action<T> depoolMethod = null)
+			: this(maxCount)
+		{
+			FactoryMethod = factoryMethod;
+			OnEnpoolMethod = enpoolMethod;
+			OnDepoolMethod = depoolMethod;
+
+			//prebake
+			for (int i = 0; i < preInit; ++i)
+				pool.Push(FactoryMethod());
+		}
+
+		/// <summary>
+		/// Will always return an intance of T.
+		/// Will call <see cref="OnDepoolMethod"/> if it is not null.
+		/// </summary>
+		/// <returns>A pooled item or one from the <seealso cref="FactoryMethod"/> if pool is empty.</returns>
 		public T Depool()
 		{
-			T depooledItem = default;
+			T item = default;
 
 			if (pool.Count > 0)
-				depooledItem = pool.Pop();
+				item = pool.Pop();
 			else
-				depooledItem = factory(); // create a new one
+				item = FactoryMethod(); // create a new one
 
-			return depooledItem;
+			OnDepoolMethod?.Invoke(item);
+			return item;
 		}
 
 		/// <summary>
 		/// Will only enpool if the pool is not full.
+		/// Will call <see cref="OnEnpoolMethod"/> if it is not null.
 		/// </summary>
 		public void Enpool(T item)
 		{
 			if (MaxCount < 0 || pool.Count < MaxCount)
+			{
 				pool.Push(item);
+				OnEnpoolMethod?.Invoke(item);
+			}
 		}
 	}
 }
