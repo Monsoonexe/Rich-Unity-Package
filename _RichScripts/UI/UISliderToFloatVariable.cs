6621586b5d54b05c7027bcc55b5067dc17ee50eb
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2022-01-22 15:22:25 -0800

    Create UISliderToFloatVariable.

---
+++ _RichScripts/UI/UISliderToFloatVariable.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Sirenix.OdinInspector;
+using ScriptableObjectArchitecture;
+
+//TODO - one for Floats, and another for Ints
+//TODO - Connect Max/MinClamp on SO to Slider.Min/Max
+//TODO - configure step based on Int/Float type.
+
+/// <summary>
+/// Synchronizes a Slider with a FloatVariable.
+/// </summary>
+[SelectionBase]
+public class UISliderToFloatVariable : RichUIElement<FloatVariable>
+{
+    [SerializeField, Required]
+    private Slider slider;
+
+	protected virtual void Reset()
+	{
+		SetDevDescription("Synchronizes a ScriptableObject with input from Slider.Value.");
+		slider = GetComponent<Slider>();
+	}
+
+	private void OnValidate()
+	{
+		ConfigureSliderToMatchVariable();
+	}
+
+	protected override void Awake()
+	{
+		base.Awake();
+		if(slider == null)
+			slider = GetComponent<Slider>();
+
+		ConfigureSliderToMatchVariable();
+		Debug.Assert(slider != null, "slider is not set.", this);
+	}
+
+	protected override void SubscribeToEvents()
+	{
+		slider.onValueChanged.AddListener(UpdateUI);
+	}
+
+	protected override void UnsubscribeFromEvents()
+	{
+		slider.onValueChanged.RemoveListener(UpdateUI);
+	}
+
+	[Button]
+	private void ConfigureSliderToMatchVariable()
+	{
+		if (slider == null || targetData == null) return;
+
+		slider.minValue = targetData.MinClampValue;
+		slider.maxValue = targetData.MaxClampValue;
+		slider.value = targetData.Value;
+
+#if UNITY_EDITOR
+		if (!Application.isPlaying)
+			UnityEditor.EditorUtility.SetDirty(slider);
+#endif
+	}
+
+	/// <summary>
+	/// Update the slider value to source value.
+	/// </summary>
+	[Button]
+	public override void UpdateUI()
+	{
+		slider.value = targetData.Value;
+	}
+
+	/// <summary>
+	/// Update source value with slider value.
+	/// </summary>
+	/// <param name="sliderValue"></param>
+	public void UpdateUI(float sliderValue)
+	{
+		targetData.Value = sliderValue;
+	}
+}
