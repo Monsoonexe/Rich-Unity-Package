ce8adcedc8ef6fc8b214946c818139012a1773a9
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2022-01-23 00:27:55 -0800

    More flexible formatting with string.Format()

--- _RichScripts/UI/FormattableIntUIElement.cs
+++ _RichScripts/UI/FormattableIntUIElement.cs
@@ -9,187 +9,64 @@ using Sirenix.OdinInspector;
 [SelectionBase]
 public class FormattableIntUIElement : RichUIElement<IntVariable>
 {
-    private enum EFormat
-    {
-        /// <summary>
-        /// just show current value
-        /// </summary>
-        Single = 0,
+	[Header("---Settings---")]
+	[Tooltip("string.Format(format, Min, Val, Max), so Min = {0}, Val = {1}, and Max = {2}.")]
+	public string formatString = "{0} | {1} | {2}";
 
-        /// <summary>
-        /// curr / max
-        /// </summary>
-        Curr_Max = 1,
+	[Header("---Prefab Refs---")]
+	[SerializeField, Required]
+	private TextMeshProUGUI readoutUIElement;
 
-        /// <summary>
-        /// min / curr / max
-        /// </summary>
-        Min_Curr_Max = 2,
+	//cached strings for readonly min/max
+	private string minString;
+	private string maxString;
 
-        /// <summary>
-        /// min / curr / max
-        /// </summary>
-        Max_Curr_Min = 3,
+	private void Reset()
+	{
+		SetDevDescription("I display text. E.g. {0} % \t {0} | {1} | {2} \t {1} credits \t {1:n} \t {#,##0}.");
+		readoutUIElement = GetComponent<TextMeshProUGUI>(); //assume you want this one
+	}
 
-        /// <summary>
-        /// min / max
-        /// </summary>
-        Min_Max = 4,
+	protected override void Awake()
+	{
+		base.Awake();
+		SetTargetData(targetData); //cache min/max strings
+	}
 
-        /// <summary>
-        /// min / curr
-        /// </summary>
-        Min_Curr = 5,
+	protected override void SubscribeToEvents()
+	{
+		targetData.AddListener(UpdateUI);
+	}
 
-        /// <summary>
-        /// X 256
-        /// </summary>
-        Prefix_Curr = 6,
+	protected override void UnsubscribeFromEvents()
+	{
+		targetData.RemoveListener(UpdateUI);
+	}
 
-        /// <summary>
-        /// 256 lives
-        /// </summary>
-        Curr_Suffix = 7,
+	public void SetTargetData(IntVariable newTargetData)
+	{
+		targetData = newTargetData;
 
-        /// <summary>
-        /// X 256 lives
-        /// </summary>
-        Prefix_Curr_Suffix = 8,
-    }
+		//cache min/max strings.
+		minString = targetData.MinClampValue.ToStringCached();
+		maxString = targetData.MaxClampValue.ToStringCached();
+	}
 
-    [Header("---Settings---")]
-    [SerializeField]
-    private EFormat format = EFormat.Curr_Max;
+	public override void ToggleVisuals()
+		=> this.enabled = readoutUIElement.enabled = !readoutUIElement.enabled;
 
-		[HideIf("@UsePrefix || UseSuffix")]
-    public string separatorString = " / ";
+	public override void ToggleVisuals(bool active)
+		=> this.enabled = readoutUIElement.enabled = active;
 
-		[ShowIf("@UsePrefix")]
-		public string prefixString = string.Empty;
+	[Button]
+	public override void UpdateUI()
+	{
+		string outputString = string.Format(
+			formatString,
+			minString,
+			targetData.Value.ToStringCached(),
+			maxString);
 
-		[ShowIf("@UseSuffix")]
-		public string suffixString = string.Empty;
-
-		public bool UsePrefix => format == EFormat.Prefix_Curr || format == EFormat.Prefix_Curr_Suffix;
-		public bool UseSuffix => format == EFormat.Curr_Suffix || format == EFormat.Prefix_Curr_Suffix;
-
-    [Header("---Prefab Refs---")]
-    [SerializeField]
-    [Required]
-    private TextMeshProUGUI readoutUIElement;
-
-    private void Reset()
-    {
-			SetDevDescription("I help format integers! I'm so helpful!");
-			readoutUIElement = GetComponent<TextMeshProUGUI>(); //assume you want this one
-    }
-
-    protected override void SubscribeToEvents()
-    {
-			targetData.AddListener(UpdateUI);
-    }
-
-    protected override void UnsubscribeFromEvents()
-    {
-			targetData.RemoveListener(UpdateUI);
-    }
-
-    public override void ToggleVisuals()
-			=> this.enabled = readoutUIElement.enabled = !readoutUIElement.enabled;
-
-    public override void ToggleVisuals(bool active)
-			=> this.enabled = readoutUIElement.enabled = active;
-
-    [Button]
-    public override void UpdateUI()
-    {
-        //all data into a single string
-        var strBuilder = CommunityStringBuilder.Instance;
-        string outputString = null;
-
-        switch (format)
-        {
-            case EFormat.Single: // 999
-                outputString = ConstStrings
-                    .GetCachedString(targetData);
-                break;
-
-            case EFormat.Curr_Max: // 450 / 1000
-                strBuilder
-                    .Append(ConstStrings.GetCachedString(
-                        targetData.Value))
-                    .Append(separatorString)
-                    .Append(ConstStrings.GetCachedString(
-                        targetData.MaxClampValue));
-                outputString = strBuilder.ToString();
-                break;
-
-            case EFormat.Max_Curr_Min: // 0 / 50 / 100
-                strBuilder
-                    .Append(ConstStrings.GetCachedString(
-                        targetData.MaxClampValue))
-                    .Append(separatorString)
-                    .Append(ConstStrings.GetCachedString(
-                        targetData.Value))
-                    .Append(separatorString)
-                    .Append(ConstStrings.GetCachedString(
-                        targetData.MinClampValue));
-                outputString = strBuilder.ToString();
-                break;
-
-            case EFormat.Min_Curr_Max: // 0 / 50 / 100
-                strBuilder
-									.Append(ConstStrings.GetCachedString(
-											targetData.MinClampValue))
-									.Append(separatorString)
-									.Append(ConstStrings.GetCachedString(
-											targetData.Value))
-									.Append(separatorString)
-									.Append(ConstStrings.GetCachedString(
-											targetData.MaxClampValue));
-                outputString = strBuilder.ToString();
-                break;
-
-            case EFormat.Min_Max: // 0 / 100
-							strBuilder.Append(ConstStrings.GetCachedString(
-									targetData.MinClampValue))
-								.Append(separatorString)
-								.Append(ConstStrings.GetCachedString(
-									targetData.MaxClampValue));
-							outputString = strBuilder.ToString();
-							break;
-
-            case EFormat.Min_Curr: // 0 / 9
-							strBuilder.Append(ConstStrings.GetCachedString(
-									targetData.MinClampValue))
-								.Append(separatorString)
-								.Append(ConstStrings.GetCachedString(
-									targetData.Value));
-							outputString = strBuilder.ToString();
-							break;
-
-            case EFormat.Prefix_Curr: // X 999
-							outputString = prefixString + ConstStrings.GetCachedString(targetData.Value);
-							break;
-
-						case EFormat.Curr_Suffix: // 999 lives
-							outputString = ConstStrings.GetCachedString(targetData.Value) + suffixString;
-							break;
-
-						case EFormat.Prefix_Curr_Suffix: // X 999 lives
-							strBuilder.Append(prefixString)
-								.Append(ConstStrings.GetCachedString(targetData.Value))
-								.Append(suffixString);
-							outputString = strBuilder.ToString();
-							break;
-
-            default:
-                Debug.LogErrorFormat("[{0}] Format enum not accounted for: {1}. " +
-                    "Add a case.", this.GetType().Name, format.ToString());
-                break;
-        }
-
-        readoutUIElement.text = outputString;
-    }
-
+		readoutUIElement.text = outputString;
+	}
 }
