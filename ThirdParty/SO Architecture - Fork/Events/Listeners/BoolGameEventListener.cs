e515f9a2438d8ba48980fafabca9dc9290eaea82
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2022-02-06 15:29:00 -0800

    Add support for Inverted listeners. Editor has not yet been updated to support this behaviour.

--- ThirdParty/SO Architecture - Fork/Events/Listeners/BoolGameEventListener.cs
+++ ThirdParty/SO Architecture - Fork/Events/Listeners/BoolGameEventListener.cs
@@ -1,9 +1,29 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace ScriptableObjectArchitecture
 {
+	/// <seealso cref="BoolVariable"/>
     [AddComponentMenu(SOArchitecture_Utility.EVENT_LISTENER_SUBMENU + "bool Event Listener")]
     public sealed class BoolGameEventListener : BaseGameEventListener<bool, BoolGameEvent, BoolUnityEvent>
     {
+        [SerializeField]
+        private BoolUnityEvent invertedResponse = new BoolUnityEvent();
+
+		protected override void RaiseResponse(bool value)
+		{
+			base.RaiseResponse(value);
+			invertedResponse.Invoke(!value);
+		}
+
+        public void AddInvertedListener(Action<bool> action)
+        {
+            invertedResponse.AddListener(action.Invoke);
+        }
+
+        public void RemoveInvertedListener(Action<bool> action)
+        {
+            invertedResponse.RemoveListener(action.Invoke);
+        }
     }
 }
