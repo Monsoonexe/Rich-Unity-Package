dba2f837a6901cfa2c225522bcf35cf1d4236e32
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2022-02-14 22:45:28 -0800

    Add more properties

--- _RichScripts/Static Classes/CommonYieldInstructions.cs
+++ _RichScripts/Static Classes/CommonYieldInstructions.cs
@@ -8,10 +8,13 @@ namespace RichPackage
 		public static readonly YieldInstruction WaitForFixedUpdate = new WaitForFixedUpdate();
 
 		public static readonly YieldInstruction WaitForTenthSecond = new WaitForSeconds(0.1f);
+		public static readonly YieldInstruction WaitForQuarterSecond = new WaitForSeconds(0.25f);
 		public static readonly YieldInstruction WaitForHalfSecond = new WaitForSeconds(0.5f);
 
 		public static readonly YieldInstruction WaitForOneSecond = new WaitForSeconds(1);
 		public static readonly YieldInstruction WaitForTwoSeconds = new WaitForSeconds(2);
-		public static readonly YieldInstruction WaitForThreeSecond = new WaitForSeconds(3);
+		public static readonly YieldInstruction WaitForThreeSeconds = new WaitForSeconds(3);
+		public static readonly YieldInstruction WaitForFiveSeconds = new WaitForSeconds(5);
+		public static readonly YieldInstruction WaitForTenSeconds = new WaitForSeconds(10);
 	}
 }
