ea7d8e788846b0176718471354b7232bdffb48d7
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2022-02-19 00:53:42 -0800

    New ISaveable interface

---
+++ _RichScripts/Save Systems/ISaveable.cs
@@ -0,0 +1,18 @@
+﻿
+namespace RichPackage.SaveSystem
+{
+	/// <summary>
+	/// Allows for saving/loading from an <see cref="ES3File"/>.
+	/// </summary>
+	/// <seealso cref="ASaveableMonoBehaviour"/>
+	public interface ISaveable
+	{
+		string SaveID { get; }
+
+		void SaveState(ES3File saveFile);
+
+		void LoadState(ES3File saveFile);
+
+		void DeleteState(ES3File saveFile);
+	}
+}
