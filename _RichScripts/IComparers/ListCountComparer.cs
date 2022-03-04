ddffd30eaceb8e8150fdbc0ff3666eaff61e6373
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2022-03-04 01:11:34 -0800

    Compares Counts of Lists

---
+++ _RichScripts/IComparers/ListCountComparer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace RichPackage
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	public class ListCountComparer<T> : IComparer<T> where T : IList
+	{
+		public int Compare(T x, T y) => x.Count.CompareTo(y.Count);
+
+		public static readonly ListCountComparer<T> Default = new ListCountComparer<T>();
+	}
+
+}
