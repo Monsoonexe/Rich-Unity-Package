800cd20b2ef1d1e6928dec10b6382b5d39275689
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2022-03-04 01:41:18 -0800

    Add more comparers

--- _RichScripts/IComparers/ListCountComparer.cs
+++ _RichScripts/IComparers/ListCountComparer.cs
@@ -13,4 +13,28 @@ namespace RichPackage
 		public static readonly ListCountComparer<T> Default = new ListCountComparer<T>();
 	}
 
+	/// <summary>
+	/// 
+	/// </summary>
+	public class ListCountReverseComparer<T> : IComparer<T> where T : IList
+	{
+		public int Compare(T x, T y) => y.Count.CompareTo(x.Count);
+
+		public static readonly ListCountReverseComparer<T> Default = new ListCountReverseComparer<T>();
+	}
+
+	public class ArrayLengthComparer<T> : IComparer<T[]>
+	{
+		public int Compare(T[] x, T[] y) => x.Length.CompareTo(y.Length);
+
+		public static readonly ArrayLengthComparer<T> Default = new ArrayLengthComparer<T>();
+	}
+
+	public class ArrayLengthReverseComparer<T> : IComparer<T[]>
+	{
+		public int Compare(T[] x, T[] y) => y.Length.CompareTo(x.Length);
+
+		public static readonly ArrayLengthReverseComparer<T> Default = new ArrayLengthReverseComparer<T>();
+	}
+
 }
