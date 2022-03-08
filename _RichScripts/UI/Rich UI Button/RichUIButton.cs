9dd2e08653aa4a9ef6ccd7e5be4bb4d3686033fd
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2022-03-08 07:26:09 -0800

    Update RichUIButton.cs
    Add to RichPackage.UI namespace
    
    Get at Button's targetGraphic instead of maintaining a hard reference to an Image.
    
    Add helpers to change tint ColorBlock.

--- _RichScripts/UI/Rich UI Button/RichUIButton.cs
+++ _RichScripts/UI/Rich UI Button/RichUIButton.cs
@@ -2,243 +2,253 @@
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
-using NaughtyAttributes;
+using Sirenix.OdinInspector;
 
-/// <summary>
-/// Automatically handles subscribing and responding to a button. 
-/// No need to rig Button in Scene.
-/// </summary>
-/// <seealso cref="RichUIButtonPayload{Tpayload}"/>
-/// <seealso cref="LoadedButton{TPayload}"/>
-[SelectionBase]
-public class RichUIButton : RichUIElement
+namespace RichPackage.UI
 {
-    [Header("---Button---")]
-    [SerializeField]
-    [Required]
-    protected Button myButton;
-    public Button Button { get => myButton; }
-    public bool ButtonEnabled 
-    { 
-        get => myButton.enabled; 
-        set => myButton.enabled = value; 
-    }
-
-    [SerializeField]
-    [Required]
-    protected Image myImage;
-    public Image Image { get => myImage; }
-    public bool ImageEnabled 
-    {
-		get => myImage.enabled; 
-        set => myImage.enabled = value;
-	}
-
-    public Sprite Sprite
-    {
-        get => myImage.sprite;
-        set => myImage.sprite = value;
-    }
-
-    [SerializeField]
-    [Required]
-    protected TextMeshProUGUI myText;
-    public TextMeshProUGUI Label { get => myText; }
-    public string Text 
-    {
-		get => myText.text; 
-        set => myText.text = value;
-	}
-
-    public bool TextEnabled 
-    {
-		get => myText.enabled; 
-        set => myText.enabled = value;
-	}
-
-    public bool Interactable
-    {
-        get => myButton.interactable;
-        set => myButton.interactable = value;
-    }
-
-    //events
-    public event Action<RichUIButton> OnPressedEvent;
-
-    protected virtual void Reset()
-    {
-        SetDevDescription("Automatically handles subscribing " +
-            "and responding to a button.");
-
-        myButton = GetComponent<Button>();
-        myImage = GetComponent<Image>();
-        myText = GetComponentInChildren<TextMeshProUGUI>();
-    }
-
     /// <summary>
-    /// Please call base.Awake() for maximum ease.
+    /// Automatically handles subscribing and responding to a button. 
+    /// No need to rig Button in Scene.
     /// </summary>
-    protected override void Awake()
+    /// <seealso cref="RichUIButton{Tpayload}"/>
+    [SelectionBase]
+    public class RichUIButton : RichUIElement
     {
-        base.Awake();
-        if (!myButton)
+        [Header("---Button---")]
+        [SerializeField, Required]
+        protected Button myButton;
+        public Button Button { get => myButton; }
+        public bool ButtonEnabled
         {
-            myButton = GetComponent<Button>(); //keep looking
+            get => myButton.enabled;
+            set => myButton.enabled = value;
+        }
+
+        public ColorBlock ColorBlock 
+        { 
+            get => myButton.colors;
+            set => myButton.colors = value;
+        }
+
+        public Image Image { get => myButton.targetGraphic as Image; }
+        public bool ImageEnabled
+        {
+            get => myButton.targetGraphic.enabled;
+            set => myButton.targetGraphic.enabled = value;
+        }
+
+        public Sprite Sprite
+        {
+            get => Image.sprite;
+            set => Image.sprite = value;
+        }
+
+        public Color Color 
+        { 
+            get => myButton.targetGraphic.color; 
+            set => myButton.targetGraphic.color = value; 
+        }
+
+        [SerializeField]
+        protected TextMeshProUGUI myText;
+        public TextMeshProUGUI Label { get => myText; }
+        public string Text
+        {
+            get => myText.text;
+            set => myText.text = value;
+        }
+
+        public bool TextEnabled
+        {
+            get => myText.enabled;
+            set => myText.enabled = value;
+        }
+
+        public bool Interactable
+        {
+            get => myButton.interactable;
+            set => myButton.interactable = value;
+        }
+
+        //events
+        public event Action<RichUIButton> OnPressedEvent;
+
+        protected virtual void Reset()
+        {
+            SetDevDescription("Automatically handles subscribing " +
+                "and responding to a button.");
+            myButton = GetComponent<Button>();
+            myText = GetComponentInChildren<TextMeshProUGUI>();
+        }
+
+        /// <summary>
+        /// Please call base.Awake() for maximum ease.
+        /// </summary>
+        protected override void Awake()
+        {
+            base.Awake();
             if (!myButton)
             {
-                myButton = GetComponentInChildren<Button>(); // don't give up!
+                myButton = GetComponent<Button>(); //keep looking
+                if (!myButton)
+                {
+                    myButton = GetComponentInChildren<Button>(); // don't give up!
+                }
             }
         }
 
-        if (!myImage)
+        /// <summary>
+        /// call base.OnEnable() to auto subscribe to button
+        /// </summary>
+        protected override void OnEnable()
         {
-            myImage = GetComponent<Image>(); //keep looking
-            if (!myImage)
-            {
-                myImage = GetComponentInChildren<Image>(); //keep looking
-            }
+            myButton.onClick.AddListener(OnButtonClick);
+            UpdateUI();
         }
 
-        if (!myText)
+        /// <summary>
+        /// call base.OnDisable() to auto unsubscribe from button
+        /// </summary>
+        protected virtual void OnDisable()
+            => myButton.onClick.RemoveListener(OnButtonClick);
+
+        public void AddListener(Action action) => myButton.onClick.AddListener(action.Invoke);
+
+        public void RemoveListener(Action action) => myButton.onClick.RemoveListener(action.Invoke);
+
+        public void RemoveAllListeners() => myButton.onClick.RemoveAllListeners();
+
+        /// <summary>
+        /// Function called by Button.
+        /// </summary>
+        [Button("Click"), DisableInEditorMode]
+        protected virtual void OnButtonClick() => OnPressedEvent?.Invoke(this);
+
+        /// <summary>
+        /// Hide/Show visual elements.
+        /// </summary>
+        /// <param name="active"></param>
+        public override void ToggleVisuals(bool active)
         {
-            myText = GetComponent<TextMeshProUGUI>(); //keep looking
-            if (!myText)
-            {
-                myText = GetComponentInChildren<TextMeshProUGUI>(); // don't give up!
-            }
+            myButton.enabled = active;
+            ImageEnabled = active;
+            if (myText)
+                myText.enabled = active;
         }
+
+        public void Show(string text)
+        {
+            Text = text;
+            Show();
+        }
+
+        public void Show(string text, Sprite sprite)
+        {
+            Text = text;
+            Sprite = sprite;
+            Show();
+        }
+
+        public void Show(Action onPress)
+        {
+            RemoveAllListeners();
+            AddListener(onPress.Invoke);
+            Show();
+        }
+
+        public void Show(Action onPress, Sprite sprite)
+        {
+            Sprite = sprite;
+            Show(onPress);
+        }
+
+        public void Show(Action onPress, string text)
+        {
+            Text = text;
+            Show(onPress);
+        }
+
+        public void Show(Action onPress, string text, Sprite sprite)
+        {
+            Sprite = sprite;
+            Show(onPress, text);
+        }
+
+        public void Show(Sprite sprite)
+        {
+            Sprite = sprite;
+            Show();
+        }
+
+        #region Button Helpers
+
+        public void SetNormalColor(in Color newColor)
+		{
+            ColorBlock overrideColors = myButton.colors;
+            overrideColors.normalColor = newColor;
+            myButton.colors = overrideColors;
+        }
+
+        public void SetHighlightedColor(in Color newColor)
+		{
+            ColorBlock overrideColors = myButton.colors;
+            overrideColors.highlightedColor = newColor;
+            myButton.colors = overrideColors;
+        }
+
+        #endregion
+
+        public static implicit operator Button (RichUIButton a) => a.Button;
     }
 
     /// <summary>
-    /// call base.OnEnable() to auto subscribe to button
+    /// Automatically handles subscribing and responding to a button. 
+    /// No need to rig Button in the Scene.
     /// </summary>
-    protected override void OnEnable()
+    /// <remarks>A "Payload" is any other data the button should hold. This is useful for mapping
+    /// a button to information, like "when I push this button, give me one tall potion". 
+    /// You can assign the payload to the button, then get the payload off the button 
+    /// when it is pushed.
+    /// </remarks>
+    /// <seealso cref="IntButton"/>
+    public abstract class RichUIButton<TPayload> : RichUIButton
+        where TPayload : new()//has a default constructor
     {
-        myButton.onClick.AddListener(OnButtonClick);
-        UpdateUI();
+        /// <summary>
+        /// class that holds the actual payload and any other data. Like if you want to play
+        /// particle effect on click or something, that data would be held inside.
+        /// </summary>
+        [SerializeField]
+        [Tooltip("Can be set in Inspector, but probably should be set " +
+            "using Configure() at runtime")]
+        public TPayload PayloadData = default;
+
+        public event Action<TPayload> OnPressedPayloadEvent;
+
+        protected override void Reset()
+        {
+            SetDevDescription("Automatically handles subscribing " +
+                "and responding to a button." +
+                "\r\nAnnounces its payload when pressed.");
+
+            //assume this hierarchy when initializing, but not always.
+            myButton = GetComponent<Button>();
+            myText = GetComponentInChildren<TextMeshProUGUI>();
+        }
+
+        public virtual void Show(TPayload newPayloadData, string label, Sprite image)
+        {
+            PayloadData = newPayloadData;
+            Show(label, image);//forward
+        }
+
+        protected override void OnButtonClick()
+        {
+            base.OnButtonClick();
+            OnPressedPayloadEvent?.Invoke(PayloadData);
+        }
+
+        public static implicit operator TPayload(RichUIButton<TPayload> a)
+            => a.PayloadData;
     }
-
-    /// <summary>
-    /// call base.OnDisable() to auto unsubscribe from button
-    /// </summary>
-    protected virtual void OnDisable()
-        => myButton.onClick.RemoveListener(OnButtonClick);
-
-    public void AddListener(Action action) => myButton.onClick.AddListener(action.Invoke);
-
-    public void RemoveListener(Action action) => myButton.onClick.RemoveListener(action.Invoke);
-
-    public void RemoveAllListeners() => myButton.onClick.RemoveAllListeners();
-
-    /// <summary>
-    /// Function called by Button.
-    /// </summary>
-    protected virtual void OnButtonClick() => OnPressedEvent?.Invoke(this);
-
-    /// <summary>
-    /// Hide/Show visual elements.
-    /// </summary>
-    /// <param name="active"></param>
-    public override void ToggleVisuals(bool active)
-    {
-        myImage.enabled = active;
-        myText.enabled = active;
-        myButton.enabled = active;
-    }
-
-    public void Show(string text)
-	{
-        Text = text;
-        Show();
-    }
-
-    public void Show(string text, Sprite sprite)
-    {
-        Text = text;
-        Sprite = sprite;
-        Show();
-    }
-
-    public void Show(Action onPress)
-	{
-        RemoveAllListeners();
-        AddListener(onPress.Invoke);
-        Show();
-	}
-
-    public void Show(Action onPress, Sprite sprite)
-    {
-        Sprite = sprite;
-        Show(onPress);
-    }
-
-    public void Show(Action onPress, string text)
-	{
-        Text = text;
-        Show(onPress);
-	}
-
-    public void Show(Action onPress, string text, Sprite sprite)
-	{
-        Sprite = sprite;
-        Show(onPress, text);
-	}
-
-    public void Show(Sprite sprite)
-	{
-        Sprite = sprite;
-        Show();
-	}
-}
-
-/// <summary>
-/// Automatically handles subscribing and responding to a button. 
-/// No need to rig Button in the Scene.
-/// </summary>
-/// <remarks>A "Payload" is any other data the button should hold. This is useful for mapping
-/// a button to information, like "when I push this button, give me one tall potion". 
-/// You can assign the payload to the button, then get the payload off the button 
-/// when it is pushed.
-/// </remarks>
-/// <seealso cref="IntButton"/>
-public abstract class RichUIButton<TPayload> : RichUIButton
-    where TPayload : new()//is a payload and has a default constructor
-{
-    /// <summary>
-    /// class that holds the actual payload and any other data. Like if you want to play
-    /// particle effect on click or something, that data would be held inside.
-    /// </summary>
-    [SerializeField]
-    [Tooltip("Can be set in Inspector, but probably should be set " +
-        "using Configure() at runtime")]
-    public TPayload PayloadData = default;
-
-    public event Action<TPayload> OnPressedPayloadEvent;
-
-    protected override void Reset()
-    {
-        SetDevDescription("Automatically handles subscribing " +
-            "and responding to a button." +
-            "\r\nAnnounces its payload when pressed.");
-
-        //assume this hierarchy when initializing, but not always.
-        myButton = GetComponent<Button>();
-        myImage = GetComponent<Image>();
-        myText = GetComponentInChildren<TextMeshProUGUI>();
-    }
-
-    public virtual void Show(TPayload newPayloadData, string label, Sprite image)
-    {
-        PayloadData = newPayloadData;
-        Show(label, image);//forward
-    }
-
-	protected override void OnButtonClick()
-	{
-		base.OnButtonClick();
-        OnPressedPayloadEvent?.Invoke(PayloadData);
-	}
-
-	public static implicit operator TPayload (RichUIButton<TPayload> a)
-        => a.PayloadData;
 }
