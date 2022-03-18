451848ebecbcd781d674bbcc3c567ce6a40d9610
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2022-03-18 02:29:12 -0700

    Align with ObjectPool

--- _RichScripts/Object Pooling/PoolablePool.cs
+++ _RichScripts/Object Pooling/PoolablePool.cs
@@ -1,192 +1,149 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 
-public delegate void InitPoolableMethod(IPoolable poolable);
-
-/// <summary>
-/// A Pool for GameObject prefabs that implement the IPoolable Interface.
-/// </summary>
-public class PoolablePool : RichMonoBehaviour
+namespace RichPackage.Pooling
 {
-    public enum InitStrategy
+    /// <summary>
+    /// A Pool for GameObject prefabs that implement the IPoolable Interface.
+    /// </summary>
+    public class PoolablePool : RichMonoBehaviour
     {
-        EAGER = 0, //init right away
-        LAZY = 1 //init as you go
-    }
+        [Header("---Prefab Pool Resources---")]
+        public GameObject objectPrefab;
 
-    [Header("---Prefab Pool Resources---")]
-    public GameObject objectPrefab;
+        [Header("---Pool Base Settings---")]
+        public bool InitOnAwake = true;
 
-    [Header("---Pool Base Settings---")]
-    public bool InitOnAwake = true;
+        public bool createWhenEmpty = true;
 
-    public bool createWhenEmpty = true;
+        [SerializeField, MinValue(0)]
+        private int startingAmount = 6;
 
-    [SerializeField]
-    protected InitStrategy initStragety = InitStrategy.EAGER;
+        [SerializeField, MinValue(0)]
+        [Tooltip("less than 0 means 'no limit'.")]
+        private int maxAmount = 10;
+        
+        [Tooltip("[Optional]")]
+        public Transform poolParent = null;
 
-    [SerializeField]
-    private int startingAmount = 6;
+        //runtime data
+        public StackPool<IPoolable> Pool { get; private set; }
 
-    [SerializeField]
-    [Tooltip("less than 0 means 'no limit'.")]
-    private int maxAmount = 10;
-    
-    [Tooltip("[Optional]")]
-    public Transform poolParent = null;
+        private List<IPoolable> manifest;
 
-    /// <summary>
-    /// [Optional] Something special to be performed on new entries.
-    /// </summary>
-    public InitPoolableMethod InitPoolableMethod = (p) => p = null;//no-op so never null
+        /// <summary>
+        /// Total items this Pool tracks.
+        /// </summary>
+        public int PopulationCount { get => manifest.Count; }
 
-    //runtime data
-    private Stack<IPoolable> pool;
-    private List<IPoolable> manifest;
+        /// <summary>
+        /// How many items are ready to be deployed.
+        /// </summary>
+        public int ReadyCount { get => Pool.Count; }
 
-    /// <summary>
-    /// Total items this Pool tracks.
-    /// </summary>
-    public int PopulationCount { get => manifest.Count; }
-
-    /// <summary>
-    /// How many items are ready to be deployed.
-    /// </summary>
-    public int ReadyCount { get => pool.Count; }
-
-    /// <summary>
-    /// Number of items currently depooled.
-    /// </summary>
-    public int InUseCount { get => manifest.Count - pool.Count; }
-    //private Queue<IPoolable> queuePool;
-
-    protected override void Awake()
-    {
-        base.Awake();
-        manifest = new List<IPoolable>(maxAmount);
-        if(InitOnAwake)
-            InitPool();
-    }
-
-    private void OnValidate()
-    {
-        if (objectPrefab != null)
-        {   //ensure prefab has an IPoolable Component on it 
-            if (objectPrefab.GetComponent<IPoolable>() == null)
+        /// <summary>
+        /// Number of items currently depooled.
+        /// </summary>
+        public int InUseCount { get => manifest.Count - Pool.Count; }
+        //private Queue<IPoolable> queuePool;
+        
+        private void OnValidate()
+        {   
+             //ensure prefab has an IPoolable Component on it 
+            if (objectPrefab != null && objectPrefab.GetComponent<IPoolable>() == null)
             {
                 Debug.LogError("[PoolablePool] Prefab {" + objectPrefab.name
                     + "} does not have an IPoolable Component", objectPrefab);
                 objectPrefab = null;
             }
         }
-    }
 
-    protected virtual IPoolable CreatePoolable()
-    {
-        if (maxAmount >= 0 && PopulationCount >= maxAmount)
-            return null; //at max capacity
-
-        var newGameObj = Instantiate(objectPrefab, poolParent);
-        var poolable = newGameObj.GetComponent<IPoolable>();
-
-        //validate
-        Debug.Assert(poolable != null,
-            "[PoolablePool] No Poolable found on " + objectPrefab, objectPrefab);
-
-        poolable.OnCreate();
-        poolable.PoolOwner = this; //know where you came from
-        manifest.Add(poolable);//track
-        
-        return poolable;
-    }
-
-    /// <summary>
-    /// Take an item out of the pool.
-    /// </summary>
-    /// <returns></returns>
-    public virtual IPoolable Depool()
-    {
-        IPoolable depooledItem = null;
-
-        if (pool.Count > 0)
-            depooledItem = pool.Pop();
-        else if(createWhenEmpty)
-            depooledItem = CreatePoolable();
-        
-        if(depooledItem != null)
+        protected override void Awake()
         {
-            if (initStragety == InitStrategy.LAZY)
-                InitPoolableMethod(depooledItem);
+            base.Awake();
+            manifest = new List<IPoolable>(maxAmount);
+            if(InitOnAwake)
+                InitPool();
+        }
+
+        protected virtual IPoolable CreatePoolable()
+        {
+            var newGameObj = Instantiate(objectPrefab, poolParent);
+            var poolable = newGameObj.GetComponent<IPoolable>();
+
+            //validate
+            Debug.Assert(poolable != null,
+                "[PoolablePool] No Poolable found on " + objectPrefab, objectPrefab);
+
+            //init
+            poolable.OnCreate();
+            poolable.PoolOwner = this; //know where you came from
+            manifest.Add(poolable);//track
             
-            depooledItem.OnDepool();
+            return poolable;
         }
 
-        return depooledItem;
-    }
+        /// <summary>
+        /// Take an item out of the pool.
+        /// </summary>
+        /// <returns></returns>
+        public virtual IPoolable Depool() => Pool.Depool();
 
-    public T Depool<T>() where T : class => Depool() as T;
+        public T Depool<T>() where T : class => Depool() as T;
 
-    /// <summary>
-    /// Add an item back into the pool.
-    /// </summary>
-    /// <param name="poolable"></param>
-    public virtual void Enpool(IPoolable poolable)
-    {
-        Debug.Assert(poolable.PoolOwner == this,
-            "[PoolablePool] Pool Error! Enpooled to non-owning Pool!", this);
-
-        if (!pool.Contains(poolable))//guard against multiple entries
+        /// <summary>
+        /// Add an item back into the pool.
+        /// </summary>
+        /// <param name="poolable"></param>
+        public virtual void Enpool(IPoolable poolable)
         {
-            pool.Push(poolable);
-            poolable.OnEnpool();
+            Debug.Assert(poolable.PoolOwner == this,
+                "[PoolablePool] Pool Error! Enpooled to non-owning Pool!", this);
+
+            if (!Pool.Stack.Contains(poolable))//guard against multiple entries
+                Pool.Enpool(poolable);
         }
-    }
 
-    /// <summary>
-    /// Create entire pool
-    /// </summary>
-    public virtual void InitPool()
-    {
-        maxAmount = maxAmount < startingAmount ? startingAmount : maxAmount;
-        pool = new Stack<IPoolable>(maxAmount);
-
-        //preload pool
-        for(var i = startingAmount; i > 0; --i)
+        /// <summary>
+        /// Create entire pool
+        /// </summary>
+        public virtual async void InitPool()
         {
-            var newP = CreatePoolable();
-
-            if (initStragety == InitStrategy.EAGER)
-                    InitPoolableMethod(newP);
-
-            Enpool(newP);
+            Pool = new StackPool<IPoolable>(maxAmount, startingAmount)
+            {
+                FactoryMethod = CreatePoolable,
+                OnDepoolMethod = (p) => p.OnDepool(),
+                OnEnpoolMethod = (p) => p.OnEnpool(),
+                CreateNewWhenEmpty = createWhenEmpty,
+            };
+            Pool.Add(startingAmount);
         }
-    }
 
-    /// <summary>
-    /// Resize the pool.
-    /// </summary>
-    /// <param name="newCapacity"></param>
-    public virtual void Resize(int newCapacity)
-    {
-        //if (!resizeable) return;
-        //Debug.Assert(resizeable, "[PoolablePool] Attempting to resize a marked non-resizable pool.");
-
-        maxAmount = Mathf.Min(newCapacity, ReadyCount);
-        while (pool.Count > maxAmount) //shrink
+        /// <summary>
+        /// Resize the pool.
+        /// </summary>
+        /// <param name="newCapacity"></param>
+        public virtual void Resize(int newCapacity)
         {
-            var poolable = pool.Pop();//trim excess
-            manifest.Remove(poolable);
+            //if (!resizeable) return;
+            //Debug.Assert(resizeable, "[PoolablePool] Attempting to resize a marked non-resizable pool.");
+
+            maxAmount = Mathf.Min(newCapacity, ReadyCount);
+            while (Pool.Count > maxAmount) //shrink
+            {
+                var poolable = Pool.Depool();//trim excess
+                manifest.Remove(poolable);
+            }
+
+            manifest.TrimExcess();
+            Pool.Stack.TrimExcess();
         }
 
-        manifest.TrimExcess();
-        pool.TrimExcess();
+        public void ReturnAllToPool()
+        {
+            var count = manifest.Count;
+            for (var i = 0; i < count; ++i)
+                Enpool(manifest[i]);
+        }
     }
-
-    public void ReturnAllToPool()
-    {
-        var count = manifest.Count;
-        for (var i = 0; i < count; ++i)
-            Enpool(manifest[i]);
-    }
-
 }
