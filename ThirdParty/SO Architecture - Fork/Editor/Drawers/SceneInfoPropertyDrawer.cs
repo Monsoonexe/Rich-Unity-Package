8a8d96e04164b9945a026f5f0b788da556db7a1d
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2022-03-19 21:57:15 -0700

    Draw icon and description

--- ThirdParty/SO Architecture - Fork/Editor/Drawers/SceneInfoPropertyDrawer.cs
+++ ThirdParty/SO Architecture - Fork/Editor/Drawers/SceneInfoPropertyDrawer.cs
@@ -11,7 +11,9 @@ namespace ScriptableObjectArchitecture.Editor
         private const string SCENE_INDEX_PROPERTY = "_sceneIndex";
         private const string SCENE_ENABLED_PROPERTY = "_isSceneEnabled";
         private const string SCENE_NAME_PROPERTY = "_sceneName";
-        private const int FIELD_COUNT = 5;
+        private const string SCENE_DESCRIPTION_PROPERTY = "_sceneDescription";
+        private const string SCENE_ICON_PROPERTY = "_icon";
+        private const int FIELD_COUNT = 7;
 
         public override void OnGUI(Rect propertyRect, SerializedProperty property, GUIContent label)
         {
@@ -19,6 +21,8 @@ namespace ScriptableObjectArchitecture.Editor
             var sceneIndexProperty = property.FindPropertyRelative(SCENE_INDEX_PROPERTY);
             var enabledProperty = property.FindPropertyRelative(SCENE_ENABLED_PROPERTY);
             var nameProperty = property.FindPropertyRelative(SCENE_NAME_PROPERTY);
+            var descriptionProperty = property.FindPropertyRelative(SCENE_DESCRIPTION_PROPERTY);
+            var iconProperty = property.FindPropertyRelative(SCENE_ICON_PROPERTY);
 
             EditorGUI.BeginProperty(propertyRect, new GUIContent(property.displayName), property);
             EditorGUI.BeginChangeCheck();
@@ -27,7 +31,7 @@ namespace ScriptableObjectArchitecture.Editor
             var sceneAssetRect = new Rect
             {
                 position = propertyRect.position,
-                size = new Vector2(propertyRect.width, 
+                size = new Vector2(propertyRect.width,
                     EditorGUIUtility.singleLineHeight)
             };
 
@@ -64,11 +68,19 @@ namespace ScriptableObjectArchitecture.Editor
             var nameRect = enabledRect;
             nameRect.y += EditorGUIUtility.singleLineHeight;
 
+            var descriptionRect = nameRect;
+            descriptionRect.y += EditorGUIUtility.singleLineHeight;
+
+            var iconRect = descriptionRect;
+            iconRect.y += EditorGUIUtility.singleLineHeight;
+
             EditorGUI.PropertyField(pathRect, scenePathProperty);
             EditorGUI.PropertyField(indexRect, sceneIndexProperty);
             EditorGUI.PropertyField(enabledRect, enabledProperty);
             EditorGUI.EndDisabledGroup();
             EditorGUI.PropertyField(nameRect, nameProperty);
+            EditorGUI.PropertyField(descriptionRect, descriptionProperty);
+            EditorGUI.PropertyField(iconRect, iconProperty);
             if (EditorGUI.EndChangeCheck())
             {
                 property.serializedObject.ApplyModifiedProperties();
