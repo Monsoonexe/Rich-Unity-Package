4cbe0af6a49466c456661d1a7959208dd5f81c0e
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2022-03-19 23:06:08 -0700

    Update Poolable.cs
    Add to RichPackage.Pooling
    prefer Odin to Naughty

--- _RichScripts/Object Pooling/Poolable.cs
+++ _RichScripts/Object Pooling/Poolable.cs
@@ -1,48 +1,53 @@
 ﻿using UnityEngine;
-using NaughtyAttributes;
+using Sirenix.OdinInspector;
 
-/// <summary>
-/// A basic Poolable MonoBehaviour.
-/// </summary>
-public class Poolable : RichMonoBehaviour, IPoolable
+namespace RichPackage.Pooling
 {
-    [MinValue(0)]
-    public float lifetime = 10f;
 
-    public PoolablePool PoolOwner { get; set; }
-
-    [ShowNativeProperty]
-    public bool InUse { get; private set;}
-
-    PoolablePool IPoolable.PoolOwner { get; set; }
-    private Coroutine lifetimeCoroutine;
-
-    public virtual void OnCreate()
+    /// <summary>
+    /// A basic Poolable MonoBehaviour. Serves as an example for how to implement an <see cref="IPoolable"/>
+    /// whose lifetime is driven by a <see cref="PoolablePool"/>.
+    /// </summary>
+    public class Poolable : RichMonoBehaviour, IPoolable
     {
-        //nada
-    }
+        [MinValue(0)]
+        public float lifetime = 10f;
 
-    public virtual void OnDepool()
-    {
-        InUse = true;
-        gameObject.SetActive(true);
-        if (lifetime > 0)
+        public PoolablePool PoolOwner { get; set; }
+
+        [ShowInInspector, ReadOnly]
+        public bool InUse { get; private set; }
+
+        PoolablePool IPoolable.PoolOwner { get; set; }
+        private Coroutine lifetimeCoroutine;
+
+        public virtual void OnCreate()
         {
-            lifetimeCoroutine = StartCoroutine(
-                Utility.InvokeAfterDelay(ReturnToPool, lifetime));
+            //nada
         }
-    }
 
-    public virtual void OnEnpool()
-    {
-        InUse = false;
-        if (lifetimeCoroutine != null)
+        public virtual void OnDepool()
         {
-            StopCoroutine(lifetimeCoroutine);
+            InUse = true;
+            gameObject.SetActive(true);
+            if (lifetime > 0)
+            {
+                lifetimeCoroutine = StartCoroutine(
+                    Utility.InvokeAfterDelay(ReturnToPool, lifetime));
+            }
         }
-        gameObject.SetActive(false);
-    }
 
-    [Button("ReturnToPool()", EButtonEnableMode.Playmode)]
-    public void ReturnToPool() => PoolOwner.Enpool(this);
+        public virtual void OnEnpool()
+        {
+            InUse = false;
+            if (lifetimeCoroutine != null)
+            {
+                StopCoroutine(lifetimeCoroutine);
+            }
+            gameObject.SetActive(false);
+        }
+
+        [Button]
+        public void ReturnToPool() => PoolOwner.Enpool(this);
+    }
 }
