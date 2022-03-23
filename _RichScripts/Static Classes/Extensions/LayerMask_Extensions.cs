f28b86e2d06daa5a0f498f5b2eb2be640957a7d5
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2022-03-22 20:18:10 -0700

    Add LayerMaskExtensions

---
+++ _RichScripts/Static Classes/Extensions/LayerMask_Extensions.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace RichPackage
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class LayerMask_Extensions
+    {
+        public static bool ContainsLayer(this LayerMask layerMask, int layer)
+            => layerMask == (layerMask | (1 << layer));
+    }
+}
