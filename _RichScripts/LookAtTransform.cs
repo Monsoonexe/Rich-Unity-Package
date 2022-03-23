cab960b102a4d0bbe3c1854470ffbc90dce18cc1
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2022-03-22 21:07:23 -0700

    Add LookAtTransform.cs

---
+++ _RichScripts/LookAtTransform.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using Sirenix.OdinInspector;
+using NaughtyAttributes;
+
+using HideIf = Sirenix.OdinInspector.HideIfAttribute;
+using ShowIf = Sirenix.OdinInspector.ShowIfAttribute;
+
+/// <seealso cref="LookAtCamera"/>
+public class LookAtTransform : RichMonoBehaviour
+{
+    [SerializeField]
+    private bool dynamicallyAssign = false;
+
+    [SerializeField, HideIf("@dynamicallyAssign")]
+    private Transform target;
+
+    [SerializeField, Tag, ShowIf("@dynamicallyAssign")]
+    private string findByTag = "Player";
+
+    private void LateUpdate()
+    {
+        if (dynamicallyAssign && target == null)
+		{
+            GameObject obj = GameObject.FindGameObjectWithTag(findByTag);
+            target = obj != null ? obj.GetComponent<Transform>() : null;
+        }
+
+        if (target != null)
+            myTransform.LookAt(target);
+    }
+}
