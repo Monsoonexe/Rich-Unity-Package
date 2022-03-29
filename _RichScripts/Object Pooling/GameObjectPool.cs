0db5cb8fd3885c664d39ca4a4c8ecebc4ad202e0
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2022-03-28 21:29:33 -0700

    Update GameObjectPool.cs

--- _RichScripts/Object Pooling/GameObjectPool.cs
+++ _RichScripts/Object Pooling/GameObjectPool.cs
@@ -1,330 +1,314 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
-using NaughtyAttributes;
+using Sirenix.OdinInspector;
+using UnityEngine.Assertions;
 
-public delegate void GameObjectMethod(GameObject poolable);
 //TODO: Reclaim when empty strategy. Like bullet holes in FPS games.
 //TODO: ability to pre-spawn items in the Editor.
 
-/// <summary>
-/// Pools a GameObject Prefab. Depool() replaces Instantiate(), and Enpool() replaces Destroy().
-/// </summary>
-public class GameObjectPool : RichMonoBehaviour
+namespace RichPackage.Pooling
 {
-    public enum InitStrategy
+    public delegate void GameObjectMethod(GameObject poolable);
+    /// <summary>
+    /// Pools a GameObject Prefab. Depool() replaces Instantiate(), and Enpool() replaces Destroy().
+    /// </summary>
+    /// <seealso cref="PoolablePool"/>
+    public class GameObjectPool : RichMonoBehaviour
     {
+        [Title("Resources")]
+        [PreviewField, Required]
+        public GameObject objectPrefab;
+
+        [Header("---Settings---")]
+        public bool initOnAwake = true;
+        public bool createWhenEmpty = true;
+
+        [Min(0)]
+        public int startingAmount = 6;
+
+        [Tooltip("less than 0 means 'no limit'.")]
+        [Min(-1)]
+        public int maxAmount = 10;
+
+        [Tooltip("[Optional]")]
+        public Transform poolParent = null;
+
         /// <summary>
-        /// Init right away when item created.
+        /// [Optional] Something special to be performed when new items are created.
         /// </summary>
-        OnCreate = 0,
+        public GameObjectMethod InitPoolableMethod;// = (p) => p = null;//no-op so delegate never null
 
         /// <summary>
-        /// Init each time item is depooled.
+        /// [Optional] What should be done when item is Depooled?
+        /// By default calls SetActive(true);
         /// </summary>
-        OnDepool = 1
-    }
+        public GameObjectMethod OnDepoolMethod = (p) => p.SetActive(true);
 
-    [Header("---Resources---")]
-    [ShowAssetPreview]
-    [Required]
-    public GameObject objectPrefab;
+        /// <summary>
+        /// [Optional] What should be done when item is Enpooled?
+        /// By default calls SetActive(false);
+        /// </summary>
+        public GameObjectMethod OnEnpoolMethod = (p) => p.SetActive(false);
 
-    [Header("---Settings---")]
-    public bool initOnAwake = true;
+        //runtime data
+        private Stack<GameObject> pool = new Stack<GameObject>(); //stack has better locality than queue
+        private List<GameObject> manifest;
+        /// <summary>
+        /// Every GameObject Managed by this pool, non- and active alike.
+        /// </summary>
+        public IList<GameObject> Manifest { get => manifest; }
 
-    public InitStrategy initStragety = InitStrategy.OnCreate;
+        /// <summary>
+        /// Total items this Pool tracks.
+        /// </summary>
+        public int PopulationCount { get => manifest.Count; }
 
-    [Min(0)]
-    public int startingAmount = 6;
+        /// <summary>
+        /// How many items are ready to be deployed.
+        /// </summary>
+        public int ReadyCount { get => pool.Count; }
 
-    [Tooltip("less than 0 means 'no limit'.")]
-    [Min(-1)]
-    public int maxAmount = 10;
+        /// <summary>
+        /// Number of items currently depooled.
+        /// </summary>
+        public int InUseCount { get => manifest.Count - pool.Count; }
 
-    [Tooltip("[Optional]")]
-    public Transform poolParent = null;
-
-    /// <summary>
-    /// [Optional] Something special to be performed when new items are created.
-    /// </summary>
-    public GameObjectMethod InitPoolableMethod = (p) => p = null;//no-op so delegate never null
-
-    /// <summary>
-    /// [Optional] What should be done when item is Depooled?
-    /// By default calls SetActive(true);
-    /// </summary>
-    public GameObjectMethod OnDepoolMethod = (p) => p.SetActive(true);
-
-    /// <summary>
-    /// [Optional] What should be done when item is Enpooled?
-    /// By default calls SetActive(false);
-    /// </summary>
-    public GameObjectMethod OnEnpoolMethod = (p) => p.SetActive(false);
-
-    //runtime data
-    private Stack<GameObject> pool = new Stack<GameObject>(); //stack has better locality than queue
-    private List<GameObject> manifest;
-    /// <summary>
-    /// Every GameObject Managed by this pool, non- and active alike.
-    /// </summary>
-    public IList<GameObject> Manifest { get => manifest; }
-
-    /// <summary>
-    /// Total items this Pool tracks.
-    /// </summary>
-    public int PopulationCount { get => manifest.Count; }
-
-    /// <summary>
-    /// How many items are ready to be deployed.
-    /// </summary>
-    public int ReadyCount { get => pool.Count; }
-
-    /// <summary>
-    /// Number of items currently depooled.
-    /// </summary>
-    public int InUseCount { get => manifest.Count - pool.Count; }
-
-    private void Reset()
-    {
-        SetDevDescription("I replace Instantiate with a pool of objects. " +
-            "I'm doing my part to reduce garbage generation.");
-        poolParent = GetComponent<Transform>();
-    }
-
-    protected override void Awake()
-    {
-        base.Awake();
-        if (initOnAwake)
-            InitPool();
-    }
-
-    private GameObject CreatePoolable()
-    {
-        GameObject newGameObject = null;//return value
-        if (maxAmount >= 0 && PopulationCount >= maxAmount)
+        private void Reset()
         {
-            Debug.Log("[" + name + "] Pool is exhausted. "
-                + "Count: " + ConstStrings.GetCachedString(maxAmount)
-                + ". Consider increasing 'maxAmount' or setting 'createWhenEmpty'."
-                , this);
-        }
-        else
-        {
-            newGameObject = Instantiate(objectPrefab, poolParent);
-            manifest.Add(newGameObject);//track
+            SetDevDescription("I replace Instantiate with a pool of objects. " +
+                "I'm doing my part to reduce garbage generation.");
+            poolParent = GetComponent<Transform>();
         }
 
-        return newGameObject;
-    }
-
-    public void AddItems(int amount = 1)
-    {
-        for (var i = amount - 1; i >= 0; --i)
+        protected override void Awake()
         {
-            var obj = CreatePoolable();
+            base.Awake();
+            if (initOnAwake)
+                InitPool();
+        }
+
+        private GameObject CreatePoolable()
+        {
+            GameObject newGameObject = null;//return value
+            if (maxAmount >= 0 && PopulationCount >= maxAmount)
+            {
+                Debug.Log("[" + name + "] Pool is exhausted. "
+                    + "Count: " + ConstStrings.GetCachedString(maxAmount)
+                    + ". Consider increasing 'maxAmount' or setting 'createWhenEmpty'."
+                    , this);
+            }
+            else
+            {
+                newGameObject = Instantiate(objectPrefab, poolParent);
+                manifest.Add(newGameObject);//track
+            }
+
+            return newGameObject;
+        }
+
+        public void AddItems(int amount = 1)
+        {
+            for (var i = amount - 1; i >= 0; --i)
+            {
+                var obj = CreatePoolable();
+                if (obj != null)
+                    Enpool(obj);
+            }
+        }
+
+        /// <summary>
+        /// Take an item out of the pool.
+        /// </summary>
+        /// <returns>Newly de-pool object.</returns>
+        public GameObject Depool()
+        {
+            GameObject depooledItem = null;
+
+            if (pool.Count > 0)
+                depooledItem = pool.Pop();
+            else
+                depooledItem = CreatePoolable(); //or not
+
+            if (depooledItem != null)
+                OnDepoolMethod?.Invoke(depooledItem);//by default SetsActive(true), like Instantiate
+
+            return depooledItem;
+        }
+
+        /// <summary>
+        /// Take an item out of the pool and place at world space with rotation.
+        /// </summary>
+        /// <returns>Newly de-pool object.</returns>
+        public GameObject Depool(Transform handle)
+        {
+            var obj = Depool();
             if (obj != null)
-                Enpool(obj);
+            {
+                var trans = obj.GetComponent<Transform>();
+                trans.position = handle.position;
+                trans.rotation = handle.rotation;
+                trans.parent = handle;
+
+                //maybe this is faster?
+                //trans.parent = handle;
+                //trans.localPosition = Vector3.zero;
+                //trans.localRotation = Quaternion.identity;
+            }
+            return obj;
         }
-    }
 
-    /// <summary>
-    /// Take an item out of the pool.
-    /// </summary>
-    /// <returns>Newly de-pool object.</returns>
-    public GameObject Depool()
-    {
-        GameObject depooledItem = null;
-
-        if (pool.Count > 0)
-            depooledItem = pool.Pop();
-        else
-            depooledItem = CreatePoolable(); //or not
-
-        if (depooledItem != null)
+        /// <summary>
+        /// Take an item out of the pool and place at world space with rotation.
+        /// </summary>
+        /// <returns>Newly de-pool object.</returns>
+        public GameObject Depool(Transform handle, bool setParent)
         {
-            if (initStragety == InitStrategy.OnDepool)
-                InitPoolableMethod(depooledItem);
-            OnDepoolMethod(depooledItem);//by default SetsActive(true), like Instantiate
+            GameObject obj = null;
+            if (setParent)
+                obj = Depool(handle);
+            else
+                obj = Depool(handle.position, handle.rotation);
+            return obj;
         }
 
-        return depooledItem;
-    }
-
-    /// <summary>
-    /// Take an item out of the pool and place at world space with rotation.
-    /// </summary>
-    /// <returns>Newly de-pool object.</returns>
-    public GameObject Depool(Transform handle)
-    {
-        var obj = Depool();
-        if (obj != null)
+        /// <summary>
+        /// Take an item out of the pool and place at world space.
+        /// </summary>
+        /// <returns>Newly de-pool object.</returns>
+        public GameObject Depool(in Vector3 position)
         {
-            var trans = obj.GetComponent<Transform>();
-            trans.position = handle.position;
-            trans.rotation = handle.rotation;
-            trans.parent = handle;
-
-            //maybe this is faster?
-            //trans.parent = handle;
-            //trans.localPosition = Vector3.zero;
-            //trans.localRotation = Quaternion.identity;
+            var obj = Depool();
+            if (obj != null)
+            {
+                var trans = obj.GetComponent<Transform>();
+                trans.position = position;
+            }
+            return obj;
         }
-        return obj;
-    }
 
-    /// <summary>
-    /// Take an item out of the pool and place at world space with rotation.
-    /// </summary>
-    /// <returns>Newly de-pool object.</returns>
-    public GameObject Depool(Transform handle, bool setParent)
-    {
-        GameObject obj = null;
-        if (setParent)
-            obj = Depool(handle);
-        else
-            obj = Depool(handle.position, handle.rotation);
-        return obj;
-    }
-
-    /// <summary>
-    /// Take an item out of the pool and place at world space.
-    /// </summary>
-    /// <returns>Newly de-pool object.</returns>
-    public GameObject Depool(in Vector3 position)
-    {
-        var obj = Depool();
-        if (obj != null)
+        /// <summary>
+        /// Take an item out of the pool and set at world space with orientation.
+        /// </summary>
+        /// <returns>Newly de-pool object.</returns>
+        public GameObject Depool(in Vector3 position, in Quaternion rotation)
         {
-            var trans = obj.GetComponent<Transform>();
-            trans.position = position;
+            var obj = Depool();
+            if (obj != null)
+            {
+                var trans = obj.GetComponent<Transform>();
+                trans.position = position;
+                trans.rotation = rotation;
+            }
+            return obj;
         }
-        return obj;
-    }
 
-    /// <summary>
-    /// Take an item out of the pool and set at world space with orientation.
-    /// </summary>
-    /// <returns>Newly de-pool object.</returns>
-    public GameObject Depool(in Vector3 position, in Quaternion rotation)
-    {
-        var obj = Depool();
-        if (obj != null)
+        /// <summary>
+        /// Take an item out of the pool and GetComponent{T} on it.
+        /// </summary>
+        /// <returns>Newly de-pool object or null if the Component wasn't found.</returns>
+        public T Depool<T>() where T : Component
         {
-            var trans = obj.GetComponent<Transform>();
-            trans.position = position;
-            trans.rotation = rotation;
+            T component = null;
+            var obj = Depool();
+            if (obj)
+                component = obj.GetComponent<T>();
+            return component;
         }
-        return obj;
-    }
 
-    /// <summary>
-    /// Take an item out of the pool and GetComponent{T} on it.
-    /// </summary>
-    /// <returns>Newly de-pool object or null if the Component wasn't found.</returns>
-    public T Depool<T>() where T : Component
-    {
-        T component = null;
-        var obj = Depool();
-        if (obj)
-            component = obj.GetComponent<T>();
-        return component;
-    }
+        /// <summary>
+        /// Useful for delegates/events.
+        /// lol. doodee pool.
+        /// </summary>
+        public void DoDepool() => Depool();
 
-    /// <summary>
-    /// Useful for delegates/events.
-    /// lol. doodee pool.
-    /// </summary>
-    public void DoDepool() => Depool();
+        /// <summary>
+        /// Useful for delegates/events.
+        /// lol. doodee pool.
+        /// </summary>
+        public void DoDepool(Transform handle) => Depool(handle);
 
-    /// <summary>
-    /// Useful for delegates/events.
-    /// lol. doodee pool.
-    /// </summary>
-    public void DoDepool(Transform handle) => Depool(handle);
+        /// <summary>
+        /// Useful for delegates/events.
+        /// lol. doodee pool.
+        /// </summary>
+        public void DoDepool(in Vector3 point) => Depool(point);
 
-    /// <summary>
-    /// Useful for delegates/events.
-    /// lol. doodee pool.
-    /// </summary>
-    public void DoDepool(in Vector3 point) => Depool(point);
+        public void Enpool(MonoBehaviour poolable)
+            => Enpool(poolable.gameObject);
 
-    public void Enpool(MonoBehaviour poolable)
-        => Enpool(poolable.gameObject);
-
-    /// <summary>
-    /// Add an item back into the pool. This replaces Destroy().
-    /// </summary>
-    /// <param name="poolable"></param>
-    public void Enpool(GameObject poolable)
-    {
-        Debug.Assert(poolable != null,
-            "[GameObjectPool] Trying to Enpool null!", this);
-
-        Debug.AssertFormat(manifest.Contains(poolable),
-            "[GameObjectPool] This item is not included on this Pool's manifest. " +
-            "This item probably belongs to another Pool. " +
-            "pendingPoolable: {0}. Pool: {1}. manifest[0] {2}.",
-            poolable, gameObject, manifest[0]);
-
-        if (!pool.Contains(poolable))//guard against multiple entries
+        /// <summary>
+        /// Add an item back into the pool. This replaces Destroy().
+        /// </summary>
+        /// <param name="poolable"></param>
+        public void Enpool(GameObject poolable)
         {
-            pool.Push(poolable);
-            OnEnpoolMethod(poolable);//by default sets inactive.
+            Debug.Assert(poolable != null,
+                "[GameObjectPool] Trying to Enpool null!", this);
+
+            Debug.AssertFormat(manifest.Contains(poolable),
+                "[GameObjectPool] This item is not included on this Pool's manifest. " +
+                "This item probably belongs to another Pool. " +
+                "pendingPoolable: {0}. Pool: {1}. manifest[0] {2}.",
+                poolable, gameObject, manifest[0]);
+
+            if (!pool.Contains(poolable))//guard against multiple entries
+            {
+                pool.Push(poolable);
+                OnEnpoolMethod?.Invoke(poolable);//by default sets inactive.
+            }
         }
-    }
 
-    /// <summary>
-    /// Create entire pool
-    /// </summary>
-    public void InitPool()
-	{
-		initOnAwake = false; //to prevent double-init'g
-		int poolSize = RichMath.Max(startingAmount, maxAmount);
-		manifest = new List<GameObject>(poolSize);
-        pool = new Stack<GameObject>(poolSize);
-
-        //preload pool
-        for (var i = startingAmount; i > 0; --i)
+        /// <summary>
+        /// Create entire pool
+        /// </summary>
+        public void InitPool()
         {
-            var newP = CreatePoolable();
+            initOnAwake = false; //to prevent double-init'g
+            int poolSize = RichMath.Max(startingAmount, maxAmount);
+            manifest = new List<GameObject>(poolSize);
+            pool = new Stack<GameObject>(poolSize);
 
-            if (initStragety == InitStrategy.OnCreate)
-                InitPoolableMethod(newP);
+            //preload pool
+            for (var i = startingAmount; i > 0; --i)
+            {
+                var newP = CreatePoolable();
+                InitPoolableMethod?.Invoke(newP);
 
-            pool.Push(newP);
-            OnEnpoolMethod(newP);//SetActive(false) by default
+                pool.Push(newP);
+                OnEnpoolMethod?.Invoke(newP);//SetActive(false) by default
+            }
         }
-    }
 
-	public void InitPool(int startingAmount, int maxAmount)
-	{
-		this.startingAmount = startingAmount;
-		this.maxAmount = maxAmount;
-		InitPool();
-	}
-
-    /// <summary>
-    /// Resize the pool.
-    /// </summary>
-    /// <param name="newCapacity"></param>
-    public void Resize(int newCapacity)
-    {
-        maxAmount = RichMath.Min(newCapacity, ReadyCount);
-        while (pool.Count > maxAmount) //shrink
+        public void InitPool(int startingAmount, int maxAmount)
         {
-            var poolable = pool.Pop();//trim excess
-            manifest.Remove(poolable);
-            Destroy(poolable.gameObject);
+            this.startingAmount = startingAmount;
+            this.maxAmount = maxAmount;
+            InitPool();
         }
 
-        manifest.TrimExcess();
-        pool.TrimExcess();
-    }
+        /// <summary>
+        /// Resize the pool.
+        /// </summary>
+        /// <param name="newCapacity"></param>
+        public void Resize(int newCapacity)
+        {
+            maxAmount = RichMath.Min(newCapacity, ReadyCount);
+            while (pool.Count > maxAmount) //shrink
+            {
+                var poolable = pool.Pop();//trim excess
+                manifest.Remove(poolable);
+                Destroy(poolable.gameObject);
+            }
 
-    public void ReturnAllToPool()
-    {
-		//iterate backwards to preserve stack order
-        for (var i = manifest.Count - 1; i >= 0; --i)
-            Enpool(manifest[i]);
+            manifest.TrimExcess();
+            pool.TrimExcess();
+        }
+
+        public void ReturnAllToPool()
+        {
+            //iterate backwards to preserve stack order
+            for (var i = manifest.Count - 1; i >= 0; --i)
+                Enpool(manifest[i]);
+        }
     }
 }
