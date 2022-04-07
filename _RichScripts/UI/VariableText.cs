4058de9b51b4d8eeb19875db2a7af0c0917db6c0
Monsoonexe <43690629+Monsoonexe@users.noreply.github.com>
2022-04-06 23:57:24 -0700

    Update VariableText.cs
    Odinize. Remove coupling to IntVariable. If you want to call its special functions, then use an IntDisplayer instead.

--- _RichScripts/UI/VariableText.cs
+++ _RichScripts/UI/VariableText.cs
@@ -1,41 +1,27 @@
 ﻿using UnityEngine;
 using TMPro;
 using ScriptableObjectArchitecture;
-using NaughtyAttributes;
+using Sirenix.OdinInspector;
 
 /// <summary>
 /// Updates a Text element based on value of a Variable.
 /// </summary>
 [SelectionBase]
-public class VariableText : VariableUIElement<BaseVariable>
+public sealed class VariableText : VariableUIElement<BaseVariable>
 {
-    [Header("---Scene Refs---")]
-    [SerializeField]
-    [Required]
+    [Title("Scene Refs")]
+    [SerializeField, Required]
     protected TextMeshProUGUI tmp;
     
-    private void Reset()
+    protected override void Reset()
     {
-        SetDevDescription("Updates a Text element based on value of a Variable.");
-        tmp = GetComponent<TextMeshProUGUI>();
+        base.Reset();
+        tmp = GetComponent<TextMeshProUGUI>(); //make a guess for convenience
     }
 
     /// <summary>
     /// Update UI elements with current values.
     /// </summary>
     public override void UpdateUI()
-    {
-        var outputString = string.Empty;
-
-        if(targetData is IntVariable intData)
-        {
-            outputString = ConstStrings.GetCachedString(intData);
-        }
-        else
-        {
-            outputString = targetData.ToString();
-        }
-
-        tmp.text = outputString; 
-    }
+        => tmp.text = targetData.ToString();
 }
