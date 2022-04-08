75c9bb4f181a2b35470e7b18d86d1d8e8efbad5d
Richard Osborn <RichardOsborn256@gmail.com>
2022-04-07 17:33:50 -0700

    Create Button_ContextMenu.cs

---
+++ Editor/Button_ContextMenu.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine.UI;
+using UnityEditor;
+
+/// <summary>
+/// 
+/// </summary>
+public static class Button_ContextMenu
+{
+	[MenuItem("CONTEXT/Button/Force Invoke")]
+	private static void ForceInvokeEvent(MenuCommand command)
+	{
+		Button button = command.context as Button;
+		button.onClick.Invoke();
+	}
+}
