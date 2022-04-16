d8202a72495308096714a4125b6a970244c672ad
Richard Osborn <RichardOsborn256@gmail.com>
2022-04-16 14:37:12 -0700

    Cache 'name' property with Name

--- ThirdParty/SO Architecture - Fork/Utility/SOArchitectureBaseMonobehaviour.cs
+++ ThirdParty/SO Architecture - Fork/Utility/SOArchitectureBaseMonobehaviour.cs
@@ -12,5 +12,13 @@ namespace ScriptableObjectArchitecture
         [SerializeField]
         private DeveloperDescription DeveloperDescription = new DeveloperDescription();
 #pragma warning restore
+
+        private string _name;
+
+        public string Name 
+        {
+            get => string.IsNullOrEmpty(_name) ? (_name = name) : _name;
+            set => _name = value;
+        }
     } 
 }
