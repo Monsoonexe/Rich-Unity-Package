07b042beade2181c008ad0be73d27ee9d7c74864
Richard Osborn <apexdevelop003@gmail.com>
2022-04-25 14:29:56 -0700

    Update String_Extensions.cs
    Add RemoveWhitespace
    Remove 'throws'. prefer GuardClauses.

--- _RichScripts/Static Classes/Extensions/String_Extensions.cs
+++ _RichScripts/Static Classes/Extensions/String_Extensions.cs
@@ -1,51 +1,51 @@
 ﻿using System;
 using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
 
-namespace RichPackage
+public static class String_Extensions
 {
-    public static class String_Extensions
+    public static readonly Regex sWhitespace = new Regex(@"\s+");
+
+    /// <summary>
+    /// If the size of a string is larger than this amount, it's probably better to use a 
+    /// <see cref="Regex"/>. Arbitrarily chosen; please tune.
+    /// </summary>
+    private const int REGEX_SIZE_LIMIT = 5000;
+
+    /// <summary>
+    /// More performant than <see cref="string.IsNullOrEmpty"/> because it's branchless.
+    /// </summary>
+    /// <param name="str"></param>
+    /// <returns></returns>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static bool IsNullOrEmpty(this string str)
+        => str == null || str.Length == 0;
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static bool IsNullOrEmptyOrWhiteSpace(this string str)
+        => IsNullOrEmpty(str) || IsWhiteSpace(str);
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static bool IsWhiteSpace(this string str)
     {
-        private const string DEFAULT_ARGUMENT_NAME = "Argument";
+        int len = str.Length; //cache for re-use
+        for (int i = 0; i < len; i++)
+            if (!char.IsWhiteSpace(str[i]))
+                return false;
+        return true;
+    }
 
-        /// <summary>
-        /// More performant than <see cref="string.IsNullOrEmpty"/> because it's branchless.
-        /// </summary>
-        /// <param name="str"></param>
-        /// <returns></returns>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static bool IsNullOrEmpty(this string str)
-            => str == null || str.Length == 0;
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static string RemoveWhitespace(this string str)
+    {
+        string output;
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static bool IsNullOrEmptyOrWhiteSpace(this string str)
-            => IsNullOrEmpty(str) || IsWhiteSpace(str);
+        if (str.Length > REGEX_SIZE_LIMIT)
+            output = sWhitespace.Replace(str, string.Empty);
+        else 
+            output = string.Join(string.Empty, str.Split(default(string[]),
+                StringSplitOptions.RemoveEmptyEntries));
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static bool IsWhiteSpace(this string str)
-        {
-            int len = str.Length; //cache for re-use
-            for (int i = 0; i < len; i++)
-                if (!char.IsWhiteSpace(str[i]))
-                    return false;
-            return true;
-        }
-
-        #region Throws
-       
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static void ThrowIfNullOrEmpty(this string str, string argumentName = DEFAULT_ARGUMENT_NAME)
-        {
-            if (str.IsNullOrEmpty())
-                throw new ArgumentException($"{argumentName} is null or empty.");
-        }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static void ThrowIfNullOrEmptyOrWhiteSpace(this string str, string argumentName = DEFAULT_ARGUMENT_NAME)
-        {
-            if (str.IsNullOrEmptyOrWhiteSpace())
-                throw new ArgumentException($"{argumentName} is null or empty or whitespace.");
-        }
-
-        #endregion
+        return output;
     }
 }
