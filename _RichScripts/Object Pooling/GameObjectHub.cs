e3352c8237484f47fd4df35b59f140738e7ca9bb
Richard Osborn <RichardOsborn256@gmail.com>
2022-05-01 11:59:15 -0700

    Add to ObjectPooling namespace

--- _RichScripts/Object Pooling/GameObjectHub.cs
+++ _RichScripts/Object Pooling/GameObjectHub.cs
@@ -1,54 +1,63 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 
-/// <summary>
-/// 
-/// </summary>
-public class GameObjectHub : RichMonoBehaviour
+namespace RichPackage.ObjectPooling
 {
-    private readonly Dictionary<string, GameObject>
-        poolTable = new Dictionary<string, GameObject>(); //lol. it's not what you think
-
-    public GameObject Get(string key)
+    public class GameObjectHub : RichMonoBehaviour
     {
-        GameObject obj = null;
+        private readonly Dictionary<string, GameObject>
+            poolTable = new Dictionary<string, GameObject>(); //lol. it's not what you think
 
-        if (!poolTable.TryGetValue(key, out obj))
+        public GameObject Get(string key)
         {
-            Debug.LogError("[PoolHub] Pool <" + key + "> not found." +
-                " Add this to the pool prior to access.");
+            GameObject obj = null;
+
+            if (!poolTable.TryGetValue(key, out obj))
+            {
+                Debug.LogError($"[{nameof(GameObjectHub)}] Pool <{key}> not found." +
+                    " Add this to the pool prior to access.");
+            }
+
+            return obj;
         }
 
-        return obj;
+        /// <summary>
+        /// Get a pool of prefabs.
+        /// </summary>
+        public U Get<U>(string key)
+            where U : Component
+        {
+            Component comp = null;
+
+            var obj = Get(key);
+            if (obj != null)
+                comp = obj.GetComponent<U>();
+
+            return comp as U;
+        }
+
+        /// <summary>
+        /// Keyed by name.
+        /// </summary>
+        public void Add(string key, GameObject newObj)
+            => poolTable.Add(key, newObj);
+
+        /// <summary>
+        /// Keyed by name.
+        /// </summary>
+        public void Add(GameObject newObj)
+            => poolTable.Add(newObj.name, newObj);
+
+        /// <summary>
+        /// Keyed by name.
+        /// </summary>
+        public void Remove(GameObject newObj)
+            => poolTable.Remove(newObj.name);
+
+        /// <summary>
+        /// Keyed by name.
+        /// </summary>
+        public void Remove(string key)
+            => poolTable.Remove(key);
     }
-
-    public U Get<U>(string key) 
-        where U : Component
-    {
-        Component comp = null;
-
-        var obj = Get(key);
-        if (obj != null)
-            comp = obj.GetComponent<U>();
-
-        return comp as U;
-    }
-
-    public void Add(string key, GameObject newObj)
-        => poolTable.Add(key, newObj);
-
-    /// <summary>
-    /// Keyed by name.
-    /// </summary>
-    public void Add(GameObject newObj)
-        => poolTable.Add(newObj.name, newObj);
-
-    /// <summary>
-    /// Keyed by name.
-    /// </summary>
-    public void Remove(GameObject newObj)
-        => poolTable.Remove(newObj.name);
-
-    public void Remove(string key)
-        => poolTable.Remove(key);
 }
