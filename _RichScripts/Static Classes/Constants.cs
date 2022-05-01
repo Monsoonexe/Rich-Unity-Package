855385e7c2e3c0f378dd83ca186facf588ea51f7
Richard Osborn <RichardOsborn256@gmail.com>
2022-05-01 11:59:24 -0700

    Create Constants.cs

---
+++ _RichScripts/Static Classes/Constants.cs
@@ -0,0 +1,15 @@
+﻿
+namespace RichPackage
+{
+	/// <summary>
+	/// Helpful constants.
+	/// </summary>
+	public static class Constants
+	{
+		public const int BytesInKilobytes = 1024;
+
+		public const int BytesInMegabytes = 1024 * 1024;
+
+		public const int BytesInGigabytes = 1024 * 1024 * 1024;
+	}
+}
