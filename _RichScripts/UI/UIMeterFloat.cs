3c7f1c11df88a4a2ea9fb80fe22fd550e15873e8
Richard Osborn <RichardOsborn256@gmail.com>
2022-05-01 11:59:35 -0700

    Add to UI namespace

--- _RichScripts/UI/UIMeterFloat.cs
+++ _RichScripts/UI/UIMeterFloat.cs
@@ -1,48 +1,63 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using ScriptableObjectArchitecture;
-using NaughtyAttributes;
+using Sirenix.OdinInspector;
 
-/// <summary>
-/// A meter in UI space that fills up based on a FloatVariable's value.
-/// </summary>
-[SelectionBase]
-public class UIMeterFloat : VariableUIElement<FloatVariable>
+namespace RichPackage.UI
 {
-    [Header("---Prefab Refs---")]
-    [Required]
-    [SerializeField]
-    private Image myImage;
-
     /// <summary>
-    /// Change sprite used to fill image.
+    /// A meter in UI space that fills up based on a <see cref="FloatVariable"/>'s value.
     /// </summary>
-    public Sprite FillSprite { get => myImage.sprite; set => myImage.sprite = value; }
-
-    /// <summary>
-    /// Change tint of image (Image.color).
-    /// </summary>
-    public Color FillTint { get => myImage.color; set => myImage.color = value; }
-
-    public void OnValidate()
+    [SelectionBase]
+    public class UIMeterFloat : VariableUIElement<FloatVariable>
     {
-        if (targetData != null && !targetData.Clampable)
-            Debug.LogError("[UIMeterFloat] sourceValue is not clampable! " + targetData.name, this);
-    }
+        [Header("---Prefab Refs---")]
+        [SerializeField, Required]
+        private Image myImage;
 
-    /// <summary>
-    /// Refresh UI element with current data values.
-    /// </summary>
-    public override void UpdateUI()
-    {
-        var min = targetData.MinClampValue; //cache
-        float range = targetData.MaxClampValue - min;
-        myImage.fillAmount = (targetData - min) / range;
-    }
+        /// <summary>
+        /// Change sprite used to fill image.
+        /// </summary>
+        public Sprite FillSprite { get => myImage.sprite; set => myImage.sprite = value; }
 
-    public override void ToggleVisuals(bool active)
-        => myImage.enabled = active;
+        /// <summary>
+        /// Change tint of image (Image.color).
+        /// </summary>
+        public Color FillTint { get => myImage.color; set => myImage.color = value; }
 
-    public override void ToggleVisuals()
-        => myImage.enabled = !myImage.enabled;
+        #region Unity Messages
+
+        private void Reset()
+        {
+            SetDevDescription("A meter in UI space that fills up based on a FloatVariable's value.");
+        }
+
+        public void OnValidate()
+        {
+            if (targetData != null && !targetData.Clampable)
+                Debug.LogError("[UIMeterFloat] sourceValue is not clampable! " + targetData.name, this);
+        }
+
+        #endregion Unity Messages
+
+        #region RichUIElement
+
+        /// <summary>
+        /// Refresh UI element with current data values.
+        /// </summary>
+        public override void UpdateUI()
+        {
+            float min = targetData.MinClampValue; //cache
+            float range = targetData.MaxClampValue - min;
+            myImage.fillAmount = (targetData - min) / range;
+        }
+
+        public override void ToggleVisuals(bool active)
+            => myImage.enabled = active;
+
+        public override void ToggleVisuals()
+            => myImage.enabled = !myImage.enabled;
+
+		#endregion RichUIElement
+	}
 }
