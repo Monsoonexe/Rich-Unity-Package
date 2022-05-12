5da5b1d45894cc28b89f458e51c19950c3358060
Richard Osborn <apexdevelop003@gmail.com>
2022-05-12 09:57:38 -0700

    Add EnumUtilities.cs

---
+++ _RichScripts/Static Classes/EnumUtilities.cs
@@ -0,0 +1,20 @@
+using System;
+
+public static class EnumUtilities
+{
+    /// <summary>
+    /// Creates a dictionary that maps enums to their string representations.
+    /// Useful because reflection is expensive and this operation caches the results.
+    /// </summary>
+    public static Dictionary<int, string> EnumNamedValues<TEnum>()
+        where TEnum : Enum
+    {
+        var values = Enum.GetValues(typeof(TEnum));
+        var result = new Dictionary<int, string>(values.Length); //return value
+
+        foreach (int item in values)
+            result.Add(item, Enum.GetName(typeof(TEnum), item));
+
+        return result;
+    }
+}
