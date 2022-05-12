e1118c67d1a183c0ec74ae49834006e10bf84743
Richard Osborn <apexdevelop003@gmail.com>
2022-05-12 13:55:50 -0700

    Create QueueExtensions.cs

---
+++ _RichScripts/Static Classes/Extensions/QueueExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.CompilerServices;
+using System.Collections.Generic;
+
+namespace ApexCommon
+{
+    /// <seealso cref="StackExtensions"/>
+    internal static class QueueExtensions
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void EnqueueRange<T>(this Queue<T> q, IEnumerable<T> e)
+        {
+            foreach (T i in e)
+                q.Enqueue(i);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool IsEmpty<T>(this Queue<T> q)
+            => q.Count == 0;
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool IsNotEmpty<T>(this Queue<T> q)
+            => q.Count > 0;
+    }
+}
