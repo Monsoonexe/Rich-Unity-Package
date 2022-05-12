8ed90331f711335874c7a82c7d274b1b87c947d9
Richard Osborn <apexdevelop003@gmail.com>
2022-05-12 13:55:54 -0700

    Create StackExtensions.cs

---
+++ _RichScripts/Static Classes/Extensions/StackExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.CompilerServices;
+using System.Collections.Generic;
+
+namespace ApexCommon
+{
+    /// <seealso cref="QueueExtensions"/>
+    internal static class StackExtensions
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void PushRange<T>(this Stack<T> s, IEnumerable<T> e)
+        {
+            foreach (T i in e)
+                s.Push(i);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool IsEmpty<T>(this Stack<T> s)
+            => s.Count == 0;
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool IsNotEmpty<T>(this Stack<T> s)
+            => s.Count > 0;
+    }
+}
