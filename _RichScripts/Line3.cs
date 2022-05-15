b069da86d3729954449486b3b211713e4818cb87
Richard Osborn <RichardOsborn256@gmail.com>
2022-05-15 15:50:51 -0700

    Add to RichPacakge

--- _RichScripts/Line3.cs
+++ _RichScripts/Line3.cs
@@ -1,29 +1,32 @@
 ﻿using System;
 using UnityEngine;
 
-/// <summary>
-/// A line is between two points in space, A and B. It's not a ray, cuz no direction.
-/// </summary>
-[Serializable]
-public struct Line3
+namespace RichPackage
 {
     /// <summary>
-    /// Start Point
+    /// A line is between two points in space, A and B. It's not a ray, cuz no direction.
     /// </summary>
-    public Vector3 A;//like start point
-
-    /// <summary>
-    /// End Point
-    /// </summary>
-    public Vector3 B;//and endpoint
-	
-	#region Constructors
-	
-	public Line3(Vector3 a, Vector3 b)
+    [Serializable]
+    public struct Line3
     {
-        A = a;
-        B = b;
+        /// <summary>
+        /// Start Point
+        /// </summary>
+        public Vector3 A;//like start point
+
+        /// <summary>
+        /// End Point
+        /// </summary>
+        public Vector3 B;//and endpoint
+        
+        #region Constructors
+        
+        public Line3(Vector3 a, Vector3 b)
+        {
+            A = a;
+            B = b;
+        }
+        
+        #endregion
     }
-	
-	#endregion
 }
