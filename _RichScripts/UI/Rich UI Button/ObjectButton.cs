88cc4f1fe2f7174e886a21f361121173317f2e2a
Richard Osborn <RichardOsborn256@gmail.com>
2022-05-16 19:19:47 -0700

    Create ObjectButton.cs

---
+++ _RichScripts/UI/Rich UI Button/ObjectButton.cs
@@ -0,0 +1,11 @@
+﻿
+namespace RichPackage.UI
+{
+	/// <summary>
+	/// A button that raises an object event when pressed.
+	/// </summary>
+	public class IntButton : RichUIButton<object>
+	{
+		//exists
+	}
+}
