fae8bd8c0115cd9ee52f6d95b3c208a3458c2057
Richard Osborn <RichardOsborn256@gmail.com>
2022-05-19 23:52:08 -0700

    Fix and add to Objectpooling namespace

--- _RichScripts/Object Pooling/ComponentHub.cs
+++ _RichScripts/Object Pooling/ComponentHub.cs
@@ -1,54 +1,53 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 
-/// <summary>
-/// A locator for objects that add themselves to hub.
-/// </summary>
-public abstract class ADictionaryComponent<T> : RichMonoBehaviour
+namespace RichPackage.ObjectPooling
 {
-    private readonly Dictionary<string, T>
-        table = new Dictionary<string, T>(4); //lol. it's not what you think
-
-    public T Get(string key)
+    /// <summary>
+    /// A locator for objects that add themselves to hub.
+    /// </summary>
+    public abstract class ADictionaryComponent<T> : RichMonoBehaviour
     {
-        if (!table.TryGetValue(key, out T value))
+        private readonly Dictionary<string, T>
+            table = new Dictionary<string, T>(4); //lol. it's not what you think
+
+        public T Get(string key)
         {
-            Debug.LogError($"[{nameof(ADictionaryComponent)}] {key} not found.");
+            if (!table.TryGetValue(key, out T value))
+            {
+                Debug.LogError($"[{nameof(ADictionaryComponent<T>)}] " +
+					$"{key} not found.");
+            }
+
+            return value;
         }
 
-        return value;
+        public U Get<U>(string key)
+            where U : class
+            => Get(key) as U;
+
+        /// <summary>
+        /// Keyed by name.
+        /// </summary>
+        public void Add(string key, T value)
+            => table.Add(key, value);
+
+        /// <summary>
+        /// Keyed by name.
+        /// </summary>
+        public void Remove(string key)
+            => table.Remove(key);
     }
 
-    public U Get<U>(string key) 
-        => Get(key) as U;
-
-    public void Add(string key, T value)
-        => table.Add(key, value);
-
     /// <summary>
-    /// Keyed by name.
+    /// A locator for objects that add themselves to hub.
     /// </summary>
-    public void Add(string key, T value)
-        => table.Add(key, value);
+    public class ComponentHub : ADictionaryComponent<Component>
+    {
+        public void Add(Component comp)
+            => Add(comp.name, comp);
 
-    /// <summary>
-    /// Keyed by name.
-    /// </summary>
-    public void Remove(string key)
-        => table.Remove(key);
-
-    public void Remove(string key)
-        => table.Remove(key);
-}
-
-/// <summary>
-/// A locator for objects that add themselves to hub.
-/// </summary>
-public class ComponentHub : ComponentHub<Component>
-{
-    public void Add(Component comp)
-        => Add(comp.name, comp);
-
-    public void Remove(Component comp)
-        => Remove(comp.name, comp);
+        public void Remove(Component comp)
+            => Remove(comp.name);
+    }
 }
