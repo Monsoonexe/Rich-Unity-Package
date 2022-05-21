141660d973a3759c1c677cb6ead19e722c28b993
Richard Osborn <RichardOsborn256@gmail.com>
2022-05-20 23:34:15 -0700

    add to RIchPackage namespace

--- _RichScripts/LevelController.cs
+++ _RichScripts/LevelController.cs
@@ -2,56 +2,59 @@
 using ScriptableObjectArchitecture;
 using Sirenix.OdinInspector;
 
-/// <summary>
-/// I control the events and behaviour of this specifc level.
-/// </summary>
-public class LevelController : RichMonoBehaviour
+namespace RichPackage
 {
-    //member functions for events to hook onto.
-
-    private void Reset()
+    /// <summary>
+    /// I control the events and behaviour of this specifc level.
+    /// </summary>
+    public class LevelController : RichMonoBehaviour
     {
-        SetDevDescription("I control the events and behaviour of this specifc level.");
+        //member functions for events to hook onto.
+
+        private void Reset()
+        {
+            SetDevDescription("I control the events and behaviour of this specifc level.");
+        }
+
+        [Button, DisableInEditorMode]
+        public void LoadLevel(SceneVariable sceneVariable)
+            => SceneManager.LoadScene(sceneVariable.Value.SceneIndex);
+
+        [Button, DisableInEditorMode]
+        public void LoadLevel(string name)
+            => SceneManager.LoadScene(name);
+
+        [Button, DisableInEditorMode]
+        public void LoadLevel(int index)
+            => SceneManager.LoadScene(index);
+
+        #region Static Interface
+
+        [Button("LoadLevelImmediately"), DisableInEditorMode]//, ConsoleCommand("loadLevel")]
+        public static void LoadLevel_(int levelIndex)
+        {
+            SceneManager.LoadScene(levelIndex);
+        }
+
+        [Button, DisableInEditorMode]//, ConsoleCommand("loadNextLevel")]
+        public static void LoadNextLevel()
+        {
+            var currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+            SceneManager.LoadScene(currentSceneIndex + 1);
+        }
+
+        [Button, DisableInEditorMode]//, ConsoleCommand("loadPreviousLevel")]
+        public static void LoadPreviousLevel()
+        {
+            var currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+            SceneManager.LoadScene(currentSceneIndex - 1);
+        }
+
+        [Button, DisableInEditorMode]//, ConsoleCommand("reloadLevel")]
+        public static void ReloadCurrentLevel()
+            => SceneManager.LoadScene(
+                SceneManager.GetActiveScene().buildIndex);
+
+        #endregion
     }
-
-    [Button, DisableInEditorMode]
-    public void LoadLevel(SceneVariable sceneVariable)
-        => SceneManager.LoadScene(sceneVariable.Value.SceneIndex);
-
-    [Button, DisableInEditorMode]
-    public void LoadLevel(string name)
-        => SceneManager.LoadScene(name);
-
-    [Button, DisableInEditorMode]
-    public void LoadLevel(int index)
-        => SceneManager.LoadScene(index);
-
-    #region Static Interface
-
-    [Button("LoadLevelImmediately"), DisableInEditorMode]//, ConsoleCommand("loadLevel")]
-    public static void LoadLevel_(int levelIndex)
-    {
-        SceneManager.LoadScene(levelIndex);
-    }
-
-    [Button, DisableInEditorMode]//, ConsoleCommand("loadNextLevel")]
-    public static void LoadNextLevel()
-    {
-        var currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
-        SceneManager.LoadScene(currentSceneIndex + 1);
-    }
-
-    [Button, DisableInEditorMode]//, ConsoleCommand("loadPreviousLevel")]
-    public static void LoadPreviousLevel()
-    {
-        var currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
-        SceneManager.LoadScene(currentSceneIndex - 1);
-    }
-
-    [Button, DisableInEditorMode]//, ConsoleCommand("reloadLevel")]
-    public static void ReloadCurrentLevel()
-        => SceneManager.LoadScene(
-            SceneManager.GetActiveScene().buildIndex);
-
-    #endregion
 }
