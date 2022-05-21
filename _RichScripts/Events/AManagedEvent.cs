423461f37d3caf904b9dcdcd3aa0df2e3e20c0c2
Richard Osborn <RichardOsborn256@gmail.com>
2022-05-20 23:33:23 -0700

    Fix base class name

--- _RichScripts/Events/AManagedEvent.cs
+++ _RichScripts/Events/AManagedEvent.cs
@@ -1,10 +1,11 @@
-﻿
+﻿using RichPackage.UnityMessages;
+
 namespace RichPackage.Managed
 {
     /// <summary>
     /// 
     /// </summary>
-    public abstract class AManagedEvent : AUnityLifetimeEvent
+    public abstract class AManagedEvent : AUnityLifetimeMessage
     {
         ////override this because specific is better than vague for this system.
         //protected virtual void OnEnable()
