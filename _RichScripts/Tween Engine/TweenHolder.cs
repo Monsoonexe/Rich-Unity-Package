661800bd48cb9d6829032864eaaabd4970faad04
Richard Osborn <RichardOsborn256@gmail.com>
2022-05-20 23:38:06 -0700

    Add to Tweening namespace

--- _RichScripts/Tween Engine/TweenHolder.cs
+++ _RichScripts/Tween Engine/TweenHolder.cs
@@ -1,39 +1,42 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
-/// <summary>
-/// I am a utility class to hold tweens so you don't have to!
-/// </summary>
-/// <seealso cref="ApexTweens"/>
-public class TweenHolder : RichMonoBehaviour
+namespace RichPackage.Tweening
 {
-    private void Reset()
-    { 
-        SetDevDescription("I am a utility class to hold tweens" +
-            " so you don't have to!");
-    }
+    /// <summary>
+    /// I am a utility class to hold tweens so you don't have to!
+    /// </summary>
+    /// <seealso cref="ApexTweens"/>
+    public class TweenHolder : RichMonoBehaviour
+    {
+        private void Reset()
+        {
+            SetDevDescription("I am a utility class to hold tweens" +
+                " so you don't have to!");
+        }
 
-    private void Start()
-    {
-        SceneManager.sceneLoaded += OnLevelLoadedHandler;
-    }
+        private void Start()
+        {
+            SceneManager.sceneLoaded += OnLevelLoadedHandler;
+        }
 
-    private void OnDestroy()
-    {
-        SceneManager.sceneLoaded -= OnLevelLoadedHandler;
-    }
+        private void OnDestroy()
+        {
+            SceneManager.sceneLoaded -= OnLevelLoadedHandler;
+        }
 
-    private void OnLevelLoadedHandler(
-        Scene scene, LoadSceneMode mode)
-    {
-        StopAllCoroutines();
-    }
-    
-    public static TweenHolder Construct()
-    {
-        var tweenObj = new GameObject("RichTween TweenHolder");
-        var tweenHolder = tweenObj.AddComponent<TweenHolder>();
-        DontDestroyOnLoad(tweenObj);
-        return tweenHolder;
+        private void OnLevelLoadedHandler(
+            Scene scene, LoadSceneMode mode)
+        {
+            StopAllCoroutines();
+        }
+
+        public static TweenHolder Construct()
+        {
+            var tweenObj = new GameObject("RichTween TweenHolder");
+            var tweenHolder = tweenObj.AddComponent<TweenHolder>();
+            DontDestroyOnLoad(tweenObj);
+            return tweenHolder;
+        }
     }
 }
