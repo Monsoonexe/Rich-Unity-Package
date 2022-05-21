dc6b58afe258628f1ed731232db47c06409e5c33
Richard Osborn <RichardOsborn256@gmail.com>
2022-05-20 23:39:53 -0700

    Prefer Quantum Console

--- Editor/PlayerPrefsEditor.cs
+++ Editor/PlayerPrefsEditor.cs
@@ -1,8 +1,8 @@
 ﻿using UnityEngine;
 using UnityEditor;
-using UnityConsole;
 using Sirenix.OdinInspector;
 using Sirenix.OdinInspector.Editor;
+using QFSW.QC;
 
 //https://docs.unity3d.com/ScriptReference/PlayerPrefs.html
 
@@ -24,28 +24,28 @@ namespace RichPackage.Editor
 			Instance.Show();
 		}
 
-		[ConsoleCommand, Button]
+		[Command, Button]
 		public static void SetInt(string key, int value)
 		{
 			PlayerPrefs.SetInt(key, value);
 			Debug.Log($"Saved {key}: {value}.");
 		}
 
-		[ConsoleCommand, Button]
+		[Command, Button]
 		public static void SetString(string key, string value)
 		{
 			PlayerPrefs.SetString(key, value);
 			Debug.Log($"Saved {key}: {value}.");
 		}
 
-		[ConsoleCommand, Button]
+		[Command, Button]
 		public static void SetFloat(string key, float value)
 		{
 			PlayerPrefs.SetFloat(key, value);
 			Debug.Log($"Saved {key}: {value}.");
 		}
 
-		[ConsoleCommand, Button]
+		[Command, Button]
 		public static void GetInt(string key)
 		{
 			if (PlayerPrefs.HasKey(key))
@@ -59,7 +59,7 @@ namespace RichPackage.Editor
 			}
 		}
 
-		[ConsoleCommand, Button]
+		[Command, Button]
 		public static void GetFloat(string key)
 		{
 			if (PlayerPrefs.HasKey(key))
@@ -73,7 +73,7 @@ namespace RichPackage.Editor
 			}
 		}
 
-		[ConsoleCommand, Button]
+		[Command, Button]
 		public static void GetString(string key)
 		{
 			if (PlayerPrefs.HasKey(key))
@@ -87,14 +87,14 @@ namespace RichPackage.Editor
 			}
 		}
 
-		[ConsoleCommand, Button]
+		[Command, Button]
 		public static void HasKey(string key)
 		{
 			bool has = PlayerPrefs.HasKey(key);
 			Debug.Log($"Key <{key}> exists: {has}.");
 		}
 
-		[ConsoleCommand, Button]
+		[Command, Button]
 		public static void DeleteKey(string key)
 		{
 			bool contains = PlayerPrefs.HasKey(key);
@@ -105,7 +105,7 @@ namespace RichPackage.Editor
 			Debug.Log(contains ? $"Key <{key}> was deleted." : $"Key <{key}> not found.");
 		}
 
-		[ConsoleCommand, Button, MenuItem(MenuPath + nameof(DeleteAllKeys))]
+		[Command, Button, MenuItem(MenuPath + nameof(DeleteAllKeys))]
 		public static void DeleteAllKeys()
 		{
 			PlayerPrefs.DeleteAll();
