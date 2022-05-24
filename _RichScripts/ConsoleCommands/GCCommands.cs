6ba49470eb26b5ffc49501f8c4bda5b5d61d2236
Richard Osborn <RichardOsborn256@gmail.com>
2022-05-23 23:44:48 -0700

    New console commands folder and GCCommands script

---
+++ _RichScripts/ConsoleCommands/GCCommands.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+using QFSW.QC;
+
+namespace RichPackage.ConsoleCommands
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	[CommandPrefix("GC.")]
+	public static class GCCommands
+	{
+		[Command, CommandDescription("Invokes the C# garbage collector and prints runtime.")]
+		public static void Collect()
+		{
+			Debug.Log("Invoking C# runtime GC...");
+			var startTime = DateTime.Now;
+			GC.Collect();
+			var runtime = DateTime.Now - startTime;
+			Debug.Log($"Done! ({runtime.TotalMilliseconds}ms)");
+		}
+
+		[Command, CommandDescription("Allocates a `new byte[size] * count`.")]
+		public static void Allocate(int size, int count = 1)
+		{
+			for(int i = 0; i < count; i++)
+			{
+				var bytes = new byte[size];
+			}
+		}
+	}
+}
