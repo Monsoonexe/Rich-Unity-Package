ae7bf44f254577367f91f5ff3df968456a0dd56a
Richard Osborn <RichardOsborn256@gmail.com>
2022-05-24 01:36:42 -0700

    Create EUnityMessage.cs

---
+++ _RichScripts/Events/EUnityMessage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace RichPackage.UnityMessages
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	[Flags]
+	public enum EUnityMessage
+	{
+		None = 1 << 0,
+		Start = 1 << 1,
+		Awake = 1 << 2,
+		OnEnable = 1 << 3,
+		OnDisable = 1 << 4,
+		OnDestroy = 1 << 5,
+		OnApplicationQuit = 1 << 6,
+		EarlyUpdate = 1 << 7,
+		Update = 1 << 8,
+		FixedUpdate = 1 << 9,
+		LateUpdate = 1 << 10,
+		OnApplicationPause = 1 << 11,
+	}
+}
