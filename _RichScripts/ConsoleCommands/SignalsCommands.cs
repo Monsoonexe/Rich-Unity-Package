79fbd8b16230e7d3cde5a11a51be639f72d3e0d8
Richard Osborn <RichardOsborn256@gmail.com>
2022-05-24 00:46:22 -0700

    Create SignalsCommands.cs

---
+++ _RichScripts/ConsoleCommands/SignalsCommands.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using QFSW.QC;
+using RichPackage.Events.Signals;
+
+[CommandPrefix("signals.")]
+public static class SignalsCommands
+{
+	[Command(aliasOverride:"dispatch"),
+		CommandDescription("Invokes the given signal registered with GlobalSignals.")]
+	public static void Dispatch(string signalHash)
+	{
+		if (GlobalSignals.Get(signalHash) is ASignal sig)
+		{
+			Debug.Log("Dispatching " + signalHash);
+			sig.Dispatch();
+		}
+		else
+		{
+			string fullyQualifiedName = nameof(RichPackage) + "." + signalHash;
+
+			if (GlobalSignals.Get(fullyQualifiedName) is ASignal sign)
+			{
+				Debug.Log("Dispatching " + fullyQualifiedName);
+				sign.Dispatch();
+			}
+			else
+			{
+				Debug.Log(signalHash + " is not supported or found.");
+			}
+		}
+	}
+}
