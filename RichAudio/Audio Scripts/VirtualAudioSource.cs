acc0ae612c7449435d2ca84bd2e1c407782b5c3b
Richard Osborn <RichardOsborn256@gmail.com>
2022-05-23 19:58:42 -0700

    Absorb SceneMusic.cs. Odinize

--- RichAudio/Audio Scripts/VirtualAudioSource.cs
+++ RichAudio/Audio Scripts/VirtualAudioSource.cs
@@ -1,25 +1,41 @@
 ﻿using UnityEngine;
+using Sirenix.OdinInspector;
 
 namespace RichPackage.Audio
 {
     /// <summary>
     /// A Mono that will play audio from static AudioManager.
     /// Helpful for rigging events and prefabs to audio that aren't
-    /// yet aware of an AudioManager in a scene.
+    /// yet aware of an <see cref="AudioManager"/> in a scene.
     /// </summary>
     /// <seealso cref="AudioManager"/>
     public class VirtualAudioSource : RichMonoBehaviour
     {
-        public RichAudioClip clip;
+        [Title("Options")]
         public bool playOnEnable = false;
         public bool stopOnDisable = false;
 
+        [Title(nameof(clip))]
+        [InlineProperty, HideLabel]
+        public RichAudioClipReference clip
+            = new RichAudioClipReference(AudioOptions.DefaultSFX);
+
+        //runtime fields
         private AudioID audioID;
+
+        #region Unity Messages
+
+        private void Reset()
+        {
+            SetDevDescription(" A Mono that will play audio from static " +
+				"AudioManager Helpful for rigging events and prefabs to audio" +
+				" that aren't yet aware of an AudioManager in a scene..");
+        }
 
         private void OnEnable()
         {
             if (playOnEnable)
-                PlaySFX(clip.AudioClip, clip.Options);
+                PlaySFX(clip.Clip, clip.Options);
         }
 
         private void OnDisable()
@@ -28,43 +44,41 @@ namespace RichPackage.Audio
                 StopSFX();
         }
 
-        public AudioID PlayBackgroundTrack()
+        #endregion Unity Messages
+
+        [Button, HorizontalGroup("butts"), DisableInEditorMode]
+        public void PlayBGM()
             => audioID = AudioManager.PlayBackgroundTrack(clip);
 
-        public AudioID PlayBackgroundTrack(AudioClip clip)
+        public void PlayBGM(AudioClip clip)
             => audioID = AudioManager.PlayBackgroundTrack(clip);
 
-        public AudioID PlayBackgroundTrack(AudioClip clip,
-            AudioOptions options)
+        public void PlayBGM(AudioClip clip, in AudioOptions options)
             => audioID = AudioManager.PlayBackgroundTrack(clip, options);
 
+        [Button, HorizontalGroup("butts"), DisableInEditorMode]
         public void PlaySFX()
-            => audioID = AudioManager.PlaySFX(clip.AudioClip, clip.Options);
+            => audioID = AudioManager.PlaySFX(clip.Clip, clip.Options);
 
         public void PlaySFX(AudioClip clip)
             => audioID = AudioManager.PlaySFX(clip);
 
-        public void PlaySFX(RichAudioClip clipRef)
-            => audioID = AudioManager.PlaySFX(
-                clipRef.AudioClip, clipRef.Options);
-
-        /// <summary>
-        /// Play the given clip. If 'duration' LT 0, then it will be length of clip.
-        /// </summary>
-        /// <param name="clip"></param>
-        /// <param name="options"></param>
-        public AudioID PlaySFX(AudioClip clip, AudioOptions options)
+        public void PlaySFX(AudioClip clip, AudioOptions options)
             => audioID = AudioManager.PlaySFX(clip, options);
 
         public void PlaySFXAndDestroy()
         {
-            PlaySFX();
             stopOnDisable = false;
+            PlaySFX();
             Destroy(this);
         }
 
+        [Button, HorizontalGroup("butts_off"), DisableInEditorMode]
+        public void StopBGM()
+            => AudioManager.StopAllBackground();
+
+        [Button, HorizontalGroup("butts_off"), DisableInEditorMode]
         public void StopSFX()
             => AudioManager.StopSFX(audioID, fadeOutDuration: 0);
-
     }
 }
