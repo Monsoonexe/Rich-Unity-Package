4912abe0199e7c61e00248edaeb381951e243895
Richard Osborn <RichardOsborn256@gmail.com>
2022-05-26 23:54:49 -0700

    Add more GetComponent variants

--- _RichScripts/Static Classes/Extensions/GameObject_Extensions.cs
+++ _RichScripts/Static Classes/Extensions/GameObject_Extensions.cs
@@ -38,7 +38,45 @@ namespace RichPackage
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void Destroy(this GameObject a)
             => UnityEngine.Object.Destroy(a);
-            
+
+        /// <returns><paramref name="comp"/> if <paramref name="comp"/> is not null, 
+        /// a <see cref="Component"/> fetched with <see cref="GameObject.GetComponent"/> if not not null,
+        /// otherwise <see cref="GameObject.AddComponent"/>.
+        /// </returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static T GetOrAddComponent<T>(this GameObject gameObject, T comp = null)
+            where T : Component
+        {
+            if (comp != null)
+                return comp;
+
+            if ((comp = gameObject.GetComponent<T>()) != null)
+                return comp;
+
+            return gameObject.AddComponent<T>();
+        }
+
+        /// <returns><paramref name="comp"/> if <paramref name="comp"/> is not null, 
+        /// a <see cref="Component"/> fetched with <see cref="GameObject.GetComponent"/>.
+        /// </returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static T GetComponentIfNull<T>(this GameObject gameObject, T comp = null)
+            where T : Component
+        {
+            return (comp == null) ? gameObject.GetComponent<T>() : comp;
+        }
+
+        /// <returns><paramref name="comp"/> if <paramref name="comp"/> is not null, 
+        /// a <see cref="Component"/> fetched with <see cref="GameObject.GetComponent"/>.
+        /// </returns>
+        public static T GetComponentIfNull<T>(this GameObject gameObject, Maybe<T> maybeComponent)
+            where T : Component
+        {
+            T comp;
+            return ((comp = maybeComponent.GetValueOrDefault()) != null)
+                ? comp : gameObject.GetComponent<T>();
+        }
+
         /// <summary>
         /// Get name of the <see cref="GameObject"/> prefixed 
         /// with the current <see cref="UnityEngine.SceneManagement.Scene"/> name. <br/>
