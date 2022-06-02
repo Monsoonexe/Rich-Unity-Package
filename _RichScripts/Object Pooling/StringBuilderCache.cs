cc2c66df3c7b3437a72757c13f558d915a890ab4
Richard Osborn <RichardOsborn256@gmail.com>
2022-06-01 21:31:44 -0700

    Re-implement CommunityStringBuilder

--- _RichScripts/Object Pooling/StringBuilderCache.cs
+++ _RichScripts/Object Pooling/StringBuilderCache.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System;
+using System.Text;
 
 namespace RichPackage.Pooling
 {
@@ -12,35 +13,47 @@ namespace RichPackage.Pooling
         [ThreadStatic]
         private static StringBuilder CachedInstance;
 
-        public static StringBuilder Rent(int capacity = 64)
+        public static StringBuilder Rent()
+            => Rent(MAX_BUILDER_SIZE);
+
+        public static StringBuilder Rent(int capacity)
         {
+            StringBuilder cachedInstance;
             if (capacity <= MAX_BUILDER_SIZE)
             {
-                StringBuilder cachedInstance = CachedInstance;
+                cachedInstance = CachedInstance;
                 if (cachedInstance != null && capacity <= cachedInstance.Capacity)
                 {
                     CachedInstance = null;
                     cachedInstance.Clear();
-                    return cachedInstance;
                 }
             }
+			else
+			{
+                cachedInstance = new StringBuilder(capacity);
+            }
 
-            return new StringBuilder(capacity);
+            return cachedInstance;
         }
 
         public static void Return(StringBuilder sb)
         {
             if (sb.Capacity <= MAX_BUILDER_SIZE)
-            {
                 CachedInstance = sb;
-            }
         }
 
-        public static string GetStringAndRelease(StringBuilder sb)
+        /// <summary>
+        /// Get the resulting <see cref="string"/> and return to the <see cref="StringBuilderCache"/>.
+        /// </summary>
+        /// <param name="sb">A <see cref="StringBuilder"/> instance
+        /// <see cref="StringBuilderCache.Rent"/>ed from
+        /// <see cref="StringBuilderCache"/>.</param>
+        public static string GetStringAndReturn(this StringBuilder sb)
         {
             string result = sb.ToString();
             Return(sb);
             return result;
         }
+
     }
 }
