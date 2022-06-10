f46b60ba206217ff1d2959e7e4f77b918224a3de
Richard Osborn <apexdevelop003@gmail.com>
2022-06-10 07:03:51 -0700

    Create ObjectFunctions.cs

---
+++ _RichScripts/FunctionalProgramming/ObjectFunctions.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace RichPackage.FunctionalProgramming
+{
+    internal static class ObjectFunctions
+    {
+        /// <summary>
+        /// Functional notation for `obj == null`
+        /// </summary>
+        /// <returns><see langword="true"/> if the object is <see langword="null"/>,
+        /// otherwise <see langword="false"/>.</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool IsNull(this object obj) => obj == null;
+
+        /// <summary>
+        /// Functional notation for `obj != null`
+        /// </summary>
+        /// <returns><see langword="true"/> if the object is not <see langword="null"/>,
+        /// otherwise <see langword="false"/>.</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool IsNotNull(this object obj) => obj != null;
+    }
+}
