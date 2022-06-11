fa385843ef89b232369c066b40e3eaa7b8a4b29f
Richard Osborn <RichardOsborn256@gmail.com>
2022-06-10 23:30:14 -0700

    Create GameObjectTags.cs

---
+++ _RichScripts/Static Classes/GameObjectTags.cs
@@ -0,0 +1,15 @@
+﻿
+namespace RichPackage
+{
+    /// <summary>
+    /// Common tags placed on <see cref="UnityEngine.GameObject"/>s. <br/>
+    /// Prefer these over naked strings.
+    /// </summary>
+    public static class GameObjectTags
+    {
+        //common tags
+        public const string Player = "Player";
+        public const string MainCamera = "MainCamera";
+        public const string EditorOnly = "EditorOnly";
+    }
+}
