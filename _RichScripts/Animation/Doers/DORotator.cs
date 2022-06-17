ecc83f648def352e99a74e53faedaea976c4c9db
Richard Osborn <RichardOsborn256@gmail.com>
2022-06-16 17:51:52 -0700

    Create DoRotator.cs

---
+++ _RichScripts/Animation/Doers/DORotator.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using DG.Tweening;
+using Sirenix.OdinInspector;
+
+namespace RichPackage.Animation
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class DORotator : ADoer
+    {
+        [Title("Animation Settings")]
+        public Space space = Space.World;
+        public Ease ease = Ease.OutQuart;
+        public Vector3 destination = new Vector3 (0, 180, 0);
+        public RotateMode mode = RotateMode.Fast;
+
+		public override void Play()
+		{
+            if (space == Space.World)
+                Tween = transform.DORotate(destination, duration, mode);
+            else
+                Tween = transform.DOLocalRotate(destination, duration, mode);
+
+            //configure
+            Tween.SetEase(ease);
+            SubscribeTweenEvents(Tween);
+        }
+	}
+}
