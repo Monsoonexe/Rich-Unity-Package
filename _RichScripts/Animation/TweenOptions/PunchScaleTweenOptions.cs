de4b56246ba893b21fc35064be138cb03fcf698f
Richard Osborn <RichardOsborn256@gmail.com>
2022-06-16 17:49:05 -0700

    Create TweenOptions

---
+++ _RichScripts/Animation/TweenOptions/PunchScaleTweenOptions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+
+namespace RichPackage.Animation
+{
+	/// <summary>
+	/// A way to package parameters together.
+	/// </summary>
+	[Serializable]
+	public struct PunchTweenOptions
+	{
+		public Vector3 Scale;
+		public float Duration;
+		public float Elasticity;
+		public int Vibrato;
+
+		public PunchTweenOptions(in Vector3 punch,
+			float duration, int vibrato, float elasticity)
+		{
+			Scale = punch;
+			Duration = duration;
+			Vibrato = vibrato;
+			Elasticity = elasticity;
+		}
+
+		public static PunchTweenOptions Default
+		{
+			get => new PunchTweenOptions(new Vector3(0.25f, 0.25f, 1), 1.0f, 10, 1);
+		}
+	}
+}
---
+++ _RichScripts/Animation/TweenOptions/TweenOptions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+using DG.Tweening;
+
+namespace RichPackage.Animation
+{
+	[Serializable]
+	public class TweenOptionsBase
+	{
+		public float Duration;
+		public Ease Ease;
+	}
+
+	[Serializable]
+	public class Vector3TweenOptions : TweenOptionsBase
+	{
+		/// <summary>
+		/// Position, rotation, or scale.
+		/// </summary>
+		public Vector3 Value;
+	}
+
+	//public static class PunchTweenExtensions
+	//{
+	//	public static Tween DoPunch(this Transform transform, PunchTweenOptions options)
+	//	{
+	//		transform.DoPunch
+	//	}
+	//}
+}
