8ddc0f5f635e79c8c05d7fc807e23049cc1516e4
Richard Osborn <RichardOsborn256@gmail.com>
2022-06-19 00:49:26 -0700

    Implement IAnimate<Transform> and IAnimate
    and use self as default

--- ThirdParty/DeVoid UI Framework/Abstract Classes/ATransitionComponent.cs
+++ ThirdParty/DeVoid UI Framework/Abstract Classes/ATransitionComponent.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using UnityEngine;
 using RichPackage;
+using RichPackage.Animation;
 
 /// <summary>
 /// Translates a target transform.
 /// </summary>
-public abstract class ATransitionComponent : RichMonoBehaviour
+public abstract class ATransitionComponent : RichMonoBehaviour, IAnimate<Transform>, IAnimate
 {
     /// <summary>
     /// How many seconds to complete the Animation.
@@ -19,10 +20,15 @@ public abstract class ATransitionComponent : RichMonoBehaviour
 
     public bool IsAnimating { get; protected set; }
 
+    [SerializeField]
+    private Transform target;
+
     protected override void Reset()
     {
         base.Reset();
         SetDevDescription("Animates a UI element.");
+        myTransform = GetComponent<Transform>();
+        target = myTransform;
     }
 
     /// <summary>
@@ -33,6 +39,8 @@ public abstract class ATransitionComponent : RichMonoBehaviour
     public abstract void Animate(Transform targetXform, Action onCompleteCallback = null);
 
     public abstract void Stop();
-    
-    public void Play() => Animate(myTransform);
+
+    public void Play(Transform puppet) => Animate(target = puppet);
+
+	public void Play() => Play(target ? target : myTransform);
 }
