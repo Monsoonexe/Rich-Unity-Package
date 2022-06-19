cc798fd9b34efbb65dea01da983d45b9e1ee7a18
Richard Osborn <RichardOsborn256@gmail.com>
2022-06-18 22:48:45 -0700

    Prefer tabs over spaces

--- _RichScripts/Advanced Collections/MinHeap.cs
+++ _RichScripts/Advanced Collections/MinHeap.cs
@@ -3,50 +3,50 @@ using System.Collections.Generic;
 
 namespace RichPackage.Collections
 {
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <seealso cref="MaxHeap{T}"/>
-    public class MinHeap<T> : AHeap<T>
-    {
-        #region Constructors
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <seealso cref="MaxHeap{T}"/>
+	public class MinHeap<T> : AHeap<T>
+	{
+		#region Constructors
 
-        public MinHeap() : base() {}
+		public MinHeap() : base() {}
 
-        public MinHeap(int capacity) : base(capacity) { }
+		public MinHeap(int capacity) : base(capacity) { }
 
-        public MinHeap(IEnumerable<T> collection) : base(collection) {}
+		public MinHeap(IEnumerable<T> collection) : base(collection) {}
 
-        #endregion
+		#endregion
 
-        protected override void HeapifyUp(int index)
-        {
-            int parentIndex = GetParentIndex(index);
-            if (parentIndex >= 0 && comparer.Compare(elements[index], elements[parentIndex]) < 0)
-            {
-                Swap(index, parentIndex);
-                HeapifyUp(parentIndex);
-            }
-        }
+		protected override void HeapifyUp(int index)
+		{
+			int parentIndex = GetParentIndex(index);
+			if (parentIndex >= 0 && comparer.Compare(elements[index], elements[parentIndex]) < 0)
+			{
+				Swap(index, parentIndex);
+				HeapifyUp(parentIndex);
+			}
+		}
 
-        protected override void HeapifyDown(int index)
-        {
-            int size = elements.Count;
-            int smallest = index;
-            int left = GetLeftIndex(index);
-            int right = GetRightIndex(index);
+		protected override void HeapifyDown(int index)
+		{
+			int size = elements.Count;
+			int smallest = index;
+			int left = GetLeftIndex(index);
+			int right = GetRightIndex(index);
 
-            if (left < size && comparer.Compare(elements[left], elements[index]) < 0)
-                smallest = left;
+			if (left < size && comparer.Compare(elements[left], elements[index]) < 0)
+				smallest = left;
 
-            if (right < size && comparer.Compare(elements[right], elements[smallest]) < 0)
-                smallest = right;
+			if (right < size && comparer.Compare(elements[right], elements[smallest]) < 0)
+				smallest = right;
 
-            if (smallest != index)
-            {
-                Swap(index, smallest);
-                HeapifyDown(smallest);
-            }
-        }
-    }
+			if (smallest != index)
+			{
+				Swap(index, smallest);
+				HeapifyDown(smallest);
+			}
+		}
+	}
 }
