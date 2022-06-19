4925233f0711494bd4cdea75e77e82235d1c3be6
Richard Osborn <RichardOsborn256@gmail.com>
2022-06-19 15:47:52 -0700

    Create WaitUntilEvent.cs

---
+++ _RichScripts/YieldInstructions/WaitUntilEvent.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using UnityEngine;
+using ScriptableObjectArchitecture;
+using RichPackage.Events.Signals;
+using UnityEngine.Events;
+
+namespace RichPackage.YieldInstructions
+{
+    public class WaitUntilEvent : CustomYieldInstruction
+    {
+        private bool eventRaised = false;
+        public override bool keepWaiting { get => !eventRaised; }
+
+        #region Constructors
+
+        public WaitUntilEvent(Action _event)
+        {
+            _event += Response;
+            void Response()
+            {
+                eventRaised = true;
+                _event -= Response;
+            }
+        }
+
+        public WaitUntilEvent(GameEvent _event)
+        {
+            _event.AddListener(Response);
+            void Response()
+            {
+                eventRaised = true;
+                _event.RemoveListener(Response);
+            }
+        }
+
+        public WaitUntilEvent(ASignal _event)
+        {
+            _event.AddListener(Response);
+            void Response()
+            {
+                eventRaised = true;
+                _event.RemoveListener(Response);
+            }
+        }
+
+        public WaitUntilEvent(UnityEvent _event)
+        {
+            _event.AddListener(Response);
+            void Response()
+            {
+                eventRaised = true;
+                _event.RemoveListener(Response);
+            }
+        }
+
+
+		#endregion Constructors
+    }
+}
