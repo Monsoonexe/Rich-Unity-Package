148ea0b1343a99c789fa6ecfe107757b343ab35d
Richard Osborn <RichardOsborn256@gmail.com>
2022-06-20 22:44:20 -0700

    Create EnumerableExtensions.cs

---
+++ _RichScripts/Static Classes/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace RichPackage
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class EnumerableExtensions
+    {
+        /// <summary>
+        /// Convert <paramref name="source"/> to a <see cref="HashSet{T}"/>.
+        /// </summary>
+        // Ref: https://stackoverflow.com/questions/34858338/how-to-elegantly-convert-ienumerablet-to-hashsett-at-runtime-without-knowing
+        public static HashSet<T> ToHashSet<T>(this IEnumerable<T> source)
+        {
+            return new HashSet<T>(source);
+        }
+    }
+}
