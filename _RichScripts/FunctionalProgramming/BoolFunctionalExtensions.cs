ed706b2621391fa452f7d76ad8b9c45447987b79
Richard Osborn <apexdevelop003@gmail.com>
2022-06-22 14:58:29 -0700

    Create BoolFunctionalExtensions.cs

---
+++ _RichScripts/FunctionalProgramming/BoolFunctionalExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace ApexCommon.FunctionalProgramming
+{
+    public static class BoolFunctionalExtensions
+    {
+        /// <summary>
+        /// Functional notation for checking truth value.
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool IsTrue(this bool b) => b;
+
+        /// <summary>
+        /// Functional notation for checking truth value.
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool IsFalse(this bool b) => !b;
+    }
+}
