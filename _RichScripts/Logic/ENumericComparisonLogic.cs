675a260e689d65756001dff814cc5a7348d9921c
Richard Osborn <RichardOsborn256@gmail.com>
2022-06-23 00:59:28 -0700

    Fix 'none' enum value

--- _RichScripts/Logic/ENumericComparisonLogic.cs
+++ _RichScripts/Logic/ENumericComparisonLogic.cs
@@ -7,12 +7,12 @@ namespace RichPackage.Logic
 	//[System.Flags]
 	public enum ENumericComparisonLogic
 	{
-		None = (1 << 0),
-		GreaterThan = (1 << 1),
-		LesserThan = (1 << 2),
-		EqualTo = (1 << 3),
+		None = 0,
+		GreaterThan = (1 << 0),
+		LesserThan = (1 << 1),
+		EqualTo = (1 << 2),
 		//Not = (1 << 4),
-		NotEqualTo = (1 << 4),
+		NotEqualTo = (1 << 3),
 		GreaterThanOrEqual = GreaterThan | EqualTo,
 		LesserThanOrEqual = LesserThan | EqualTo,
 	}
