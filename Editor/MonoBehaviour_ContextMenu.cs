f7ee70bc5e1419aef4abba24fdc6e060c164948e
Richard Osborn <RichardOsborn256@gmail.com>
2022-06-29 00:15:03 -0700

    test

---
+++ Editor/MonoBehaviour_ContextMenu.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System;
+using Sirenix.OdinInspector;
+using Sirenix.OdinInspector.Editor.Drawers;
+
+namespace RichPackage.Editor
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	[InitializeOnLoad]
+	public static class Monobehaviour_ContextMenu
+	{
+		static Monobehaviour_ContextMenu()
+		{
+			EditorApplication.contextualPropertyMenu += OnContextMenuOpening;
+			//PropertyContextMenuDrawer.FillUnityContextMenu();
+		}
+
+		private static void OnContextMenuOpening(GenericMenu menu, SerializedProperty property)
+		{
+			//var comp = (Component)property.exposedReferenceValue;
+			//Debug.Log($"Inspecting type: {property.serializedObject.targetObject}.");
+			//menu.AddItem(new GUIContent("GetComponent()"), on: false,
+			//	() =>
+			//	{
+			//		property.objectReferenceValue = property.serializedObject
+			//		.targetObject.CastTo<Component>()
+			//		.GetComponent("Graphic");
+			//	});
+		}
+
+		private static void Log()
+		{
+			Debug.Log("Hello world!");
+		}
+	}
+}
---
+++ Editor/OdinUtilities/UnityEventContextMenu.cs
@@ -0,0 +1,34 @@
+﻿/* It doesn't work because UnityEvent isn't drawn by odin. :(
+ * 
+ */
+
+
+using UnityEngine.Events;
+using UnityEditor;
+using Sirenix.OdinInspector.Editor;
+using Sirenix.Utilities.Editor;
+
+namespace RichPackage.Editor
+{
+    /// <summary>
+    /// Context menu entries for <see cref="UnityEvent"/>s.
+    /// </summary>
+ //   public class UnityEventContextMenu : OdinValueDrawer<UnityEvent>,
+ //       IDefinesGenericMenuItems
+	//{
+ //       protected override void Initialize()
+ //       {
+ //           // We don't use this drawer to "draw" something so skip it when drawing.
+ //           SkipWhenDrawing = true;
+ //       }
+
+ //       public void PopulateGenericMenu(InspectorProperty property, GenericMenu genericMenu)
+ //       {
+ //           genericMenu.AddItem(GUIHelper.TempContent(
+ //               $"Invoke OnClick"),
+ //               on: false,
+ //               () => property.Parent.BaseValueEntry.WeakSmartValue
+ //                   .CastTo<UnityEvent>().Invoke());
+ //       }
+ //   }
+}
