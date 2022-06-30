42b8b23825cc766fd583434770a797ec3f16e81a
Richard Osborn <RichardOsborn256@gmail.com>
2022-06-29 23:16:51 -0700

    Create ExceptionUtilities.cs

---
+++ _RichScripts/Exceptions/ExceptionUtilities.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace RichPackage
+{
+	public static class ExceptionUtilities
+	{
+		public static string GetEnumMemberNotImplementedExceptionString<TEnum>(TEnum value)
+			where TEnum : Enum
+		{
+			return $"{value} is not implemented. You should do so!";
+		}
+
+		public static Exception GetEnumNotImplementedException<TEnum>(TEnum value)
+			where TEnum : Enum
+		{
+			return new NotImplementedException(GetEnumMemberNotImplementedExceptionString(value));
+		}
+	}
+}
