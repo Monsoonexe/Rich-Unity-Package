0ff9bf2789ed13149c968c7a72d7e46d4890ac85
Richard Osborn <RichardOsborn256@gmail.com>
2022-07-02 17:36:43 -0700

    Add bool variable listener

---
+++ ThirdParty/SO Architecture - Fork/Events/Listeners/Variable Listeners/BoolVariableListener.cs
@@ -0,0 +1,21 @@
+﻿
+namespace ScriptableObjectArchitecture
+{
+	public sealed class BoolVariableListener : AVariableListener<BoolVariable, bool, BoolUnityEvent>
+	{
+		//exists
+		public BoolUnityEvent invertedResponse = default;
+
+		protected override void OnDisable()
+		{
+			base.OnDisable();
+			variable.RemoveInvertedListener(invertedResponse.Invoke);
+		}
+
+		protected override void OnEnable()
+		{
+			base.OnEnable();
+			variable.AddInvertedListener(invertedResponse.Invoke);
+		}
+	}
+}
