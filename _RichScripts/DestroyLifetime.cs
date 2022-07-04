c9804f96b444dab255e4864bfa5ba72662a23557
Richard Osborn <RichardOsborn256@gmail.com>
2022-07-04 14:00:46 -0700

    Odinize and document

--- _RichScripts/DestroyLifetime.cs
+++ _RichScripts/DestroyLifetime.cs
@@ -1,20 +1,21 @@
 ﻿using UnityEngine;
+using Sirenix.OdinInspector;
 
 namespace RichPackage
 {
     /// <summary>
-    /// Destroys parent GameObject after given time.
+    /// Destroys owning <see cref="GameObject"/> after given time.
     /// </summary>
-    public class DestroyLifetime : RichMonoBehaviour
+    public sealed class DestroyLifetime : RichMonoBehaviour
     {
-        [Header("---Settings---")]
+        [Title("Settings")]
         [Min(0)]
         public float lifeTime = 10;
 
         protected override void Reset()
         {
             base.Reset();
-            SetDevDescription("Destroys parent GameObject after given time.");
+            SetDevDescription("Destroys owning GameObject after given time.");
         }
 
         private void Start()
