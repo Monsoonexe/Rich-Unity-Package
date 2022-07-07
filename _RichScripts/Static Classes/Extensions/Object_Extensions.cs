0db7320a2dca816d02d658a6c6546f6425189a47
Richard Osborn <RichardOsborn256@gmail.com>
2022-07-06 23:11:02 -0700

    Allow fluent-style chaining

--- _RichScripts/Static Classes/Extensions/Object_Extensions.cs
+++ _RichScripts/Static Classes/Extensions/Object_Extensions.cs
@@ -29,10 +29,11 @@ namespace RichPackage
         /// <param name="paramName">The name of the parameter being checked.</param>
         /// <exception cref="ArgumentNullException"></exception>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static void ThrowIfNull(this object obj, string paramName)
+        public static object ThrowIfNull(this object obj, string paramName)
         {
             if (obj == null)
                 throw new ArgumentNullException(paramName);
+            return obj; // allow chaining
         }
         
         /// <summary>
