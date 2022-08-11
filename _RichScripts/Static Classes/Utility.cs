77954a19537f3e3f75effbb97b71bc589ad9cd52
Richard Osborn <richardosborn256@gmail.com>
2022-08-10 19:02:07 -0700

    Remove duplicate functions

--- _RichScripts/Static Classes/Utility.cs
+++ _RichScripts/Static Classes/Utility.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections;
 using System.Diagnostics;
 using System.Runtime.CompilerServices;
-using UnityEngine;
-using DG.Tweening;
-using RichPackage.YieldInstructions;
 
 //clarifications between UnityEngine and System classes
 using Debug = UnityEngine.Debug;
@@ -55,93 +51,6 @@ namespace RichPackage
         public static int RollDie(int sides) => Random.Range(1, sides + 1);
 
         #endregion
-
-        #region DelayInvokation
-
-        /// <summary>
-        /// Must be called from StartCoroutine() on a Mono.
-        /// </summary>
-        /// <param name="delay"></param>
-        /// <param name="callback"></param>
-        /// <returns></returns>
-        public static IEnumerator InvokeAfterDelay(this Action callback, float delay)
-        {
-            yield return new WaitForSeconds(delay);
-            callback();
-        }
-
-        public static IEnumerator InvokeAfterDelay(this Action callback,
-            YieldInstruction yieldInstruction)
-        {
-            yield return yieldInstruction;
-            callback();
-        }
-
-        /// <summary>
-        /// Version of InvokeAfterDelay that uses DOTween instead of coroutines
-        /// </summary>
-        /// <param name="callback"></param>
-        /// <param name="delay"></param>
-        /// <returns></returns>
-        public static Sequence InvokeAfterDelaySequence(this Action callback, float delay)
-        {
-            return DOTween.Sequence() //new Sequence()
-                .AppendInterval(delay)
-                .AppendCallback(callback.Invoke);
-        }
-
-        public static IEnumerator InvokeAtEndOfFrame(this Action callback)
-        {
-            yield return CommonYieldInstructions.WaitForEndOfFrame;
-            callback();
-        }
-
-        public static IEnumerator InvokeNextFrame(this Action callback)
-        {
-            yield return null;
-            callback();
-        }
-
-        /// <summary>
-        /// Postpone execution until after so many frames have passed.
-        /// </summary>
-        /// <param name="callback"></param>
-        /// <param name="frames"></param>
-        /// <returns></returns>
-        public static IEnumerator InvokeAfterFrames(this Action callback, int frames)
-        {
-            for(var i = 0; i < frames; ++i)
-            {
-                yield return null;
-            }
-            callback();
-        }
-
-        #endregion
-
-        /// <summary>
-        /// Set this and all children (and on) to given layer.
-        /// </summary>
-        /// <param name="obj"></param>
-        /// <param name="newLayer"></param>
-        public static void SetLayerRecursively(this Transform obj, int newLayer)
-        {
-            obj.gameObject.layer = newLayer;
-            var childCount = obj.childCount;
-            for (var i = 0; i < childCount; ++i)
-            {
-                var child = obj.GetChild(i);
-                SetLayerRecursively(child, newLayer);
-            }
-        }
-
-        /// <summary>
-        /// Set this and all children (and on) to given layer.
-        /// </summary>
-        /// <param name="obj"></param>
-        /// <param name="newLayer"></param>
-        public static void SetLayerRecursively(this GameObject gameObj, int newLayer)
-            => SetLayerRecursively(gameObj.transform, newLayer);
 
         #region Functional Iterating
 
