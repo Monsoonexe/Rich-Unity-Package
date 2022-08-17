d46691aac6252b1d00b91e8e9fbd947802707006
Richard Osborn <richardosborn256@gmail.com>
2022-08-16 20:39:12 -0700

    Create TaskUtilities.cs

---
+++ _RichScripts/Static Classes/TaskUtilities.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace RichPackage.Threading.Tasks
+{
+    public static class TaskUtilities
+    {
+        /// <summary>
+        /// Removes compiler warning caused by not awaiting an awaitable.
+        /// </summary>
+        [Conditional(ConstStrings.UNITY_EDITOR)]
+        public static void Forget(this Task t)
+        {
+            // nada
+        }
+    }
+}
