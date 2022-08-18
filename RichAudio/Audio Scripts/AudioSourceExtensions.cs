bf0d718f17b340e88f164e446f48d9ff42ef1982
Richard Osborn <richardosborn256@gmail.com>
2022-08-17 20:02:06 -0700

    Create AudioSourceExtensions.cs

---
+++ RichAudio/Audio Scripts/AudioSourceExtensions.cs
@@ -0,0 +1,35 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+namespace RichPackage.Audio
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	public static class AudioSourceExtensions
+	{
+		public static AudioSource Restart(this AudioSource source)
+        {
+			source.Stop();
+			source.Play();
+			return source;
+		}
+
+		public static Tween DOFadeIn(this AudioSource source, float duration, Ease ease = Ease.Linear, float volume = 1.0f)
+		{
+			source.volume = 0; // start quiet
+			source.Play();
+			return source
+				.DOFade(volume, duration)
+				.SetEase(ease);
+		}
+
+		public static Tween DOFadeOut(this AudioSource source, float duration, Ease ease = Ease.Linear)
+		{
+			return source
+				.DOFade(0, duration)
+				.SetEase(ease)
+				.OnComplete(source.Stop);
+		}
+	}
+}
