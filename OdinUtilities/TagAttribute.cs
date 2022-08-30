4cce300132d964abb9d1b45c31bc04a451f5796c
Richard Osborn <richardosborn256@gmail.com>
2022-08-29 22:14:42 -0700

    Add TagAttribute

---
+++ OdinUtilities/TagAttribute.cs
@@ -0,0 +1,7 @@
+﻿/* credit: https://gist.github.com/My-Key/67ddd544ede0d38c5f5998b7b3adf38e
+*/
+
+using System;
+
+[AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
+public class TagAttribute : Attribute { }
