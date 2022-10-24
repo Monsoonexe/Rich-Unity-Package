54bc5d434d5d242cfafa9ed8e2c41cf47a970a1c
Richard Osborn <rich.osborn@apexofficer.com>
2022-10-24 12:57:34 -0700

    Create InlineSuggestor.cs

---
+++ _RichScripts/ConsoleCommands/InlineSuggestor.cs
@@ -0,0 +1,52 @@
+﻿using QFSW.QC;
+using System.Collections.Generic;
+
+namespace ApexOfficer.ConsoleCommands
+{
+    /// <summary>
+    /// Object that holds all the inlined suggestions provided to <see cref="SuggestionsAttribute"/>.
+    /// </summary>
+    public struct InlineSuggestionTag : IQcSuggestorTag
+    {
+        public readonly string[] Suggestions;
+
+        public InlineSuggestionTag(string[] suggestions)
+        {
+            Suggestions = suggestions;
+        }
+    }
+
+    public sealed class SuggestionsAttribute : SuggestorTagAttribute
+    {
+        private readonly IQcSuggestorTag[] _tags;
+
+        public SuggestionsAttribute(params string[] suggestions)
+        {
+            var tag = new InlineSuggestionTag(suggestions);
+            _tags = new IQcSuggestorTag[] { tag };
+        }
+
+        public override IQcSuggestorTag[] GetSuggestorTags() => _tags;
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class InlineSuggestor : IQcSuggestor
+    {
+        public IEnumerable<IQcSuggestion> GetSuggestions(SuggestionContext context, SuggestorOptions options)
+        {
+            if (context.Tags != null)
+            {
+                foreach (var tag in context.Tags)
+                {
+                    if (tag is InlineSuggestionTag suggestorTag)
+                    {
+                        foreach (var s in suggestorTag.Suggestions)
+                            yield return new RawSuggestion(s, true);
+                    }
+                }
+            }
+        }
+    }
+}
