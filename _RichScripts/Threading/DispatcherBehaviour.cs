b0a8b2ceea955996b68b7fc4132f77f31201ec50
Richard Osborn <richardosborn256@gmail.com>
2022-10-27 22:32:42 -0700

    Create DispatchBehaviour.cs
    monobehaviour

---
+++ _RichScripts/Threading/DispatcherBehaviour.cs
@@ -0,0 +1,73 @@
+﻿using Cysharp.Threading.Tasks;
+using System;
+
+namespace RichPackage.Threading
+{
+    /// <summary>
+    /// A <seealso cref="Dispatcher"/> that lives on a <see cref="UnityEngine.GameObject"/>.
+    /// </summary>
+    public class DispatcherBehaviour : RichMonoBehaviour, IDispatcher
+    {
+        private Dispatcher dispatcher;
+
+        public bool Enabled => enabled;
+
+        #region Unity Messages
+
+        protected override void Awake()
+        {
+            base.Awake();
+            dispatcher = new Dispatcher(this);
+        }
+
+        private void OnDestroy()
+        {
+            dispatcher.Dispose();
+        }
+
+        private void OnEnable()
+        {
+            dispatcher.Run();
+        }
+
+        private void OnDisable()
+        {
+            dispatcher.Stop();
+        }
+
+        #endregion Unity Messages
+
+        public UniTask BeginInvoke(Action action)
+        {
+            return dispatcher.BeginInvoke(action);
+        }
+
+        public UniTask<T> BeginInvoke<T>(Func<T> function)
+        {
+            return dispatcher.BeginInvoke(function);
+        }
+
+        public void Invoke(Action action)
+        {
+            dispatcher.Invoke(action);
+        }
+
+        public void Invoke(Action action, int delay)
+        {
+            dispatcher.Invoke(action, delay);
+        }
+
+        /// <summary>
+        /// Begin message pumping.
+        /// </summary>
+        public void Run()
+        {
+            enabled = true;
+        }
+
+        public void Stop()
+        {
+            enabled = false;
+        }
+    }
+}
