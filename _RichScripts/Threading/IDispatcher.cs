7fe8aba18d7255cd592c2366c7fa0c9e1dfb67f9
Richard Osborn <richardosborn256@gmail.com>
2022-10-27 22:32:10 -0700

    Create IDispatcher.cs

---
+++ _RichScripts/Threading/IDispatcher.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace RichPackage.Threading
+{
+    public interface IDispatcher
+    {
+        bool Enabled { get; }
+
+        void Invoke(Action action);
+        void Run();
+        void Stop();
+    }
+}
