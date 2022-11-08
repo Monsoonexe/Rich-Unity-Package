1ac6280af900cb35db1b714ae960000a89cc9131
Richard Osborn <richardosborn256@gmail.com>
2022-11-07 22:36:39 -0800

    Create SimpleTimer.cs

---
+++ _RichScripts/SimpleTimer.cs
@@ -0,0 +1,35 @@
+﻿
+namespace RichPackage
+{
+    /// <summary>
+    /// Default timer that calculates the elapsed time based on <see cref="UnityEngine.Time.time"/>.
+    /// </summary>
+    public class SimpleTimer
+    {
+        public float startTime;
+
+        public float Elapsed => RichAppController.Time - startTime;
+
+        public SimpleTimer()
+        {
+            startTime = RichAppController.Time;
+        }
+
+        public void Reset()
+        {
+            startTime = RichAppController.Time;
+        }
+
+        public static bool operator >(SimpleTimer timer, float duration)
+            => timer.Elapsed > duration;
+
+        public static bool operator <(SimpleTimer timer, float duration)
+            => timer.Elapsed < duration;
+
+        public static bool operator >=(SimpleTimer timer, float duration)
+            => timer.Elapsed >= duration;
+
+        public static bool operator <=(SimpleTimer timer, float duration)
+            => timer.Elapsed <= duration;
+    }
+}
