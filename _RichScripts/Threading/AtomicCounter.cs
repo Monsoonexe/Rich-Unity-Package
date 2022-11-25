94cc9cae50867902dd383b5e7778b97e4f379aa4
Richard Osborn <richardosborn256@gmail.com>
2022-11-25 11:03:28 -0800

    Create AtomicCounter.cs

---
+++ _RichScripts/Threading/AtomicCounter.cs
@@ -0,0 +1,76 @@
+﻿using System.Runtime.CompilerServices;
+using System.Threading;
+
+namespace RichPackage.Threading
+{
+    /// <summary>
+    /// Thread-safe counter.
+    /// </summary>
+    internal class AtomicCounter
+    {
+        private int counter;
+
+        public AtomicCounter() : this(0) { }
+
+        public AtomicCounter(int value)
+        {
+            counter = value;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Set(int x)
+        {
+            Interlocked.Exchange(ref counter, x);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public int Add(int x)
+        {
+            return Interlocked.Add(ref counter, x);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public int Sub(int x)
+        {
+            return Interlocked.Add(ref counter, -x);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public int Increment()
+        {
+            return Interlocked.Increment(ref counter);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public int Decrement()
+        {
+            return Interlocked.Decrement(ref counter);
+        }
+
+        public int Value
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get => Interlocked.Add(ref counter, 0);
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            set => Interlocked.Exchange(ref counter, value);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static implicit operator int(AtomicCounter a) => a.Value;
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static AtomicCounter operator --(AtomicCounter a)
+        {
+            Interlocked.Decrement(ref a.counter);
+            return a;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static AtomicCounter operator ++(AtomicCounter a)
+        {
+            Interlocked.Increment(ref a.counter);
+            return a;
+        }
+    }
+}
