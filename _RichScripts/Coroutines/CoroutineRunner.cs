037869c9d6b777d197c0e5e7e4869d826f7f5fe7
Richard Osborn <richardosborn256@gmail.com>
2022-12-11 13:51:45 -0800

    Create CoroutineRunner.cs

---
+++ _RichScripts/Coroutines/CoroutineRunner.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace RichPackage.Coroutines
+{
+    /// <summary>
+    /// A cohort whose only purpose is to run coroutines.
+    /// </summary>
+    public class CoroutineRunner : RichMonoBehaviour
+    {
+        protected override void Reset()
+        {
+            SetDevDescription("My only job is to run coroutines for those that cannot!");
+        }
+
+        /// <summary>
+        /// Creates a new instance of a <see cref="CoroutineRunner"/>.
+        /// </summary>
+        /// <param name="scenePersistent">Should this be set to not destroy on load?</param>
+        /// <returns>A new instance.</returns>
+        public static CoroutineRunner CreateNew(bool scenePersistent)
+        {
+            var go = new GameObject(nameof(CoroutineRunner));
+            if (scenePersistent)
+                DontDestroyOnLoad(go);
+            return go.AddComponent<CoroutineRunner>();
+        }
+    }
+}
