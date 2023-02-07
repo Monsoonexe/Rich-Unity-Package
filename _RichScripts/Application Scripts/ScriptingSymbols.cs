bf3e52f272c57731b168adc7a1d6016bb4012ba2
Richard Osborn <richardosborn256@gmail.com>
2023-02-07 00:09:55 -0800

    Create ScriptingSymbols.cs

---
+++ _RichScripts/Application Scripts/ScriptingSymbols.cs
@@ -0,0 +1,24 @@
+﻿
+using UnityEngine;
+using System.Diagnostics;
+using Debug = UnityEngine.Debug;
+
+namespace RichPackage.Application
+{
+	/// <summary>
+	/// Contains utilites for scripting symbols that exist in this project.
+	/// </summary>
+	public static partial class ScriptingSymbols
+	{
+        public const string UNITY_EDITOR = "UNITY_EDITOR";
+
+		public static void PrintPresent()
+		{
+			PrintUnityEditor();
+        }
+
+        [Conditional(UNITY_EDITOR)]
+
+        private static void PrintUnityEditor() => Debug.Log(UNITY_EDITOR);
+    }
+}
