22e3be1de6d1494690f713f11b89d1bb30eab915
Richard Osborn <richardosborn256@gmail.com>
2023-02-13 16:42:02 -0800

    Create UnityObject_Extensions.cs

---
+++ _RichScripts/Static Classes/Extensions/UnityObject_Extensions.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace UnityEngine
+{
+	/// <summary>
+	/// Extensions for <see cref="Object"/>.
+	/// </summary>
+	public static class UnityObject_Extensions
+	{
+        /// <summary>
+        /// Returns true if the two objects are the same instance. Faster than the equality
+        /// operator if both objects are known to be non-null and alive.
+        /// </summary>
+        public static bool QuickEquals(this Object a, Object b)
+        {
+            return a.GetInstanceID() == b.GetInstanceID();
+        }
+
+    }
+}
