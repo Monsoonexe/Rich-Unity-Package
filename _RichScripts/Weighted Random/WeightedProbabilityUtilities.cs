5625b6f445c2ad8a84636b3370bf109e4550d916
Richard Osborn <richardosborn256@gmail.com>
2023-02-17 23:02:05 -0800

    Code cleanup

--- _RichScripts/Weighted Random/WeightedProbabilityUtilities.cs
+++ _RichScripts/Weighted Random/WeightedProbabilityUtilities.cs
@@ -11,8 +11,7 @@ namespace RichPackage.WeightedProbabilities
         /// <summary>
         /// Get a random element using Weighted algorithm.
         /// </summary>
-        public static int GetWeightedIndex<T>(
-            this IList<T> items)
+        public static int GetWeightedIndex<T>(this IList<T> items)
             where T : AWeightedProbability
         {
             int totalWeight = GetTotalWeight(items);
@@ -26,8 +25,7 @@ namespace RichPackage.WeightedProbabilities
         /// Get a random element using Weighted algorithm.
         /// </summary>
         /// <param name="randomValue">The weighted random value (ticket).</param>
-        public static int GetWeightedIndex<T>(
-            this IList<T> items, int randomValue)
+        public static int GetWeightedIndex<T>(this IList<T> items, int randomValue)
             where T : AWeightedProbability
         {
             int index = 0;
@@ -45,17 +43,15 @@ namespace RichPackage.WeightedProbabilities
         /// <summary>
         /// Get a random element using Weighted algorithm.
         /// </summary>
-        public static T GetWeightedRandomElement<T>(
-            this IList<AWeightedProbability<T>> items)
-		{
+        public static T GetWeightedRandomElement<T>(this IList<AWeightedProbability<T>> items)
+        {
             return GetWeightedRandomElement<T, AWeightedProbability<T>>(items);
-		}
+        }
 
         /// <summary>
         /// Get a random element using Weighted algorithm.
         /// </summary>
-        public static T GetWeightedRandomElement<T, U>(
-            this IList<U> items)
+        public static T GetWeightedRandomElement<T, U>(this IList<U> items)
             where U : AWeightedProbability<T>
         {
             return items[GetWeightedIndex(items)].Value;
@@ -64,8 +60,7 @@ namespace RichPackage.WeightedProbabilities
         /// <summary>
         /// Sum the <see cref="AWeightedProbability.Weight"/>s.
         /// </summary>
-        public static int GetTotalWeight<T>(this IList<T>
-            probabilityTemplates)
+        public static int GetTotalWeight<T>(this IList<T> probabilityTemplates)
             where T : AWeightedProbability
         {
             return probabilityTemplates.Sum((prob) => prob.Weight);
