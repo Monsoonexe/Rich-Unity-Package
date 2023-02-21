32fe8c92887229311f1d69dce2a07de460673643
Richard Osborn <richardosborn256@gmail.com>
2023-02-20 19:30:40 -0800

    Add struct constraint for enum

--- _RichScripts/Exceptions/ExceptionUtilities.cs
+++ _RichScripts/Exceptions/ExceptionUtilities.cs
@@ -5,13 +5,13 @@ namespace RichPackage
 	public static class ExceptionUtilities
 	{
 		public static string GetEnumMemberNotAccountedExceptionString<TEnum>(TEnum value)
-			where TEnum : Enum
-		{
+			where TEnum : struct, Enum
+        {
 			return $"{value} is not implemented. You should do so!";
 		}
 
 		public static Exception GetInvalidEnumCaseException<TEnum>(TEnum value)
-			where TEnum : Enum
+			where TEnum : struct, Enum
 		{
 			return new NotImplementedException(
 				GetEnumMemberNotAccountedExceptionString(value));
