37fc3ad8dd8a238d5c9f8997500bde1406878e5d
Richard Osborn <richardosborn256@gmail.com>
2023-02-26 11:56:04 -0800

    Split ANavigationProvider into ACachedNavigationProvider

---
+++ ThirdParty/DeVoid UI Framework/UINavigation/ACachedNavigationProvider.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System;
+
+/// <summary>
+/// A <see cref="ANavigationProvider"/> that caches a mapping of screenIds to methods that open the window.
+/// </summary>
+public abstract class ACachedNavigationProvider : ANavigationProvider
+{
+    protected readonly Dictionary<string, Action> windowMapping
+        = new Dictionary<string, Action>();
+
+    public override void NavigateTo(string screenID)
+    {
+        OpenWindow(screenID);
+    }
+
+    protected void MapWindow(string screenID, Action opener)
+    {
+        // TODO - error handling
+        windowMapping.Add(screenID, opener);
+    }
+
+    protected void OpenWindow(string screenID)
+    {
+        // TODO - error handling
+        windowMapping[screenID]();
+    }
+
+}
+
--- ThirdParty/DeVoid UI Framework/UINavigation/ANavigationProvider.cs
+++ ThirdParty/DeVoid UI Framework/UINavigation/ANavigationProvider.cs
@@ -1,26 +1,10 @@
 ﻿using RichPackage;
-using System;
-using System.Collections.Generic;
 
 /// <summary>
 /// Routes navigation requests to the correct screen with the proper properties.
 /// </summary>
 public abstract class ANavigationProvider : RichMonoBehaviour
 {
-    protected readonly Dictionary<string, Action> windowMapping
-        = new Dictionary<string, Action>();
-
     public abstract void NavigateTo(string screenID);
-
-    protected void MapWindow(string screenID, Action opener)
-    {
-        // TODO - error handling
-        windowMapping.Add(screenID, opener);
-    }
-
-    protected void OpenWindow(string screenID)
-    {
-        // TODO - error handling
-        windowMapping[screenID]();
-    }
 }
+
