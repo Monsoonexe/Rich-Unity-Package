8960cb556c69c5c18527792c92b99bad6349f1d9
Richard Osborn <richardosborn256@gmail.com>
2023-02-28 18:40:39 -0800

    Odinize

--- _RichScripts/Health/HealthController.cs
+++ _RichScripts/Health/HealthController.cs
@@ -1,4 +1,4 @@
-/*
+﻿/*
 TODO - replace this with a Stats system
 health and mana are virtually identical, but the base class shouldn't be "health"
 
@@ -6,11 +6,10 @@ also, attributes like Strength traditionally work the same way
 
 */
 
+using ScriptableObjectArchitecture;
+using Sirenix.OdinInspector;
 using UnityEngine;
 using UnityEngine.Events;
-using ScriptableObjectArchitecture;
-using NaughtyAttributes;
-using RichPackage;
 
 namespace RichPackage.HealthSystem
 {
@@ -29,38 +28,39 @@ namespace RichPackage.HealthSystem
         private IntReference maxHealth = new IntReference(100); 
 
         public int MaxHealth { get => maxHealth; }
+
         public int CurrentHealth { get => currentHealth; }
 
         /// <summary>
         /// Current / max health.
         /// </summary>
-        [ShowNativeProperty]
+        [ShowInInspector]
         public float HealthRatio { get => (float)currentHealth / maxHealth; }
 
-        [ShowNativeProperty]
+        [ShowInInspector]
         public bool IsDamaged { get => currentHealth < maxHealth; }
 
-        [ShowNativeProperty]
+        [ShowInInspector]
         public bool IsDead { get; private set; }
 
         #region Events
 
-        [Foldout("---Events---")]
+        [FoldoutGroup("---Events---")]
         [SerializeField]
         private UnityEvent healthGainedEvent = new UnityEvent();
         public UnityEvent HealthGainedEvent { get => healthGainedEvent; }
 
-        [Foldout("---Events---")]
+        [FoldoutGroup("---Events---")]
         [SerializeField]
         private UnityEvent healthLostEvent = new UnityEvent();
         public UnityEvent HealthLostEvent { get => healthLostEvent; }
 
-        [Foldout("---Events---")]
+        [FoldoutGroup("---Events---")]
         [SerializeField]
         private UnityEvent revivedEvent = new UnityEvent();
         public UnityEvent RevivedEvent { get => revivedEvent; }
 
-        [Foldout("---Events---")]
+        [FoldoutGroup("---Events---")]
         [SerializeField]
         private UnityEvent deadEvent = new UnityEvent();
         public UnityEvent DeadEvent { get => deadEvent; }
