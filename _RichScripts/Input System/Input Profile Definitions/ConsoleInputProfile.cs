098c4ea341c16587d3f27bf66ada6a61d8c600f6
Richard Osborn <richardosborn256@gmail.com>
2023-03-12 02:06:22 -0700

    Create console input profile

---
+++ _RichScripts/Input System/Input Profile Definitions/ConsoleInputProfile.cs
@@ -0,0 +1,25 @@
+﻿using QFSW.QC;
+using System;
+using UnityEngine;
+
+namespace RichPackage.InputSystem
+{
+    public class ConsoleInputProfile
+        : AInputProfileAsset<StandardAppInputProfile.InputProfile>
+    {
+        [Serializable]
+        public sealed class InputProfile : PlayerInputProfile
+        {
+            public override void OnLogic()
+            {
+                // submit input
+                if (Input.GetKeyDown(KeyCode.KeypadEnter))
+                    QuantumConsole.Instance.InvokeCommand();
+
+                // close console
+                if (Input.GetKeyDown(KeyCode.Escape))
+                    QuantumConsole.Instance.Deactivate();
+            }
+        }
+    }
+}
