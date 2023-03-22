24409f5836aabcb6b131e2d4b8c7b6bfff5f6fb4
Richard Osborn <richardosborn256@gmail.com>
2023-03-21 22:51:54 -0700

    Create CurveAssset.cs

---
+++ _RichScripts/Math/CurveAsset.cs
@@ -0,0 +1,26 @@
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace RichPackage.Mathematics
+{
+    /// <summary>
+    /// A mathematical curve that can be scripted.
+    /// </summary>
+    public partial class CurveAsset : RichScriptableObject
+    {
+        [SerializeField]
+        protected AnimationCurve curve;
+
+        [Button]
+        public virtual float Evaluate(float t)
+        {
+            return curve.Evaluate(t);
+        }
+
+        [Button]
+        public virtual int Evaluate(int t)
+        {
+            return (int)curve.Evaluate(t);
+        }
+    }
+}
