7eb4e4ad7e30d0d482803f162948b6ed53219386
Richard Osborn <Rich.Osborn@ApexOfficer.com>
2023-03-29 14:05:32 -0700

    Create Database scripts

---
+++ _RichScripts/Scriptable Database Asset/ADatabaseMember.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace RichPackage.Databases
+{
+	/// <summary>
+	/// An asset that holds the data and lookup key for an item inside
+	/// a <see cref="ADatabaseAsset{TData}"/>.
+	/// </summary>
+    public abstract class ADatabaseMember<TData> : ApexScriptableObject<TData>,
+		IDatabaseMember
+	{
+		[SerializeField, HideInInspector]
+		private int key;
+
+		/// <summary>
+		/// The lookup key in <see cref="ADatabaseAsset{TData}"/>.
+		/// </summary>
+		public int Key { get => key; set => key = value; }
+	}
+}
---
+++ _RichScripts/Scriptable Database Asset/IDatabaseMember.cs
@@ -0,0 +1,10 @@
+﻿namespace RichPackage.Databases
+{
+    /// <summary>
+    /// Interface for items that are kept within an <see cref="ADatabaseAsset{TData}"/>.
+    /// </summary>
+    public interface IDatabaseMember
+    {
+        int Key { get; set; }
+    }
+}
