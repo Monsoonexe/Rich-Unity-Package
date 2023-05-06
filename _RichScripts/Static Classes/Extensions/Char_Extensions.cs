b16841fe630f6f1b76ef5711c7c9dd1b2f6715bb
Richard Osborn <richardosborn256@gmail.com>
2023-05-06 14:50:15 -0700

    Add QuickToLower and QuickToUpper

--- _RichScripts/Static Classes/Extensions/Char_Extensions.cs
+++ _RichScripts/Static Classes/Extensions/Char_Extensions.cs
@@ -24,5 +24,35 @@ namespace RichPackage
                     return false;
             }
         }
+
+        /// <summary>
+        /// Quicker than <see cref="char.ToLower(char)"/> when using ASCII.
+        /// </summary>
+        public static char QuickToLower(this char c)
+        {
+            if (c >= 'A' && c <= 'Z')
+            {
+                return (char)(c + 32);
+            }
+            else
+            {
+                return c;
+            }
+        }
+
+        /// <summary>
+        /// Quicker than <see cref="char.ToUpper(char)"/> when using ASCII.
+        /// </summary>
+        public static char QuickToUpper(this char c)
+        {
+            if (c >= 'a' && c <= 'c')
+            {
+                return (char)(c - 32);
+            }
+            else
+            {
+                return c;
+            }
+        }
     }
 }
