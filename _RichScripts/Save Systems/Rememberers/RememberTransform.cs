0a0d68d57c7f04e5c35282140b4f4a2a29fe0b34
Richard Osborn <richardosborn256@gmail.com>
2023-05-15 19:22:23 -0700

    Fix transform not being loaded with saved values

--- _RichScripts/Save Systems/Rememberers/RememberTransform.cs
+++ _RichScripts/Save Systems/Rememberers/RememberTransform.cs
@@ -5,16 +5,18 @@ namespace RichPackage.SaveSystem
         public override void SaveState(ES3File saveFile)
         {
             SaveData.IsDirty = true;
-            SaveData.properties.Load(myTransform);
+            SaveData.properties.Load(myTransform); // move live data to memento
             base.SaveState(saveFile);
         }
 
         public override void LoadState(ES3File saveFile)
         {
+            // use current state if no save data
             if (!saveFile.KeyExists(SaveID))
                 return;
 
             base.LoadState(saveFile);
+            SaveData.properties.Store(myTransform); // set live data from memento
         }
 
         [System.Serializable]
