0bfee9274c55a33f3483a8fc98535e5be7f8bacf
Richard Osborn <richardosborn256@gmail.com>
2023-05-15 19:22:07 -0700

    Update TransformProperties.cs
    Remove scale property altogether since there is no real way to set the scale.
    Add more load/store methods.
    Add TransformPropertiesExtensions for more api.

--- _RichScripts/TransformProperties.cs
+++ _RichScripts/TransformProperties.cs
@@ -6,54 +6,113 @@ namespace RichPackage
 	/// <summary>
 	/// Properties for a <see cref="Transform"/>.
 	/// </summary>
+	[System.Serializable]
 	public struct TransformProperties
 	{
 		public Space space;
 		public Vector3 position;
         public Quaternion rotation;
-        public Vector3 scale;
-
-		public void Load(Transform t)
-		{
-			// validate
-			GuardAgainst.ArgumentIsNull(t, nameof(t));
-
-			// operate
-			if (space == Space.Self)
-			{
-				position = t.localPosition;
-				rotation = t.localRotation;
-				scale = t.localScale;
-			}
-			else
-			{
-				position = t.position;
-				rotation = t.rotation;
-				scale = t.lossyScale;
-			}
-		}
 
         #region Constructors
 
         public TransformProperties(Transform transform, Space space = Space.World)
 		{
+            // validate
 			GuardAgainst.ArgumentIsNull(transform, nameof(transform));
 
+            // operate
 			this.space = space;
 			if (space == Space.Self)
 			{
                 position = transform.localPosition;
                 rotation = transform.localRotation;
-                scale = transform.localScale;
             }
 			else
 			{
                 position = transform.position;
                 rotation = transform.rotation;
-                scale = transform.lossyScale;
             }
 		}
 
         #endregion Constructors
+
+        /// <summary>
+        /// Sets this object's properties to <paramref name="t"/>'s.
+        /// </summary>
+        public void Load(Transform t)
+        {
+            // validate
+            GuardAgainst.ArgumentIsNull(t, nameof(t));
+
+            // operate
+            if (space == Space.Self)
+            {
+                position = t.localPosition;
+                rotation = t.localRotation;
+            }
+            else
+            {
+                position = t.position;
+                rotation = t.rotation;
+            }
+        }
+
+        /// <summary>
+        /// Sets this object's properties to <paramref name="t"/>'s.
+        /// </summary>
+        public void Load(Transform t, Space space)
+        {
+            this.space = space;
+            Load(t);
+        }
+
+        /// <summary>
+        /// Stores this object's properties to <paramref name="t"/>'s.
+        /// </summary>
+        public void Store(Transform t)
+        {
+            // validate
+            GuardAgainst.ArgumentIsNull(t, nameof(t));
+
+            // operate
+            if (space == Space.Self)
+            {
+                t.localPosition = position;
+                t.localRotation = rotation;
+            }
+            else
+            {
+                t.position = position;
+                t.rotation = rotation;
+            }
+        }
+    }
+
+    public static class TransformPropertiesExtensions
+    {
+        /// <summary>
+        /// Sets the world space position and rotation of <paramref name="t"/>.
+        /// </summary>
+        public static void SetPositionAndRotation(this Transform t, TransformProperties props)
+        {
+            // validate
+            Debug.Assert(props.space == Space.World);
+
+            // operate
+            t.SetPositionAndRotation(props.position, props.rotation);
+        }
+
+        /// <summary>
+        /// Sets the local space position and rotation of <paramref name="t"/>.
+        /// </summary>
+        public static void SetLocalPositionAndRotation(this Transform t, TransformProperties props)
+        {
+            // validate
+            Debug.Assert(props.space == Space.Self);
+
+            // operate
+            t.localPosition = props.position;
+            t.localRotation = props.rotation;
+        }
     }
 }
