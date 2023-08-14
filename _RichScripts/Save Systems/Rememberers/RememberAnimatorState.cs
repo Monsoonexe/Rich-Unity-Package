e8b7f82f08ef95be2fb399c9dea24a10c03f2b4c
Richard Osborn <richardosborn256@gmail.com>
2023-08-13 22:38:40 -0700

    Fix not saving animator state when being disabled

--- _RichScripts/Save Systems/Rememberers/RememberAnimatorState.cs
+++ _RichScripts/Save Systems/Rememberers/RememberAnimatorState.cs
@@ -11,12 +11,29 @@ namespace RichPackage.SaveSystem
         [Required]
         public Animator myAnimator;
 
+        /// <summary>
+        /// The live state of the animator when this was last disabled.
+        /// </summary>
+        private AnimatorStateInfo? stagedState;
+
         protected override void Reset()
         {
             base.Reset();
             SetDevDescription("Remembers the last time and state of the animation.");
             myAnimator = GetComponent<Animator>();
             SaveID = $"{name}-{nameof(RememberAnimatorState)}";
+        }
+
+        protected override void OnDisable()
+        {
+            base.OnDisable();
+            stagedState = myAnimator.GetCurrentAnimatorStateInfo(0); // cache the current state in case we need to save
+        }
+
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+            stagedState = null; // use live state instead when enabled
         }
 
         #region Save/Load
@@ -28,7 +45,7 @@ namespace RichPackage.SaveSystem
 
         protected override void SaveStateInternal()
         {
-            AnimatorStateInfo state = myAnimator.GetCurrentAnimatorStateInfo(0);
+            AnimatorStateInfo state = stagedState ?? myAnimator.GetCurrentAnimatorStateInfo(0);
             SaveData.time = state.normalizedTime;
             SaveData.stateHash = state.fullPathHash;
         }
