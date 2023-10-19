16887c14c03931776fdd9e9c90109b7ca136c2ad
Richard Osborn <richardosborn256@gmail.com>
2023-10-18 22:33:41 -0700

    Create RichEditorUtility.cs

---
+++ Editor/RichEditorUtility.cs
@@ -0,0 +1,39 @@
+using RichPackage;
+using UnityEngine;
+using UnityEditor;
+
+namespace RichPackage.Editor
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static partial class RichEditorUtility
+    {
+        /// <summary>
+        /// The primary menu item folder name for tools and helpers.
+        /// </summary>
+        /// <remarks>Ends in a backslash.</remarks>
+        public const string MenuName = "Tools/RichPackage/";
+
+        private const string CompilerLockMenu = "Tools/Lock Compiler";
+
+        [MenuItem(CompilerLockMenu)]
+        public static void ToggleCompilerLock()
+        {
+            // get current state
+            bool locked = Menu.GetChecked(CompilerLockMenu);
+
+            // flip
+            Menu.SetChecked(CompilerLockMenu, !locked);
+
+            if (locked)
+            {
+                EditorApplication.UnlockReloadAssemblies();
+            }
+            else
+            {
+                EditorApplication.LockReloadAssemblies();
+            }
+        }
+    }
+}
