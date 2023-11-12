d6274af1b3cdae66d4cc6da7f636b68f91e254f3
Richard Osborn <richardosborn256@gmail.com>
2023-11-12 12:05:10 -0800

    Support inheritance

--- _RichScripts/Save Systems/GameObjectVariables.cs
+++ _RichScripts/Save Systems/GameObjectVariables.cs
@@ -11,13 +11,17 @@ namespace RichPackage.SaveSystem
         [SerializeField]
         private VariableTable table;
 
-        public VariableTable Table => table;
+        public VariableTable Table
+        {
+            get => table;
+            protected set => table = value;
+        }
 
         #region Editor
 #if UNITY_EDITOR
 
         [UnityEditor.MenuItem("CONTEXT/" + nameof(GameObjectVariables) + "/Add Rememberer")]
-        private static void AddRememberer(UnityEditor.MenuCommand command)
+        protected static void AddRememberer(UnityEditor.MenuCommand command)
         {
             var g = (GameObjectVariables)command.context; // the thing clicked on
             g.gameObject.AddComponent<RememberGameObjectVariables>()
