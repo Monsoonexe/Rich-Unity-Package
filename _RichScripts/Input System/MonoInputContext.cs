06bd425d3507556f420731157d08bdc26e8906b0
Richard Osborn <richardosborn256@gmail.com>
2023-11-26 12:23:18 -0800

    Create MonoInputContext.cs

---
+++ _RichScripts/Input System/MonoInputContext.cs
@@ -0,0 +1,28 @@
+namespace RichPackage.InputSystem
+{
+    /// <summary>
+    /// An input context that exists in the scene. <br/>
+    /// Allows self-driving through 'Update'.
+    /// </summary>
+    public abstract class MonoInputContext : RichMonoBehaviour, IInputContext
+    {
+        #region IInputContext
+
+        public virtual string Name => GetType().Name;
+
+        /// <remarks>Override 'Update' instead.</remarks>
+        public void Execute() { }
+
+        public void OnEnter() => enabled = true;
+
+        public void OnExit() => enabled = false;
+
+        #endregion IInputContext
+
+        #region Unity Messages
+
+        protected abstract void Update();
+
+        #endregion Unity Messages
+    }
+}
