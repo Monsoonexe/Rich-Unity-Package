fe8d8c3fea19757f29b80e0b16c034a40bf4b515
Richard Osborn <richardosborn256@gmail.com>
2024-01-19 20:49:13 -0800

    Update JsonTable.cs
    Fix bugs :(

--- _RichScripts/Advanced Collections/JsonTable.cs
+++ _RichScripts/Advanced Collections/JsonTable.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 
 namespace ScaryRobot.LostInSpace
 {
+    /// <summary>
+    /// A table of values that are serialized to JSON.
+    /// </summary>
     [System.Serializable]
     public class JsonTable : Dictionary<string, string>
     {
@@ -13,14 +16,14 @@ namespace ScaryRobot.LostInSpace
         
         public T Get<T>(string key)
         {
-            return JsonConvert.DeserializeObject<T>(key);
+            return JsonConvert.DeserializeObject<T>(this[key]);
         }
         
         public T GetOrDefault<T>(string key, T @default = default)
         {
             return TryGetValue(key, out string value)
                 ? JsonConvert.DeserializeObject<T>(value)
-                : default;
+                : @default;
         }
     }
 }
