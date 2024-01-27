71b8c36b78ccf6c0b5de2c8c0bc851aff5bcbd7d
Richard Osborn <richardosborn256@gmail.com>
2024-01-26 21:18:14 -0800

    Upgrade database asset
    use keys instead of indices

--- _RichScripts/Database Assets/ADatabaseAsset.cs
+++ _RichScripts/Database Assets/ADatabaseAsset.cs
@@ -1,110 +1,256 @@
+using RichPackage.UI;
 using Sirenix.OdinInspector;
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
 namespace RichPackage.Databases
 {
-	/// <summary>
-	/// An asset that holds references to <see cref="Object"/>s and can look them up by keys.
-	/// Useful for propagating known entities through the network.
-	/// </summary>
-	/// <typeparam name="TData">The type of data that actually gets held in the database.</typeparam>
-    public abstract class ADatabaseAsset<TData> : RichScriptableObject
-		where TData : RichScriptableObject, IDatabaseMember
+    public abstract class ADatabaseAsset : RichScriptableObject
     {
-		[Title("Database")]
-		[Tooltip("Index all the replay objects here. Be sure their replay entity script has this index set in the inspector!")]
-		[SerializeField, AssetsOnly, Required, ListDrawerSettings(NumberOfItemsPerPage = 40,
-			ShowIndexLabels = true), LabelWidth(50)]
+        // common base class to differentiate these classes
+    }
+
+    /// <summary>
+    /// An asset that holds references to <see cref="Object"/>s and can look them up by keys.
+    /// Useful for propagating known entities through the network.
+    /// </summary>
+    /// <typeparam name="TData">The type of data that actually gets held in the database.</typeparam>
+    public abstract class ADatabaseAsset<TData> : ADatabaseAsset, IEnumerable<TData>
+        where TData : UnityEngine.Object, IDatabaseMember
+    {
+        [Title("Database")]
+        [Tooltip("Index all the replay objects here. Be sure their replay entity script has this index set in the inspector!")]
+        [SerializeField, AssetsOnly, Required, LabelWidth(50),
+            ListDrawerSettings(NumberOfItemsPerPage = 40, ShowIndexLabels = true)]
 #if UNITY_EDITOR
-		[InfoBox("$" + nameof(editorErrorMessage), InfoMessageType.Error, VisibleIf = nameof(EditorHasError))]
+        [InfoBox("$" + nameof(editorErrorMessage), InfoMessageType.Error, VisibleIf = nameof(EditorHasError))]
 #endif
-		protected TData[] items;
+        protected TData[] items = Array.Empty<TData>(); // helps prevent null-refs
 
-		public bool autoAssignKeys = false;
+        protected readonly Dictionary<int, TData> lookupTable = new Dictionary<int, TData>();
 
-		protected virtual void Reset()
-		{
-			SetDevDescription($"A database for {typeof(TData).Name}s.");
-		}
+        /// <summary>
+        /// All of the items in the database.
+        /// </summary>
+        public IReadOnlyCollection<TData> All => items;
 
-		public virtual TData Get(int index)
-		{
-			if (!items.IndexIsInRange(index))
-				throw new System.IndexOutOfRangeException($"{nameof(items)}: {index} | {items.Length}");
+        protected virtual void Reset()
+        {
+            SetDevDescription($"A database for {typeof(TData).Name}.");
+        }
 
-			return items[index];
-		}
+        private void OnEnable()
+        {
+            lookupTable.Clear();
+            foreach (var item in items)
+            {
+#if UNITY_EDITOR
+                try
+                {
+                    lookupTable.Add(item.Key, item);
+                }
+                catch
+                {
+                    Debug.LogError($"Duplicate key: {item.Key}.", item);
+                }
+#else
+				lookupTable.Add(item.Key, item);
+#endif
+            }
+        }
 
-		public virtual int GetKey(TData value)
-		{
-			int index = items.IndexOf(value);
+        /// <summary>
+        /// Retrieve an item from the database.
+        /// </summary>
+        [Button]
+        public TData Get(int key)
+        {
+            if (lookupTable.TryGetValue(key, out TData value))
+                return value;
+                
+            throw new KeyNotFoundException($"Key {key} not found in {this}.");
+        }
 
-			// complain if couldn't be found
-			if (index < 0)
-			{
-				string message = $"The item '{value}' could not be found in {name}.";
-				throw new System.Collections.Generic.KeyNotFoundException(message);
-			}
+        public TData Get(Predicate<TData> query)
+        {
+            foreach (TData i in items)
+            {
+                if (query(i))
+                    return i;
+            }
 
-			return items[index].Key;
-		}
+            return null;
+        }
 
-		public bool TryGet(int key, out TData item)
-		{
-			item = null;
-			if (items.IndexIsInRange(key))
-			{
-				item = items[key];
-				return true;
-			}
+        public TData GetOrDefault(int key, TData @default = default)
+        {
+            if (lookupTable.TryGetValue(key, out var value))
+                return value;
+            else
+                return @default;
+        }
 
-			return false;
-		}
+        public bool TryGet(int key, out TData value)
+        {
+            return lookupTable.TryGetValue(key, out value);
+        }
 
-		#region Editor
+        /// <summary>
+        /// Retrieve a random element from the database.
+        /// </summary>
+        public TData GetRandom() => items.GetRandomElement();
+
+        public bool Contains(TData data) => lookupTable.ContainsValue(data);
+
+        public bool Contains(int key) => lookupTable.ContainsKey(key);
+
+        protected virtual int GenerateNewKey() => UnityEngine.Random.Range(int.MinValue + 1, int.MaxValue);
+
+        #region IEnumerable
+
+        IEnumerator<TData> IEnumerable<TData>.GetEnumerator() => All.GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() => All.GetEnumerator();
+
+        #endregion IEnumerable
+
+        #region Editor
 #if UNITY_EDITOR
 
-		protected string editorErrorMessage;
+        protected string editorErrorMessage;
 
-		protected bool EditorHasError { get => !editorErrorMessage.IsNullOrEmpty(); }
+        protected bool EditorHasError { get => !editorErrorMessage.IsNullOrEmpty(); }
 
-		/// <summary>
-		/// Validate database and assign keys.
-		/// </summary>
-		protected virtual void OnValidate()
-		{
-			bool hasNull = false;
-			editorErrorMessage = null;
+        /// <summary>
+        /// Validate database and assign keys.
+        /// </summary>
+        [Button, HideInPlayMode]
+        protected virtual void OnValidate()
+        {
+            editorErrorMessage = null;
 
-			// assign indices
-			foreach (var (Item, Index) in items.ForEachWithIndex())
-			{
-				if (Item == null)
-				{
-					Debug.LogError(editorErrorMessage = $"Entity {Index} is null, and it should not be. " +
-						$"Please remove or assign the item.", this);
-					hasNull = true;
-				}
-				else
+            if (!VerifyNoNullEntries())
+                return;
+
+            if (!VerifyCollectionHasNoDuplicateEntries())
+                return;
+
+            EnsureEntriesHaveUniqueKeys();
+        }
+
+        protected void EnsureEntriesHaveUniqueKeys()
+        {
+            var table = new Dictionary<int, TData>();
+
+            foreach (TData item in items)
+            {
+                // check for uninitialized keys
+                if (item.Key == 0)
                 {
-                    if (autoAssignKeys)
-						Item.Key = Index;
-				}
-			}
+                    AssignUniqueKey(item);
+                }
+                // check for key collision
+                else if (table.TryGetValue(item.Key, out TData existingItem))
+                {
+                    AssignUniqueKey(item);
+                    Debug.LogWarning($"{item} has a key collision with {existingItem}" +
+                        $" ({existingItem.Key}) in {name}." +
+                        $" Assigned new key to {item} ({item.Key}).", item);
+                }
 
-			// can't continue if there are null entries
-			if (hasNull)
-				return;
+                table.Add(item.Key, item);
+            }
+        }
 
-			// detect duplicates
-			var groups = items.GroupBy(x => x.name);
-            foreach (var duplicate in groups.Where(g => g.Count() > 1))
-                Debug.LogError(editorErrorMessage = $"There are duplicate <{duplicate.Key}> entities in the database. " +
-                    $"Please remove them leaving only 1.", this);
+        protected bool VerifyCollectionHasNoDuplicateEntries()
+        {
+            bool hasDuplicate = false;
+            var hashSet = new HashSet<TData>();
+
+            foreach (TData item in items)
+            {
+                if (!hashSet.Add(item))
+                {
+                    hasDuplicate = true;
+                    editorErrorMessage =
+                        $"There are duplicate <{item}> entities in the database. " +
+                        "Please remove duplicates and leave only 1.";
+                    Debug.LogError(editorErrorMessage, this);
+                }
+            }
+
+            return !hasDuplicate;
+        }
+
+        protected bool VerifyNoNullEntries()
+        {
+            bool valid = true;
+            foreach (var (Item, Index) in items.ForEachWithIndex())
+            {
+                if (Item == null)
+                {
+                    valid = false;
+                    Debug.LogError(editorErrorMessage = $"{this} has an item at index {Index} that is null. Please fix this.", this);
+                }
+            }
+
+            return valid;
+        }
+
+        protected bool IsKeyUnique(TData query)
+        {
+            foreach (var item in items)
+            {
+                if (item.Key == query.Key && item != query)
+                    return false;
+            }
+
+            return true;
+        }
+
+        [Button, HideInPlayMode]
+        public void FindAllAssets()
+        {
+            var assets = new List<TData>(8);
+            FindAllAssets(assets);
+            items = assets.ToArray();
+            UnityEditor.EditorUtility.SetDirty(this);
+            OnValidate();
+        }
+
+        // TODO - move this to a class anywhere can access, not just the editor assembly
+        /// <param name="assets">Collection to add the assets to.</param>
+        public static void FindAllAssets<TAsset>(List<TAsset> assets)
+            where TAsset : UnityEngine.Object
+        {
+            // get all the guids
+            foreach (string guid in UnityEditor.AssetDatabase.FindAssets("t:" + typeof(TAsset).Name))
+            {
+                // find the path
+                string path = UnityEditor.AssetDatabase.GUIDToAssetPath(guid);
+
+                // find the asset 
+                TAsset asset = UnityEditor.AssetDatabase.LoadAssetAtPath<TAsset>(path);
+
+                // add to list
+                assets.Add(asset);
+            }
+        }
+
+        protected void AssignUniqueKey(TData data)
+        {
+            int newKey; 
+            do
+                newKey = GenerateNewKey(); // assign a newly generated key
+            while (Contains(newKey)); // check for key collision
+            data.Key = newKey;
+            UnityEditor.EditorUtility.SetDirty(data);
         }
 
 #endif
-		#endregion Editor
-	}
+        #endregion Editor
+    }
 }
