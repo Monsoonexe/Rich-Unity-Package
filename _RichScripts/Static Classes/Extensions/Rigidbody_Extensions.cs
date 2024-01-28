5d872ae64f624f4489aaae56201cf6a042adf1d7
Richard Osborn <richardosborn256@gmail.com>
2024-01-27 21:30:47 -0800

    Added more helper methods

--- _RichScripts/Static Classes/Extensions/Rigidbody_Extensions.cs
+++ _RichScripts/Static Classes/Extensions/Rigidbody_Extensions.cs
@@ -6,6 +6,34 @@ using UnityEngine;
 /// </summary>
 public static class Rigidbody_Extensions
 {
+    public static Rigidbody FreezePosition(this Rigidbody rb)
+    {
+        // add the freeze position mask
+        rb.constraints |= RigidbodyConstraints.FreezePosition;
+        return rb;
+    }
+
+    public static Rigidbody UnFreezePosition(this Rigidbody rb)
+    {
+        // remove the freeze position mask
+        rb.constraints &= ~RigidbodyConstraints.FreezePosition;
+        return rb;
+    }
+    
+    public static Rigidbody FreezeRotation(this Rigidbody rb)
+    {
+        // add the freeze position mask
+        rb.constraints |= RigidbodyConstraints.FreezeRotation;
+        return rb;
+    }
+
+    public static Rigidbody UnFreezeRotation(this Rigidbody rb)
+    {
+        // remove the freeze position mask
+        rb.constraints &= ~RigidbodyConstraints.FreezeRotation;
+        return rb;
+    }
+
     public static Rigidbody FreezePositionAndRotation(this Rigidbody rb)
     {
         rb.constraints = RigidbodyConstraints.FreezeAll;
