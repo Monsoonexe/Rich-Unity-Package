7023e1ebad58d71965104521463c1206ecb83448
Richard Osborn <richardosborn256@gmail.com>
2024-01-31 17:39:29 -0800

    Create EnableEventMessages.cs

---
+++ _RichScripts/Events/EnableEventMessages.cs
@@ -0,0 +1,53 @@
+using RichPackage;
+using RichPackage.Events;
+using System;
+
+namespace UnityEngine
+{
+    /// <summary>
+    /// Raises events when the behavior is en/disabled.
+    /// </summary>
+    public class EnableEventMessages : RichMonoBehaviour
+    {
+        private EventHandlerList onDisabledList;
+        private EventHandlerList onEnabledList;
+        private EventHandlerList<bool> eventList;
+        private EventHandlerList<EnableEventMessages, bool> eventList1;
+
+        #region Unity Messages
+
+        private void OnDestroy()
+        {
+            onDisabledList?.RemoveAll();
+            onEnabledList?.RemoveAll();
+            eventList?.RemoveAll();
+            eventList1?.RemoveAll();
+
+            onDisabledList = null;
+            onEnabledList = null;
+            eventList = null;
+            eventList1 = null;
+        }
+
+        private void OnEnable()
+        {
+            onEnabledList?.Invoke();
+            eventList?.Invoke(true);
+            eventList1?.Invoke(this, true);
+        }
+
+        private void OnDisable()
+        {
+            onDisabledList?.Invoke();
+            eventList?.Invoke(false);
+            eventList1?.Invoke(this, false);
+        }
+
+        #endregion Unity Messages
+
+        public void AddListener(Action<bool> action) => (eventList ??= new()).Add(action);
+        public void RemoveListener(Action<bool> action) => eventList.Remove(action);
+
+        // TODO - add more support
+    }
+}
