e1237cfe25113a5b0eccc878099d43525b831e23
Richard Osborn <richardosborn256@gmail.com>
2024-03-31 12:46:06 -0700

    Create ScriptingSymbols.cs

---
+++ _RichScripts/Static Classes/ScriptingSymbols.cs
@@ -0,0 +1,7 @@
+// Common scripting symbols
+
+public static partial class ScriptingSymbols
+{
+    public const string UnityEditor = "UNITY_EDITOR";
+    public const string UnityAssertions = "UNITY_ASSERTIONS";
+}
