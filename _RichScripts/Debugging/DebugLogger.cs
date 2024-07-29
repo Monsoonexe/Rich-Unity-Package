9eabc7235bf4c4cf8917a4e7ee8e193a5fe8e43f
Richard Osborn <richardosborn256@gmail.com>
2024-07-28 19:13:05 -0700

    Hide logs from callstack

--- _RichScripts/Debugging/DebugLogger.cs
+++ _RichScripts/Debugging/DebugLogger.cs
@@ -15,52 +15,61 @@ namespace RichPackage.Debugging
 		/// Log a message to the console with Debug.Log(string).
 		/// </summary>
 		[Title("Log Helpers")]
-		[Button, FoldoutGroup(BUTTON_GROUP_TAG)]
+		[Button, FoldoutGroup(BUTTON_GROUP_TAG),
+            HideInCallstack]
 		public void Log(string message) => Debug.Log(message);
 
-		/// <summary>
-		/// Logs <see langword="this"/>'s <see cref="UnityEngine.Object.name"/>.
-		/// </summary>
+        /// <summary>
+        /// Logs <see langword="this"/>'s <see cref="UnityEngine.Object.name"/>.
+        /// </summary>
+        [HideInCallstack]
 		public void LogSelf() => Debug.Log(name, this);
 
 		/// <summary>
 		/// Log a message to the console with Debug.Log(string, object) and gives self as context.
 		/// </summary>
-		[Button, FoldoutGroup(BUTTON_GROUP_TAG)]
+		[Button, FoldoutGroup(BUTTON_GROUP_TAG),
+            HideInCallstack]
 		public void LogSelf(string message) => Debug.Log(message, this);
 
 		/// <summary>
 		/// Log a message to the console with Debug.LogError(string).
 		/// </summary>
-		[Button, FoldoutGroup(BUTTON_GROUP_TAG)]
+		[Button, FoldoutGroup(BUTTON_GROUP_TAG),
+            HideInCallstack]
 		public void LogError(string message) => Debug.LogError(message);
 
-		/// <summary>
-		/// Logs <see langword="this"/>'s <see cref="UnityEngine.Object.name"/>.
-		/// </summary>
-		public void LogErrorSelf() => Debug.LogError(name, this);
+        /// <summary>
+        /// Logs <see langword="this"/>'s <see cref="UnityEngine.Object.name"/>.
+        /// </summary>
+        [HideInCallstack]
+        public void LogErrorSelf() => Debug.LogError(name, this);
 
 		/// <summary>
 		/// Log a message to the console with Debug.LogError(string, object) and gives self as context.
 		/// </summary>
-		[Button, FoldoutGroup(BUTTON_GROUP_TAG)]
+		[Button, FoldoutGroup(BUTTON_GROUP_TAG),
+            HideInCallstack]
 		public void LogErrorSelf(string message) => Debug.LogError(message, this);
 
 		/// <summary>
 		/// Log a message to the console with Debug.LogWarning(string).
 		/// </summary>
-		[Button, FoldoutGroup(BUTTON_GROUP_TAG)]
+		[Button, FoldoutGroup(BUTTON_GROUP_TAG),
+            HideInCallstack]
 		public void LogWarning(string message) => Debug.LogWarning(message);
 
-		/// <summary>
-		/// Logs <see langword="this"/>'s <see cref="UnityEngine.Object.name"/>.
-		/// </summary>
-		public void LogWarningSelf() => Debug.LogWarning(name, this);
+        /// <summary>
+        /// Logs <see langword="this"/>'s <see cref="UnityEngine.Object.name"/>.
+        /// </summary>
+        [HideInCallstack]
+        public void LogWarningSelf() => Debug.LogWarning(name, this);
 
 		/// <summary>
 		/// Log a message to the console with Debug.LogWarning(string, object) and gives self as context.
 		/// </summary>
-		[Button, FoldoutGroup(BUTTON_GROUP_TAG)]
+		[Button, FoldoutGroup(BUTTON_GROUP_TAG),
+            HideInCallstack]
 		public void LogWarningSelf(string message) => Debug.LogWarning(message, this);
 	}
 }
