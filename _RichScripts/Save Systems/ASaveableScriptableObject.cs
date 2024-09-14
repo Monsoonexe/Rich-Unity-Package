3fa53eb6bdc7fe4c14674f6693fc1962be083d1c
Richard Osborn <richardosborn256@gmail.com>
2024-09-13 23:52:44 -0700

    Rename memento to value

--- _RichScripts/Save Systems/ASaveableScriptableObject.cs
+++ _RichScripts/Save Systems/ASaveableScriptableObject.cs
@@ -6,21 +6,21 @@ namespace RichPackage.SaveSystem
         where TMemento : class, new()
     {
         [SerializeField]
-        private TMemento memento = new TMemento();
+        private TMemento value = new TMemento();
 
-        public virtual TMemento Value { get => memento; set => memento = value; }
+        public virtual TMemento Value { get => value; set => this.value = value; }
 
         public override void LoadState(ISaveStore saveFile)
         {
             if (saveFile.KeyExists(SaveID))
             {
-                saveFile.LoadInto(SaveID, memento);
+                saveFile.LoadInto(SaveID, value);
             }
         }
 
         public override void SaveState(ISaveStore saveFile)
         {
-            saveFile.Save(SaveID, memento);
+            saveFile.Save(SaveID, value);
         }
 
         public static implicit operator TMemento(ASaveableScriptableObject<TMemento> a) => a.Value;
