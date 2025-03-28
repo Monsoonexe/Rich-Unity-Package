25a3773877befd2d1b4e04c60c3cf38fb27c8fba
Richard Osborn <richardosborn256@gmail.com>
2025-03-28 00:42:40 -0700

    Create Primitive pointer type

---
+++ _RichScripts/PrimitivePointers.cs
@@ -0,0 +1,33 @@
+﻿namespace RichPackage.PrimitivePointers
+{
+    /// <summary>
+    /// Pointer-like object for an <see langword="int"/>.
+    /// </summary>
+    /// <remarks>Maybe 'BoxedInt'?</remarks>
+    public sealed class IntObject : APtr<int>
+    {
+        public IntObject() : this(default) { }
+
+        public IntObject(int value)
+        {
+            Value = value;
+        }
+    }
+
+    public abstract class APtr<T> : VoidPtr
+    {
+        public T Value;
+
+        public APtr() : this(default) { }
+
+        public APtr(T value)
+        {
+            Value = value;
+        }
+
+        public static implicit operator T (APtr<T> ptr) => ptr.Value;
+    }
+
+    // basically 'object'
+    public abstract class VoidPtr { }
+}
