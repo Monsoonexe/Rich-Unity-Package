e9c74b6e2cef1874534b6636a41cba8c7e9c71a9
Richard Osborn <richardosborn256@gmail.com>
2025-08-19 00:12:37 -0700

    Create Wait.cs

---
+++ _RichScripts/YieldInstructions/Wait.cs
@@ -0,0 +1,11 @@
+﻿namespace UnityEngine
+{
+    public static class Wait
+    {
+        public static readonly WaitForSeconds ForOneSecond = new WaitForSeconds(1.0f);
+        public static readonly WaitForSeconds ForHalfSecond = new WaitForSeconds(0.5f);
+        public static readonly WaitForSeconds ForQuarterSecond = new WaitForSeconds(0.25f);
+        public static readonly WaitForSeconds ForTenthSecond = new WaitForSeconds(0.1f);
+        public const object ForNextFrame = null;
+    }
+}
