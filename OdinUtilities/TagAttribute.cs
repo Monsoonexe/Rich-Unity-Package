44cb21818c951025102a6126b44c0a315a35ea1a
Richard Osborn <richardosborn256@gmail.com>
2025-09-28 01:15:13 -0700

    Move to namespace

--- OdinUtilities/TagAttribute.cs
+++ OdinUtilities/TagAttribute.cs
@@ -3,5 +3,8 @@
 
 using System;
 
-[AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
-public class TagAttribute : Attribute { }
+namespace RichPackage
+{
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
+    public class TagAttribute : Attribute { }
+}
